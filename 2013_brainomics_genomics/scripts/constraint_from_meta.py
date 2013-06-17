1d7b17e0865525306bd2803f89bee13f031b0ddd
Vincent Frouin <vincent.frouin@cea.fr>
2013-06-17 17:33:22 +0200

    clarify example of go constraint generation

--- 2013_brainomics_genomics/scripts/constraint_from_meta.py
+++ 2013_brainomics_genomics/scripts/constraint_from_meta.py
@@ -7,33 +7,42 @@ import pybedtools
 import json
 import numpy as np
 
-#  load meta info from GO/GSEA, USCC refGene and snps available from mes
-go  = json.load(open("/neurospin/brainomics/2013_brainomics_genomics/data/test.json"))
-ref = pybedtools.BedTool('/neurospin/brainomics/2013_brainomics_genomics/data/refGene.bed')
-snps = pybedtools.BedTool("/neurospin/brainomics/2013_brainomics_genomics/data/snps_b37_ref.bed")
 
-def tree_path_gene_snp(name = 'synaptic transmission'):
+def loadrefDB():
+   ref = pybedtools.BedTool('/neurospin/brainomics/2013_brainomics_genomics/data/refGene.bed')
+   snps = pybedtools.BedTool("/neurospin/brainomics/2013_brainomics_genomics/data/snps_b37_ref.bed")
+
+   return ref, snps
+
+def tree_path_gene_snp(ontology, ref=None, snps=None, name = 'synaptic transmission'):
    go_gene_snp = dict()
-   go_gene_snp[n] = dict()
+   go_gene_snp[name] = dict()
 
-   for g in go[n]:
-      print g
+   for g in ontology[name]:
+      #print g
       if len(g)>0:
          subset = pybedtools.BedTool(ref.filter(lambda b: b.name.endswith('|%s'%g)>0).saveas())
          tmp = snps.intersect(subset)
          if len(tmp)>0:
-            go_gene_snp[n][g] = np.unique([i.name for i in tmp]).tolist()
+            go_gene_snp[name][g] = np.unique([i.name for i in tmp]).tolist()
    return go_gene_snp
-
 
 def gene_snp(name='UTS2'):
    subset = pybedtools.BedTool(ref.filter(lambda b: b.name.endswith('|%s'%name)>0).saveas())
    tmp = snps.intersect(subset)
    return np.unique([i.name for i in tmp]).tolist()
-   
+
+
+
+
 if __name__=="__main__":
+   ref, snps = loadrefDB()
+   
+   #  load meta info from GO/GSEA, USCC refGene and snps available from mes
+   go  = json.load(open("/neurospin/brainomics/2013_brainomics_genomics/data/test.json"))
+
    # example with a go entry
-   tree = tree_path_gene_snp()
+   tree = tree_path_gene_snp(go, ref=ref, snps=snps, name = 'synaptic transmission')
 
    # exmaple with a gene entry
-   snp_list = gene_snp()
+   snp_list = gene_snp(ref=ref, snps=snps,name='UTS2')
