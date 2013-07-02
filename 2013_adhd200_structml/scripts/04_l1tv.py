5c3545ae9b2885376cedeaed63bdf0ee36e97b33
duchesnay <edouard.duchesnay@gmail.com>
2013-07-02 12:09:23 +0200

    2013_cati-freesurfer-roi-thickness: Jeff Demo; 2013_adhd200_structml: performances

--- 2013_adhd200_structml/scripts/04_l1tv.py
+++ 2013_adhd200_structml/scripts/04_l1tv.py
@@ -5,9 +5,11 @@ Created on Tue Jun 25 10:28:34 2013
 @author: edouard.duchesnay@cea.fr
 """
 
+import numpy as np
 from structured import LogisticRegressionL1TV
 from sklearn import preprocessing
 from sklearn.metrics import Scorer, recall_score 
+from structured import loss_functions
 
 # Scripts PATH
 try:
@@ -20,6 +22,8 @@ ADHD200_DATA_BASE_PATH = "/volatile/duchesnay/data/2013_adhd_structml"
 INPUT_PATH = os.path.join(ADHD200_DATA_BASE_PATH, "python_analysis", "data")
 
 execfile(os.path.join(ADHD200_SCRIPTS_BASE_PATH, "lib", "data_api.py"))
+import cPickle as pickle
+tv_path = os.path.join(ADHD200_DATA_BASE_PATH, "python_analysis", "precomp", "tv_loss.pkl")
 
 ## ==========================================================================
 feature = "mw"
@@ -29,21 +33,71 @@ feature = "mw"
 #INPUT_PATH2 = os.path.join(ADHD200_DATA_BASE_PATH, "python_analysis", "data")
 
 X_train , y_train = get_data(INPUT_PATH, feature, test=False)
-X_test , y_test = get_data(INPUT_PATH, feature, test=True)
+#X_test , y_test = get_data(INPUT_PATH, feature, test=True)
 
 # Scale data
 scaler = preprocessing.StandardScaler().fit(X_train)
 X_train = scaler.transform(X_train)
-X_test = scaler.transform(X_test)
+#X_test = scaler.transform(X_test)
 
-mask = get_mask(INPUT_PATH)
 
-from structured import loss_functions
-l=1; gamma=1; shape=mask.shape; mask=mask; mu=None
-self = LogisticRegressionL1TV(l=1, gamma=1, shape=mask.shape, mask=mask)
+#mask = np.array(get_mask(INPUT_PATH) == 1, dtype=np.int)
+#shape=mask.shape
+#mask = mask.ravel()
 
-clf = LogisticRegressionL1TV(l=1, gamma=1, shape=mask.shape, mask=mask)
 
+l=1; gamma=1; mu=None
+
+## BUILD SAVE tv =============================================================
+l=1; gamma=1; mu=None
+#clf = LogisticRegressionL1TV(l=1, gamma=1, shape=mask.shape, mask=mask)
+#tv = loss_functions.TotalVariation(gamma, shape, mu, mask)
+#with open(tv_path, 'wb') as outfile: pickle.dump(tv, outfile, pickle.HIGHEST_PROTOCOL)
+
+## SAVE Everything
+nith open("X.npy", 'wb') as outfile: pickle.dump(tv, outfile, pickle.HIGHEST_PROTOCOL)
+np.save("X.npy", X_train)
+np.save("y.npy", y_train)
+
+
+# RE-READ tv ============================================================
+with open(tv_path, 'rb') as infile: tv = pickle.load(infile)
+
+# save Ax, Ay, Az, 
+
+clf = LogisticRegressionL1TV(l=1, gamma=1, shape=(5,5,5))
+self = clf
+
+self._tv =  tv
+self._tv.buff = np.zeros((tv.Ax.shape[1], 1))
+self._tv.mu = 0.0001
+
+
+self._combo = loss_functions.CombinedNesterovLossFunction(self._lr,
+                                                          self._tv)
+self.set_g(self._combo)
+self._l1 = loss_functions.L1(l)
+self.set_h(self._l1)
+
+X = X_train
+#y = np.asanyarray(y_train, dtype = 'int32')
+
+y = np.asanyarray(y_train, dtype = X.dtype)[:, np.newaxis]
+
+######### GO INTO ALGO
+self.set_data(X, y)
+clf.algorithm.g.a.lipschitz = 5029851.1238164632
+#Out[69]: 5029851.1238164632
+clf.algorithm.g.b.lambda_max = 88.849255330364926
+
+self = clf.algorithm
+t=None; tscale=0.95; early_stopping_mu=None
+self.run(X, y)
+
+t = 1.6051788336542154e-07
+#######################
+
+%time clf.fit(X, y)
 
 #%time svmlin.fit(X_train, y_train)
 # CPU times: user 17.58 s, sys: 1.50 s, total: 19.09 s
--- 2013_cati-freesurfer-roi-thickness/01_classification.py
+++ 2013_cati-freesurfer-roi-thickness/01_classification.py
@@ -21,18 +21,28 @@ X = Xy["X"]
 y = Xy["y"]
 
 from sklearn.lda import LDA
-from sklearn.svm import SVC
+from sklearn.svm import SVC, LinearSVC
 from sklearn.feature_selection import SelectKBest
+from sklearn import preprocessing
+
 from epac import range_log2
 from epac import Perms, CV, CVBestSearchRefit, Pipe, Methods
 k_values = range_log2(X.shape[1], add_n=True)
 C_values = [.1, 1, 10, 100]
 n_folds = 10
 
+LinearSVC(class_weight='auto')
+
 # Pipelines(Anova filter + svm) for many values of k and C
-anova_svm = Methods(*[Pipe(SelectKBest(k=k),
-                      Methods(*[SVC(kernel="linear", C=C) for C in C_values]))
+#anova_svm = Methods(*[Pipe(SelectKBest(k=k),preprocessing.StandardScaler(),
+#                      Methods(*[SVC(kernel="linear", C=C) for C in C_values]))
+#                  for k in k_values])
+
+anova_svm = Methods(*[Pipe(SelectKBest(k=k), preprocessing.StandardScaler(),
+                      Methods(*[LinearSVC(C=C, penalty=penalty, class_weight='auto', dual=False) 
+                                for C in C_values for penalty in  ['l1', 'l2']]))
                   for k in k_values])
+
 # Take a look
 print [l for l in anova_svm.walk_leaves()]
 
@@ -46,18 +56,19 @@ cv = CV(anova_svm_all, n_folds=n_folds)
 cv.fit_predict(X=X, y=y)
 cv.reduce()
 
+# {'key': SelectKBest(k=4)/StandardScaler/LinearSVC(penalty=l1,C=10), 'mean_score_te': 0.84, 'mean_score_tr': 0.846666666667},
 
-# Add LDA
-# -------
+pipe = Pipe(SelectKBest(k=4), preprocessing.StandardScaler(), LinearSVC(C=10, penalty="l1", 
+     class_weight='auto', dual=False))
+pipe.fit_predict(X=X, y=y)
 
-anova_svm_lda = Methods(*[Pipe(SelectKBest(k=k),
-                      Methods(*[LDA()]+[SVC(kernel="linear", C=C) for C in C_values]))
-                  for k in k_values])
-anova_svm_lda_cv = CVBestSearchRefit(anova_svm_lda)
+sv = pipe.children[0].children[0]     
+sv.estimator.coef_
+kb = pipe.estimator
+kb.get_support()
+np.where(kb.get_support())
+#Out[114]: (array([ 22,  36,  96, 146]),)
 
-anova_svm_lda_all = Methods(anova_svm_lda, anova_svm_lda_cv)
+d_num.columns[np.array([ 22,  36,  96, 146])]
+Index([u'LH_G_OCTEMP_MEDPARAHIP_THICKNESS', u'LH_G_TEMPORAL_INF_THICKNESS', u'RH_G_OCTEMP_MEDPARAHIP_THICKNESS', u'RH_S_TEMPORAL_SUP_THICKNESS'], dtype=object)
 
-# Cross validate
-cv = CV(anova_svm_lda_all, n_folds=n_folds)
-cv.fit_predict(X=X, y=y)
-cv.reduce()
