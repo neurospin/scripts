d9bf262691e6d137cc6c208eccd954375055be87
Mathieu Dubois <mathieu.dubois@cea.fr>
2013-07-16 17:49:01 +0200

    ENH 2013_subdepression/scripts/02_svm.py: don't run the workflow (instead we save it for later execution with SOMA workflow); also add some configuration options.
    This changes the way the script work but hopefully make it more simple to use.

--- 2013_subdepression/scripts/02_svm.py
+++ 2013_subdepression/scripts/02_svm.py
@@ -8,80 +8,113 @@ DOC
 
 """
 
+DEFAULT_C_VALUES = [0.1, 1, 10]
+DEFAULT_REGULARIZATION_METHODS = ['l1', 'l2']
+DEFAULT_K_VALUES = 'auto'
+
+DEFAULT_FOLDS_NESTED  = 5
+DEFAULT_FOLDS_EVAL    = 5
+
+DEFAULT_NUM_PROCESSES = 5
+
 # Standard library modules
-import os, sys, argparse
+import os, sys, argparse, ast
 # Numpy and friends
-import numpy
 import sklearn, sklearn.svm, sklearn.feature_selection
 # For writing HDF5 files
 import tables
 
 import epac, epac.map_reduce.engine
 
-select = sklearn.feature_selection.SelectKBest
-scale  = sklearn.preprocessing.StandardScaler()
 # Local import
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../lib'))
 import data_api
 
-def apply_svm(h5filename, n_folds_nested, n_folds_eval, n_cores):
+def do_all(h5filename, workflow_dir,
+           C_values = DEFAULT_C_VALUES, k_values = DEFAULT_K_VALUES, regularization_methods = DEFAULT_REGULARIZATION_METHODS,
+           n_folds_nested = DEFAULT_FOLDS_NESTED, n_folds_eval = DEFAULT_FOLDS_EVAL,
+           num_processes = DEFAULT_NUM_PROCESSES):
+    '''Create EPAC workflow for SVM parameter selection'''
+
     # Open the output file
     h5file = tables.openFile(h5filename, mode = "r")
     X, Y, mask = data_api.get_data(h5file)
     n_features = X.shape[1]
+    y = Y[:, 0]
 
-    # EPAC workflow
-    C_values = [0.1, 1, 10]
-    k_values = epac.range_log2(n_features, add_n=True)
-    regularization_methods = ['l1', 'l2']
+    if k_values == 'auto':
+        k_values = epac.range_log2(n_features, add_n=True)
+
+    # Create the SVM parameter selection wf
     # Create all the classifiers
     pipelines = epac.Methods(*[
-      epac.Pipe(select(k=k), scale, epac.Methods(*[
-                                      sklearn.svm.LinearSVC(class_weight='auto',
-                                                            C=C, penalty=penalty,
-                                                            dual=False)
-                                      for C in C_values
-                                      for penalty in regularization_methods]))
-       for k in k_values])
+      epac.Pipe(sklearn.feature_selection.SelectKBest(k=k),
+                sklearn.preprocessing.StandardScaler(),
+                epac.Methods(*[sklearn.svm.LinearSVC(class_weight='auto',
+                               C=C, penalty=penalty,
+                               dual=False)
+                               for C in C_values
+                               for penalty in regularization_methods]))
+                               for k in k_values])
     # Select the best with CV
     best_pipeline = epac.CVBestSearchRefit(pipelines,
                                            n_folds=n_folds_nested)
     # Evaluate it
-    wf = epac.CV(best_pipeline,
-                 n_folds=n_folds_eval)
-   
-    # Run the workflow
-    print 'Running'
-    epac.conf.TRACE_TOPDOWN=True
-    wf.run(X=X, y=Y[:, 0])
-    print 'Finished'
-    
+    select_wf = epac.CV(best_pipeline,
+                 n_folds=n_folds_eval,
+                 keep=True)
+
+    # Save it
+    sfw_engine = epac.map_reduce.engine.SomaWorkflowEngine(tree_root=select_wf,
+                                                           num_processes=num_processes)
+    sfw_engine.export_to_gui(workflow_dir, X=X, y=y)
+
     h5file.close()
-    return wf.reduce()
+    return X, y, select_wf
 
 if __name__ == '__main__':
-    DEFAULT_FOLDS_NESTED = 5
-    DEFAULT_FOLDS_EVAL   = 5
-    DEFAULT_CORES = 5
+    # Stupid type convert for k_values
+    def convert_k_values(arg):
+        try:
+            k_values = ast.literal_eval(arg)
+        except:
+            k_values = arg
+        return k_values
 
     # Parse CLI
-    parser = argparse.ArgumentParser(description='''Load the data in HDF5 and apply SVM.''')
+    parser = argparse.ArgumentParser(description='''Create a workflow to select SVM parameters.''')
 
     parser.add_argument('h5filename',
       type=str,
-      help='Write to outfilename')
+      help='Read from filename')
+
+    parser.add_argument('workflow_dir',
+      type=str,
+      help='Directory on which to save the workflow')
+
+    parser.add_argument('--C_values',
+      type=ast.literal_eval, default=DEFAULT_C_VALUES,
+      help='Values for C (python list)')
+
+    parser.add_argument('--k_values',
+      type=convert_k_values, default=DEFAULT_K_VALUES,
+      help='Values for k (python list or \'auto\')')
+
+    parser.add_argument('--regularization_methods',
+      type=ast.literal_eval, default=DEFAULT_REGULARIZATION_METHODS,
+      help='Values for k (python string)')
 
     parser.add_argument('--n_folds_nested',
       type=int, default=DEFAULT_FOLDS_NESTED,
       help='Number of folds to use for model selection')
-    
+
     parser.add_argument('--n_folds_eval',
       type=int, default=DEFAULT_FOLDS_EVAL,
       help='Number of folds to use for model evaluation')
-      
-    parser.add_argument('--n_cores',
-      type=int, default=DEFAULT_CORES,
+
+    parser.add_argument('--num_processes',
+      type=int, default=DEFAULT_NUM_PROCESSES,
       help='Number of processes to use')
-      
+
     args = parser.parse_args()
-    reduced_wf = apply_svm(**vars(args))
+    X, y, select_wf = do_all(**vars(args))
