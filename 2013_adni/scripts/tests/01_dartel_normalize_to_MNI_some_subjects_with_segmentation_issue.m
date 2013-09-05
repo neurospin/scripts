aaabc1cde0b7064bf5b0876a6851ba1ceb506d40
Matthieu Perrot <matthieu.perrot@gmail.com>
2013-09-05 10:37:22 +0200

    ADD: script to normalize subject on a template with Dartel (+ tests)

---
+++ 2013_adni/scripts/tests/01_dartel_normalize_to_MNI_some_subjects_with_segmentation_issue.m
@@ -0,0 +1,28 @@
+%-----------------------------------------------------------------------
+% Job configuration created by cfg_util (rev $Rev: 4252 $)
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.tools.dartel.mni_norm.template = {'/neurospin/cati/Users/perrot/brainomics_2013_adni/templates/template510/Template_6.nii'};
+%%
+matlabbatch{1}.spm.tools.dartel.mni_norm.data.subjs.flowfields = {
+	'/neurospin/cati/Users/perrot/brainomics_2013_adni/AD/u_130_S_1201_S25082_I63758_Nat_dartel_greyProba_Template.nii'
+	'/neurospin/cati/Users/perrot/brainomics_2013_adni/AD/u_131_S_0691_S17266_I48048_Nat_dartel_greyProba_Template.nii'
+	'/neurospin/cati/Users/perrot/brainomics_2013_adni/Control/u_002_S_1280_S26453_I60057_Nat_dartel_greyProba_Template.nii'
+                                                                  };
+%%
+matlabbatch{1}.spm.tools.dartel.mni_norm.data.subjs.images = {
+                                                              {
+	'/neurospin/cati/ADNI/ADNI_510/BVdatabase/AD/130_S_1201_S25082_I63758/t1mri/default_acquisition/spm_new_segment/segmentation/130_S_1201_S25082_I63758_Nat_greyProba.nii'
+	'/neurospin/cati/ADNI/ADNI_510/BVdatabase/AD/131_S_0691_S17266_I48048/t1mri/default_acquisition/spm_new_segment/segmentation/131_S_0691_S17266_I48048_Nat_greyProba.nii'
+	'/neurospin/cati/ADNI/ADNI_510/BVdatabase/Control/002_S_1280_S26453_I60057/t1mri/default_acquisition/spm_new_segment/segmentation/002_S_1280_S26453_I60057_Nat_greyProba.nii'
+                                                              }
+                                                              {
+	'/neurospin/cati/ADNI/ADNI_510/BVdatabase/AD/130_S_1201_S25082_I63758/t1mri/default_acquisition/spm_new_segment/segmentation/130_S_1201_S25082_I63758_Nat_whiteProba.nii'
+	'/neurospin/cati/ADNI/ADNI_510/BVdatabase/AD/131_S_0691_S17266_I48048/t1mri/default_acquisition/spm_new_segment/segmentation/131_S_0691_S17266_I48048_Nat_whiteProba.nii'
+	'/neurospin/cati/ADNI/ADNI_510/BVdatabase/Control/002_S_1280_S26453_I60057/t1mri/default_acquisition/spm_new_segment/segmentation/002_S_1280_S26453_I60057_Nat_whiteProba.nii'
+                                                              }
+                                                              }';
+matlabbatch{1}.spm.tools.dartel.mni_norm.vox = [NaN NaN NaN];
+matlabbatch{1}.spm.tools.dartel.mni_norm.bb = [NaN NaN NaN
+                                               NaN NaN NaN];
+matlabbatch{1}.spm.tools.dartel.mni_norm.preserve = 1;
+matlabbatch{1}.spm.tools.dartel.mni_norm.fwhm = [0 0 0];
