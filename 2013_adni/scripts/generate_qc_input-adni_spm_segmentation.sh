d741560efacf26d430e03f61bd30f6c6ab3f8209
Matthieu Perrot <matthieu.perrot@gmail.com>
2013-09-05 18:13:36 +0200

    ADD: generate input for quantitative QC scripts

---
+++ 2013_adni/scripts/generate_qc_input-adni_spm_segmentation.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+for a in AD Control MCI; do for b in $(ls /neurospin/cati/ADNI/ADNI_510/BVdatabase/$a/*/ -d); do c="$b/t1mri/default_acquisition/spm_new_segment/segmentation/"; echo $(basename $b | sed 's/_.*//g') $(basename $b) $(ls $c/*Nat_greyProba.nii) $(ls $c/*Nat_whiteProba.nii) $(ls $c/*Nat_csfProba.nii) $(ls $c/*Nat_skullProba.nii) $(ls $c/*Nat_scalpProba.nii); done; done  > /home/mp210984/tarball/git/neurospin/scripts/2013_adni/scripts/qc_input-adni_spm_segmentation.txt 
