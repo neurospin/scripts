8c75feb28a67494bfeaae32a95acebf2e716f014
duchesnay <edouard.duchesnay@gmail.com>
2013-10-09 20:26:04 +0200

    mescog: QC after feedback from Marco: units homogeneization Paris vs Munich

--- 2013_mescog/clinic/01_merge_CADASIL-ASPS.py
+++ 2013_mescog/clinic/01_merge_CADASIL-ASPS.py
@@ -1,28 +1,42 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Jun 10 13:39:45 2013
+INPUT:
 
-Produce two files:
-- db_clinic_cadasil-asps_mapping_summary.csv
-  Contain a summary of the variables within the merged DB.
+1) base_commun_20131003.csv
+2) ASPS_klinVariables_20130806.csv
+3) DB_Mapping_Longit_Last_EJ_20131007.csv
 
-  Fields:
+OUTPUT:
+    
+1) Summary of common DB
+    "db_clinic_cadasil-asps_mapping_summary.csv"
+    "db_clinic_cadasil-asps_mapping_summary.html"
+    Fields:
 
       NEW_NAME: The new name in the merged DB
 
-      IN_BOTH : Is the variable present in both DB ?
+      CADASIL_NAME & ASPS_NAME: original names
 
-      IN_COMMON_BD: Is the variable present in the common DB?
+      IN_COMMON: Is the variable present in the common DB?
       A variable is present in the common DB if it is in both DB at baseline
       and it is least present in one two DB for other time points.
 
-      CADASIL_NAME & ASPS_NAME: original names
+      IN_CAD: was the variable found in CADASIL?
 
-      cada_base_commun: is the variable present in the file "base_commun.xls" ?
+      IN_ASPS: was the variable found in ASPS?
+      
+      CAD/ASPS_NaN: number of missing data.
 
-      cada_france2012: is the variable present in the file "france2012.xls" ?
+      CAD/ASPS_mean: Means, for numerical variables only.
 
-      recoding: The re-coding python code if relevent. 3 types of re-coding may occcur:
+      CAD/ASPS_std: Std-dev, for numerical variables only.
+
+      CAD/ASPS_min/max: Min/max, for numerical variables only.
+
+      CAD/ASPS_count: Count of each level of categorial variables
+
+      RECODING: The re-coding python code if relevent. 3 types of re-coding may occcur:
           - change discrete value: ex: " cada={1:'M', 2:'F'};" means that 1
           are re-coded 'M' and 2 'F'
           - Creation of new variables using original ones. Ex for BMI
@@ -30,9 +44,10 @@ Produce two files:
           - Unit convertion. Exa: var_cada = mmoll_to_mgdl(cadasil_base[cadasil_name], mm=386.65)
 
       remark: 
-    
-- db_clinic_cadasil-asps-common.csv
-  the common DB
+
+2) Common DB
+    "db_clinic_cadasil-asps-common.csv"
+
 
 @author: edouard.duchesnay@cea.fr
 """
@@ -44,71 +59,125 @@ import pandas as pd
 import numpy as np
 
 WD = "/neurospin/mescog"
-WD = "/home/edouard/data/2013_mescog"
+#WD = "/home/edouard/data/2013_mescog"
 
+###################################################################################
+# INPUTS
 MAPPING_SUMMARY_FILEPATH = os.path.join(WD, "clinic", "db_clinic_cadasil-asps_mapping_summary") #.csv | .html
 MERGE_CADASIL_ASPS_FILEPATH = os.path.join(WD, "clinic", "db_clinic_cadasil-asps-common") #.csv | .html
 
 mapping_filepath = os.path.join(WD, "clinic",
-                                "DB_Mapping_Longit_Last_EJ_2013-05-08.csv")
+                                "DB_Mapping_Longit_Last_EJ_20131007.csv")
 
-cadasil_base_commun_filepath = os.path.join(WD, "clinic", "base_commun.csv")
-cadasil_base_commun = pd.read_table(cadasil_base_commun_filepath, header=0).replace("-", np.nan)
+cadasil_base_commun_filepath = os.path.join(WD, "clinic", "base_commun_20131008.csv")
+cadasil_base = pd.read_table(cadasil_base_commun_filepath, header=0, sep=',').replace("-", np.nan)
 
-cadasil_france2012_filepath = os.path.join(WD, "clinic", "france2012.csv")
-cadasil_france2012 = pd.read_table(cadasil_france2012_filepath, header=0).replace("-", np.nan)
+asps_filepath = os.path.join(WD, "clinic", "ASPS_klinVariables_20130806.csv")
+asps_base = pd.read_table(asps_filepath, header=0)
 
 # MAPPING
 mapping = pd.read_table(mapping_filepath, header=0).replace("-", np.nan)
 
-##############################################################################
-## IN_BOTH variables output => common_df
-## Merged clinical base will contain all variables with a  "new common name"
-## in DB_Mapping_Longit_Last_EJ_2013-05-08.csv
-## Standardized variables:
-## If a variable is IN_BOTH at M0, all time point should be standardized
-## Other are simply copied
-##############################################################################
-# If present in both, keep all time points even if not present in ASPS
 # Upper case cadasil_base_commun.columns
-cadasil_base_commun.columns = [s.upper() for s in cadasil_base_commun.columns]
-cadasil_france2012.columns = [s.upper() for s in cadasil_france2012.columns]
+cadasil_base.columns = [s.upper() for s in cadasil_base_commun.columns]
+#asps.columns = [s.upper() for s in asps.columns]  ## ASPS already in upper case
 
 # Check no doublons
-print len(set(cadasil_base_commun.columns)) == len(cadasil_base_commun.columns)
-print len(set(cadasil_france2012.columns)) == len(cadasil_france2012.columns)
+print len(set(cadasil_base.columns)) == len(cadasil_base_commun.columns)
 
-# cadasil_base is cadasil_base_commun
-cadasil_base = cadasil_base_commun
-##############################################################################
-## Fake ASPS base
-n_asps = 100
-asps_dict = {mapping["ASPS.name"][i]: [np.nan] * n_asps for i in \
-    xrange(mapping.shape[0]) if mapping["ASPS.given"][i]}
-asps_dict["ID"] = range(n_asps)
-asps_base = pd.DataFrame(asps_dict)
+# cadasil_base_commun
 
+###################################################################################
+# UTILS
 def replace(values, rep):
     return [rep[v] if v in rep else v for v in values]
 
 def mmoll_to_mgdl(mmolpl, mm):
     return mmolpl * mm / 10.0
 
-#mmol_per_liter_to_mg_per_del(53, mm=180)
+unit = cadasil_base[cadasil_name+"C"]
+x = cadasil_base[cadasil_name]
+mm=180
 
-# IN_BOTH : present in both cadasil and asps
-# IN_COMMON_BD: present in the common database. If the baseline is in both
-# dDB (IN_BOTH=1) all the next time-point will be in the common DB even if they
-# are missing in one DB.
+out = list()
+for i in xrange(len(unit)):
+    #unit[i], x[i]
+    if pd.isnull(x[i]):
+        mgd = np.nan
+    elif unit[i] == 'G/L':
+        mgdl = x[i] * 100.
+    elif unit[i] == 'MG/DL':
+        mgdl = x[i]
+    elif unit[i] == '\xc2\xb5MOL/L':  #µMOL/L
+        mgdl = x[i] * mm / 100.0
+    elif unit[i] == 'MMOL/L':
+        mgdl = x[i] * mm / 10.0
+    else:
+        raise ValueError("??%s %i" % (unit[i], i))
+    out.append(mgdl)
+
+
+#def convert_to_mgdl(x)
+#set([nan, 'G/L', 'DM', '%', 'NF', 'MG/DL', '\xc2\xb5MOL/L', 'MMOL/L'])
+
+def do_stat(var_cada, var_asps):
+    """Do some statistics on variable. Return a tuble with:
+    "CAD_NaN",  "ASPS_NaN" # of missing
+
+    for quantititative variables:
+        "CAD_mean", "ASPS_mean", "CAD_std", "ASPS_std": basic stats
+        "CAD_min", "ASPS_min", "CAD_max", "ASPS_max": basic stats
+
+    for qulitative variables:
+        "CAD_count", "ASPS_count"
+    """
+#    def is_numeric(obj):
+#        attrs = ['__add__', '__sub__', '__mul__', '__div__', '__pow__']
+#        return all(hasattr(obj, attr) for attr in attrs)
+    c = pd.Series(var_cada)
+    a = pd.Series(var_asps)
+    c_na = pd.isnull(c)
+    a_na = pd.isnull(a)
+    if len(set(c[np.logical_not(c_na)]).union(set(a[np.logical_not(a_na)]))) > 6:  
+        stats = [
+        c_na.sum(), a_na.sum(),
+        np.mean(c[np.logical_not(c_na)]), np.mean(a[np.logical_not(a_na)]),
+        np.std(c[np.logical_not(c_na)]), np.std(a[np.logical_not(a_na)]),
+        np.nan if not len(c[np.logical_not(c_na)]) else np.min(c[np.logical_not(c_na)]),
+        np.nan if not len(a[np.logical_not(a_na)]) else np.min(a[np.logical_not(a_na)]),
+        np.nan if not len(c[np.logical_not(c_na)]) else np.max(c[np.logical_not(c_na)]),
+        np.nan if not len(a[np.logical_not(a_na)]) else np.max(a[np.logical_not(a_na)]),
+        np.nan, np.nan]
+    else:
+        c_counts = ['%s:%i' % (str(l), (c[np.logical_not(c_na)]==l).sum()) for 
+                l in set(c[np.logical_not(c_na)])]
+        a_counts = ['%s:%i' % (str(l), (a[np.logical_not(a_na)]==l).sum()) for 
+                l in set(a[np.logical_not(a_na)])]
+        stats = [
+            c_na.sum(), a_na.sum(),
+            np.nan, np.nan,
+            np.nan, np.nan,
+            np.nan, np.nan,
+            np.nan, np.nan,
+            "; ".join(c_counts), "; ".join(a_counts)]
+    return stats
+
+stat_colnames = ["CAD_NaN",  "ASPS_NaN",
+            "CAD_mean", "ASPS_mean", "CAD_std", "ASPS_std",
+            "CAD_min", "ASPS_min", "CAD_max", "ASPS_max",
+            "CAD_count", "ASPS_count"]
+
+###################################################################################
+# DO THE MAPPING
 variables = list()
 common_db = dict()
 for i in xrange(mapping.shape[0]):
     #i = 0
     l = mapping.ix[i, :]
-    if l['time point'] == 'M0' or pd.isnull(l['time point']):  # Reset IN_COMMON_BD flag
-        IN_COMMON_BD = 0
+    if l['time point'] == 'M0' or pd.isnull(l['time point']):  # Reset IN_COMMON flag
+        IN_COMMON = 0
     cadasil_name = asps_name = new_name = recode_srt = remark_str = ""
-    IN_BOTH = in_cadasil_base = in_cadasil_france2012 = 0
+    in_cadasil_base = in_asps_base = 0
     if pd.notnull(l["new common name"]):  # could be null since noty repeated for time points
         curr = l['new common name']
         new_name = curr
@@ -116,29 +185,23 @@ for i in xrange(mapping.shape[0]):
         new_name = curr + "@" + l['time point']
     if l["CADASIL.given"] == 1:
         cadasil_name = l["CADASIL.name"].upper()
+        in_cadasil_base = int(cadasil_name in cadasil_base.columns)
     if l["ASPS.given"] == 1:
         asps_name = l["ASPS.name"]
+        in_asps_base = int(asps_name in asps_base.columns)
     if cadasil_name and asps_name:
-        IN_BOTH = 1
-        IN_COMMON_BD = 1
-    if IN_COMMON_BD and (cadasil_name or asps_name):
+        IN_COMMON = 1
+    if IN_COMMON and (in_cadasil_base or in_asps_base):
         print i, "new_name", new_name, "cada:", cadasil_name, "asps:", asps_name, "time", l['time point']
-        in_cadasil_base = int(cadasil_name in cadasil_base.columns)
-        in_cadasil_france2012 = int(cadasil_name in cadasil_france2012.columns)
+        #in_cadasil_france2012 = int(cadasil_name in cadasil_france2012.columns)
         if in_cadasil_base:
             var_cada = cadasil_base[cadasil_name].tolist()
         else:
             var_cada = [np.nan] * cadasil_base.shape[0]
-        in_asps_base = int(asps_name in asps_base.columns)
         if in_asps_base:
             var_asps = asps_base[asps_name].tolist()
         else:
             var_asps = [np.nan] * asps_base.shape[0]
-
-        # Start re-coding
-        if new_name == "AGE_AT_INCLUSION":
-            recode_srt = """var_cada=cadasil_base["DATEINCL"]-cadasil_base["DATENAIS"]"""
-            remark_str = "PROBLEM: DATEINCL, DATENAIS not found in base_commun, What is Ts in base_commun only for german patient"
 
         if new_name.find("SYS_BP") == 0:
             remark_str = "ok"
@@ -160,7 +223,7 @@ for i in xrange(mapping.shape[0]):
             remark_str = "ok"
 
         if (new_name.find('MIGRAINE_WO_AURA') == 0) or (new_name.find('MIGRAINE_WITH_AURA') == 0) :
-            recode_srt = "cada={2:0}; asps={}"
+            recode_srt = """cada={2:0}; asps={"nein":0, "ja":1}"""
             remark_str = "recode"
             exec(recode_srt)
             var_cada = replace(var_cada, cada)
@@ -174,42 +237,44 @@ for i in xrange(mapping.shape[0]):
             ## var_asps = replace(var_asps, asps)
 
         if new_name == "MIGRAINE_FREQ":
-            remark_str = "STANDARDIZE: CADASIL:during 2 last years : 1 (>1/week) ; 2 (>1/month < 1/week) ; 3 (>1/3 months < 1/month) ; 4 (>1 /2years < 1/3 months) ; 5 (none) WITH ASPS:migraine attacks frequency with therapy"
-            recode_srt = """cada={}; asps={}"""
-            exec(recode_srt)
-            var_cada = replace(var_cada, cada)
-            var_asps = replace(var_asps, asps)
+#            remark_str = "STANDARDIZE: CADASIL:during 2 last years : 1 (>1/week) ; 2 (>1/month < 1/week) ; 3 (>1/3 months < 1/month) ; 4 (>1 /2years < 1/3 months) ; 5 (none) WITH ASPS:migraine attacks frequency with therapy"
+#            recode_srt = """cada={}; asps={}"""
+#            exec(recode_srt)
+#            var_cada = replace(var_cada, cada)
+#            var_asps = replace(var_asps, asps)
+            IN_COMMON = 0
+            remark_str = "Difficulties to standardize, removed"
 
         if new_name == "MIGRAINE_MED":
-            remark_str = "STANDARDIZE: CADASIL: See Medications WITH ASPS: 1=true, 0=false"
-            recode_srt = """cada={}; asps={}"""
-            exec(recode_srt)
-            var_cada = replace(var_cada, cada)
-            var_asps = replace(var_asps, asps)
+#            remark_str = "STANDARDIZE: CADASIL: See Medications WITH ASPS: 1=true, 0=false"
+#            recode_srt = """cada={}; asps={}"""
+#            exec(recode_srt)
+#            var_cada = replace(var_cada, cada)
+#            var_asps = replace(var_asps, asps)
+            IN_COMMON = 0
+            remark_str = "Difficulties to standardize, removed"
 
         if new_name == "BMI":
-            recode_srt = 'var_cada = cadasil_base["POIDS"] / cadasil_base["TAILLE"]**2'
+            recode_srt = 'var_cada = cadasil_base["POIDS"] / (cadasil_base["TAILLE"] / 100.)**2'
             remark_str = "ok"
             exec(recode_srt)
             var_cada = var_cada.tolist()
 
         if new_name == "BMI@M18":
-            recode_srt = 'var_cada = cadasil_base["POIDS27"] / cadasil_base["TAILLE"]**2'
+            recode_srt = 'var_cada = cadasil_base["POIDS27"] / (cadasil_base["TAILLE"] / 100.)**2'
             remark_str = "ok"
             exec(recode_srt)
             var_cada = var_cada.tolist()
 
         if new_name == "BMI@M36":
-            recode_srt = 'var_cada = cadasil_base["POIDS40"] / cadasil_base["TAILLE"]**2'
+            recode_srt = 'var_cada = cadasil_base["POIDS40"] / (cadasil_base["TAILLE"] / 100.)**2'
             remark_str = "ok"
             exec(recode_srt)
             var_cada = var_cada.tolist()
 
         if new_name == "BMI@M54":
-            recode_srt = 'var_cada = cadasil_base["POIDS54"] / cadasil_base["TAILLE"]**2'
+            recode_srt = 'var_cada = cadasil_base["POIDS54"] / (cadasil_base["TAILLE"] / 100.)**2'
             remark_str = "PROBLEM: POIDS54 not in cadasil_base"
-            #exec(recode_srt)
-            #var_cada = var_cada.tolist()
 
         if new_name == "BMI@M72":
             remark_str = "ok"
@@ -227,19 +292,23 @@ for i in xrange(mapping.shape[0]):
             remark_str = "ok"
             var_asps = replace(var_asps, asps)
 
+        recode_srt_alcohol =\
+        """def asps_to_cad(v):
+                if np.isnan(v): return np.nan
+                if v == 0: return 1
+                if v <= 2: return 2
+                return 3
+var_asps = [asps_to_cad(v) for v in var_asps]
+        """
         if new_name == "ALCOHOL":
             remark_str = "STANDARDIZE:  CADASIL: 1 = none, 2 = < 2 drinks a day ; 3 = > 2 drinks a day ASPS: alcohol in drinks per day"
-            recode_srt = """cada={}; asps={}"""
+            recode_srt = recode_srt_alcohol
             exec(recode_srt)
-            var_cada = replace(var_cada, cada)
-            var_asps = replace(var_asps, asps)
-
+            
         if new_name == "ALCOHOL@M36" or new_name == "ALCOHOL@M72":
             remark_str = "STANDARDIZE:  See ALCOHOL"
-            recode_srt = """cada={}; asps={}"""
+            recode_srt = recode_srt_alcohol
             exec(recode_srt)
-            #var_cada = replace(var_cada, cada)
-            var_asps = replace(var_asps, asps)
 
         if new_name.find("HYPERTENSION") == 0 :
             remark_str = "ok"
@@ -325,43 +394,50 @@ for i in xrange(mapping.shape[0]):
             exec(recode_srt)
             var_cada = replace(var_cada, cada)
             #var_asps = replace(var_asps, asps)
-
+        
+        stats = do_stat(var_cada, var_asps)
         ## Add to common DB
         common_db[new_name] = pd.Series(var_cada + var_asps)
-    if new_name:  # if new common name has been difined keep it
-        variables.append((new_name, IN_BOTH, IN_COMMON_BD, cadasil_name, asps_name,
-                          in_cadasil_base, in_cadasil_france2012,
-                          recode_srt, remark_str))
-
-common_db["ID"] = pd.Series(cadasil_base["ID"].tolist() + asps_base["ID"].tolist())
-common_db["BASE"] = pd.Series(["cadasil"]*len(cadasil_base["ID"]) + ["asps"]*len(asps_base["ID"]))
+        variables.append([new_name, cadasil_name, asps_name, IN_COMMON, 
+                          in_cadasil_base, in_asps_base] + stats + 
+                          [recode_srt, remark_str])
 
 
-## Mapping summary re-order IN_COMMON_BD first
+# recode ID
+common_db["ID"] = pd.Series(
+["CAD_%i" % v for v in cadasil_base["ID"]] + ["ASPS_%i" % v for v in asps_base["ID"]])
+
+#common_db["ID"] = pd.Series(cadasil_base["ID"].tolist() + asps_base["ID"].tolist())
+common_db["BASE"] = pd.Series(["CAD"]*len(cadasil_base["ID"]) + ["ASPS"]*len(asps_base["ID"]))
+
+
+## Mapping summary re-order IN_COMMON first
 mapping_summary = pd.DataFrame(variables,
-                               columns=['NEW_NAME', 'IN_BOTH', 'IN_COMMON_BD', 'CADASIL_NAME', 'ASPS_NAME',
-                      'cada_base_commun', 'cada_france2012',
-                      'recoding', 'remark'])
+                               columns=['NEW_NAME', 'CAD_NAME', 'ASPS_NAME', 'IN_COMMON',
+                      'IN_CAD', 'IN_ASPS'] + stat_colnames + ['RECODING', 'REMARKS'])
 
-mapping_summary = pd.concat([mapping_summary[mapping_summary['IN_COMMON_BD'] == 1],
-mapping_summary[mapping_summary['IN_COMMON_BD'] == 0]])
+
+mapping_summary = pd.concat([mapping_summary[mapping_summary['IN_COMMON'] == 1],
+mapping_summary[mapping_summary['IN_COMMON'] == 0]])
 mapping_summary.index = range(mapping_summary.shape[0])
 #print mapping_summary.to_string()
-print mapping_summary[mapping_summary['IN_COMMON_BD'] == 1].to_string()
+#print mapping_summary[mapping_summary['IN_COMMON'] == 1].to_string()
 
+# SAVE
+print "Save summary\n%s\n%s" % (MAPPING_SUMMARY_FILEPATH+".csv", MAPPING_SUMMARY_FILEPATH+".html")
 mapping_summary.to_csv(MAPPING_SUMMARY_FILEPATH+".csv", sep="\t", index=False)
 mapping_summary.to_html(MAPPING_SUMMARY_FILEPATH+".html")
 
-mapping_summary.to_csv(MAPPING_SUMMARY_FILEPATH+".csv", sep="\t", index=False)
 
 ## COMMON_DB
-db_columns = ["ID", "BASE"] + mapping_summary[mapping_summary.IN_COMMON_BD==1]["NEW_NAME"].tolist()
+db_columns = ["ID", "BASE"] + mapping_summary[mapping_summary.IN_COMMON==1]["NEW_NAME"].tolist()
 
 common_db = pd.DataFrame(common_db, columns=db_columns)
+print "Save common DB\n%s" % MERGE_CADASIL_ASPS_FILEPATH+".csv"
 common_db.to_csv(MERGE_CADASIL_ASPS_FILEPATH+".csv", sep="\t", index=False)
 
-mapping_summary = pd.read_table(MAPPING_SUMMARY_FILEPATH+".csv", header=0)
-s = mapping_summary.to_string()
-f = open(MERGE_CADASIL_ASPS_FILEPATH+".txt", "w")
-f.write(s)
-f.close()
+#mapping_summary = pd.read_table(MAPPING_SUMMARY_FILEPATH+".csv", header=0)
+#s = mapping_summary.to_string()
+#f = open(MERGE_CADASIL_ASPS_FILEPATH+".txt", "w")
+#f.write(s)
+#f.close()
