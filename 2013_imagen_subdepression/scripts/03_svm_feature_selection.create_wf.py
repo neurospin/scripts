f7a10dfae736a161f53d589d6aaefc8b630434c4
Mathieu Dubois <mathieu.dubois@cea.fr>
2013-10-18 18:12:06 +0200

    ENH 2013_imagen_subdepression/scripts/03_svm_feature_selection.create_wf: rewrite in a much simpler way (no more argparse) and create several workflow at the same time (for different images). Also the workflow are a bit different (use of CVBestSearchRefit).

--- 2013_imagen_subdepression/scripts/03_svm_feature_selection.create_wf.py
+++ 2013_imagen_subdepression/scripts/03_svm_feature_selection.create_wf.py
@@ -5,12 +5,13 @@ Created on Tue Jul  7 11:45:36 2013
 @author: Mathieu Dubois (mathieu.dubois@cea.fr)
 
 This script creates a workflow for classification with SVM and feature selection.
-We don't use model selection so the results are biased.
+We create a large number of SVM and put them in a CV loop.
+There is also a parameter selection loop (also in CV).
 
 """
 
 # Standard library modules
-import os, sys, argparse
+import os, sys
 # Numpy and friends
 import numpy
 import sklearn, sklearn.svm, sklearn.feature_selection
@@ -31,9 +32,9 @@ except NameError:
 import data_api, utils
 
 TEST_MODE = False
-MAX_N_FEATURES = 16384
-DEFAULT_WF_NAME    = "svm_feature_selection_wf"
-DEFAULT_IMAGES_NAME = "masked_images"
+MAX_N_FEATURES = 336188 # All features
+IMAGES = ["masked_images", "masked_images_Gender_Age_VSF_ImagingCentreCity",
+          "masked_images_Gender_Age_VSF_Scanner_Type"]
 
 if TEST_MODE:
     DB_PATH='/volatile/DB/micro_subdepression/'
@@ -41,18 +42,6 @@ if TEST_MODE:
 else:
     DB_PATH='/neurospin/brainomics/2013_imagen_subdepression'
     LOCAL_PATH='/volatile/DB/cache/imagen_subdepression.hdf5'
-
-parser = argparse.ArgumentParser(description='''Create a classification workflow wit SVM and parameter selection.''')
-
-parser.add_argument('--wf_name',
-      type=str, default=DEFAULT_WF_NAME,
-      help='Name of the workflow (default: %s)'% (DEFAULT_WF_NAME))
-
-parser.add_argument('--images_name',
-      type=str, default=DEFAULT_IMAGES_NAME,
-      help='Name of the image dataset (default: %s)'% (DEFAULT_IMAGES_NAME))
-
-args = parser.parse_args()
 
 if TEST_MODE:
     C_VALUES = [0.1, 1, 10]
@@ -67,14 +56,16 @@ else:
     K_VALUES = utils.range_log2(32, MAX_N_FEATURES)
 
 if TEST_MODE:
+    N_FOLDS_NESTED  = 3
     N_FOLDS_EVAL    = 3
 else:
+    N_FOLDS_NESTED  = 5
     N_FOLDS_EVAL    = 10
 
 OUT_DIR=os.path.join(DB_PATH, 'results', 'svm_feature_selection/')
 if not os.path.exists(OUT_DIR):
     os.makedirs(OUT_DIR)
-WORKFLOW_PATH= os.path.join(OUT_DIR, args.wf_name)
+WF_NAME_PATTERN="svm_feature_selection_{images}"
 
 #########################
 # Oth step: access data #
@@ -88,14 +79,12 @@ mask = babel_mask.get_data()
 binary_mask = mask != 0
 
 h5file = tables.openFile(LOCAL_PATH)
-masked_images = data_api.get_images(h5file, name=args.images_name)
 
-###########################################
-# 1st step: SVM with feature selection    #
-#           We cross-validate the results #
-###########################################
+####################
+# Create workflows #
+####################
 
-# Create all the classifiers
+# Base workflow: SVM + feature selection
 svms = pipelines = epac.Methods(*[
       epac.Pipe(sklearn.feature_selection.SelectKBest(k=k),
                 sklearn.preprocessing.StandardScaler(),
@@ -106,20 +95,31 @@ svms = pipelines = epac.Methods(*[
                                for penalty in REGULARIZATION_METHODS]))
                                for k in K_VALUES])
 
-# Evaluate it
-svms_cv = epac.CV(svms, n_folds=N_FOLDS_EVAL)
+# Feature selection workflow
+svms_auto_cv = epac.CVBestSearchRefit(svms, n_folds=N_FOLDS_NESTED)
 
-# Store workflow
-X = numpy.asarray(masked_images)
+cv = epac.CV(epac.Methods(svms, svms_auto_cv), n_folds=N_FOLDS_EVAL)
+
+#################################
+# Store workflow per image type #
+#################################
+
 y = numpy.asarray(utils.numerical_coding(df, variables=['group_sub_ctl']).group_sub_ctl)
+for images_name in IMAGES:
+    images = data_api.get_images(h5file, images_name)
+    X = numpy.asarray(images)
 
-engine = epac.map_reduce.engine.SomaWorkflowEngine(svms_cv,
-                                                   resource_id="md238665@gabriel",
-                                                   login="md238665",
-                                                   num_processes=10,
-                                                   remove_finished_wf=False)
-print "Creating workflow at {path}".format(path=WORKFLOW_PATH)
-engine.export_to_gui(WORKFLOW_PATH, X=X, y=y)
-print "Execute the workflow at {path} with SOMA Workflow GUI and run 03_svm_feature_selection.analyze_wf.py".format(path=WORKFLOW_PATH)
+    wf_name = WF_NAME_PATTERN.format(images=images_name)
+    path = os.path.join(OUT_DIR, wf_name)
+    print "Creating workflow at {path}".format(path=path)
+
+    engine = epac.map_reduce.engine.SomaWorkflowEngine(cv,
+                                                       resource_id="md238665@gabriel",
+                                                       login="md238665",
+                                                       num_processes=10,
+                                                       remove_finished_wf=False)
+    engine.export_to_gui(path, X=X, y=y)
+#    print "Running for {images} images".format(images=images_name)
+#    cv.run(X=X, y=y)
 
 h5file.close()
