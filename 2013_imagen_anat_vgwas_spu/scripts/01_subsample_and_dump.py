ddc7d5f621d81daffc415e580833d94a748274e2
Li JINPENG <mr.li.jinpeng@gmail.com>
2013-11-29 09:39:54 +0100

    REFAC: rename to split into data trunks

--- 2013_imagen_anat_vgwas_spu/scripts/01_subsample_and_dump.py
+++ 2013_imagen_anat_vgwas_spu/scripts/01_subsample_and_dump.py
@@ -3,17 +3,10 @@
 Created on Mon Oct 25 2013
 
 @author: vf140245
+@author: jl237561
 
-This script perform :
-    - consider the gaser-segmented data modulated by the new-segment transform
-    (det of Jacobian)
-    - mask (without cereb.)
-    - subsample
-
-The order of the csv file is considered for :
-    - the covariate
-    - the images
-    - the SNP data
+This script performs :
+    - dump all images into disk with mask
 
 It uses a recent version of nilearn.
 The git repository can be cloned at https://github.com/nilearn/nilearn.git
@@ -56,42 +49,52 @@ def check_image(image_array):
     check_array_zero(image_array)
 
 
+def resample(input_image,
+             target_affine,
+             target_shape,
+             interpolation='continuous'):
+    outim = resample_img(input_image,
+                         target_affine=target_affine,
+                         target_shape=target_shape,
+                         interpolation=interpolation)
+    return outim
+
+
+BASE_DIR = '/neurospin/brainomics/2013_imagen_anat_vgwas_spu'
+
 # Input
-BASE_DIR = '/neurospin/brainomics/2013_imagen_bmi/'
-DATA_DIR = os.path.join(BASE_DIR, 'data')
+BASE_INPUT_DIR = os.path.join(BASE_DIR, '2013_imagen_bmi')
+DATA_DIR = os.path.join(BASE_INPUT_DIR, 'data')
 CLINIC_DIR = os.path.join(DATA_DIR, 'clinic')
 IMAGES_DIR = os.path.join(DATA_DIR, 'VBM', 'new_segment_spm8')
 INIMG_FILENAME_TEMPLATE = 'smwc1{subject_id:012}*.nii'
-is_resample = True
+COVARIATE_CSV = os.path.join(BASE_DIR, 'inputdata', '1534bmi-vincent2.csv')
+is_resample = False
 
 # Original mask and mask without cerebellum
-MASK_FILE = os.path.join(DATA_DIR, 'mask.nii')
+MASK_FILE = os.path.join(DATA_DIR, 'mask', 'mask.nii')
 MASK_WITHOUT_CEREBELUM_FILE = os.path.join(DATA_DIR,
                                            'mask_without_cerebellum',
                                            'mask_without_cerebellum_7.nii')
 
 # Images will be shaped as this images
-TARGET_FILE = '/neurospin/brainomics/neuroimaging_ressources/atlases/HarvardOxford/HarvardOxford-LR-cort-333mm.nii.gz'
+TARGET_FILE = '/neurospin/brainomics/neuroimaging_ressources'\
+              '/atlases/HarvardOxford/HarvardOxford-LR-cort-333mm.nii.gz'
 
 # Output files
-OUT_DIR=os.path.join(DATA_DIR, 'dataset_pa_prace')
-OUTIMG_FILENAME_TEMPLATE='rsmwc1{subject_id:012}*.nii'
-RMASK_FILE=os.path.join(OUT_DIR, 'rmask.nii')
-RMASK_WITHOUT_CEREBELUM_FILE=os.path.join(OUT_DIR, 'rmask_without_cerebellum_7.nii')
-OUT_HDF5_FILE=os.path.join(OUT_DIR, 'cache.hdf5')
-OUT_HDF5_FILE_FULRES=os.path.join(OUT_DIR, 'cache_full_res.hdf5')
+BASE_INPUT_DIR = os.path.join(BASE_DIR, 'data')
+OUT_DIR = BASE_INPUT_DIR
+OUTIMG_FILENAME_TEMPLATE = 'rsmwc1{subject_id:012}*.nii'
+RMASK_FILE = os.path.join(OUT_DIR, 'rmask.nii')
+RMASK_WITHOUT_CEREBELUM_FILE = os.path.join(OUT_DIR,
+                                            'rmask_without_cerebellum_7.nii')
+OUT_HDF5_FILE = os.path.join(OUT_DIR, 'cache.hdf5')
+OUT_HDF5_FILE_FULRES = os.path.join(OUT_DIR, 'cache_full_res.hdf5')
 if not os.path.exists(OUT_DIR):
     os.makedirs(OUT_DIR)
 
-def resample(input_image, target_affine, target_shape, interpolation='continuous'):
-    outim = resample_img(input_image,
-                         target_affine=target_affine, target_shape=target_shape,
-                         interpolation=interpolation)
-    return outim
-
 # Open the clinic file to get subject's ID
-# Jinpeng: It misses some two age data  (Age.for.timestamp)
-df = pandas.io.parsers.read_csv(os.path.join(CLINIC_DIR, '1534bmi-vincent2.csv'))
+df = pandas.io.parsers.read_csv(COVARIATE_CSV)
 subject_indices = df.Subjects
 n_images = df.shape[0]
 
@@ -101,30 +104,49 @@ target_shape = target.shape
 target_affine = target.get_affine()
 
 # Resize masks & save them
-babel_mask  = nibabel.load(MASK_FILE)
-# babel_rmask = resample(babel_mask, target_affine, target_shape, interpolation='continuous')
-babel_rmask = babel_mask
+babel_mask = nibabel.load(MASK_FILE)
+if is_resample:
+    babel_rmask = resample(babel_mask,
+                           target_affine,
+                           target_shape,
+                           interpolation='continuous')
+else:
+    babel_rmask = babel_mask
+
 nibabel.save(babel_rmask, RMASK_FILE)
-rmask        = babel_rmask.get_data()
-binary_rmask = rmask!=0
-useful_voxels = numpy.ravel_multi_index(numpy.where(binary_rmask), rmask.shape)
+rmask = babel_rmask.get_data()
+binary_rmask = (rmask != 0)
+useful_voxels = numpy.ravel_multi_index(numpy.where(binary_rmask),
+                                        rmask.shape)
 n_useful_voxels = len(useful_voxels)
 print "Mask reduced: {n} true voxels".format(n=n_useful_voxels)
 
-babel_mask_without_cerebellum  = nibabel.load(MASK_WITHOUT_CEREBELUM_FILE)
-# babel_rmask_without_cerebellum = resample(babel_mask_without_cerebellum, target_affine, target_shape, interpolation='continuous')
-babel_rmask_without_cerebellum = babel_mask_without_cerebellum
-nibabel.save(babel_rmask_without_cerebellum, RMASK_WITHOUT_CEREBELUM_FILE)
-rmask_without_cerebellum        = babel_rmask_without_cerebellum.get_data()
-binary_rmask_without_cerebellum = rmask_without_cerebellum!=0
-useful_voxels_without_cerebellum = numpy.ravel_multi_index(numpy.where(binary_rmask_without_cerebellum), rmask_without_cerebellum.shape)
+# ==========================================================================
+babel_mask_without_cerebellum = nibabel.load(MASK_WITHOUT_CEREBELUM_FILE)
+if is_resample:
+    babel_rmask_without_cerebellum = resample(babel_mask_without_cerebellum,
+                                              target_affine,
+                                              target_shape,
+                                              interpolation='continuous')
+else:
+    babel_rmask_without_cerebellum = babel_mask_without_cerebellum
+nibabel.save(babel_rmask_without_cerebellum,
+             RMASK_WITHOUT_CEREBELUM_FILE)
+rmask_without_cerebellum = babel_rmask_without_cerebellum.get_data()
+binary_rmask_without_cerebellum = (rmask_without_cerebellum != 0)
+useful_voxels_without_cerebellum = \
+    numpy.ravel_multi_index(numpy.where(binary_rmask_without_cerebellum),
+    rmask_without_cerebellum.shape)
 n_useful_voxels_without_cerebellum = len(useful_voxels_without_cerebellum)
-print "Mask without cerebellum reduced: {n} true voxels".format(n=n_useful_voxels_without_cerebellum)
-
+print "Mask without cerebellum reduced: {n} true voxels".format(
+                                n=n_useful_voxels_without_cerebellum)
+# ==========================================================================
 # Read images in the same order than subjects, resample & dump
 images = numpy.zeros((n_images, n_useful_voxels))
-images_without_cerebellum = numpy.zeros((n_images, n_useful_voxels_without_cerebellum))
+images_without_cerebellum = numpy.zeros((n_images,
+                                         n_useful_voxels_without_cerebellum))
 images_dir_files = os.listdir(IMAGES_DIR)
+
 # for (index, subject_index) in enumerate(subject_indices[:2]):
 for (index, subject_index) in enumerate(subject_indices):
     # Find filename
@@ -137,7 +159,7 @@ for (index, subject_index) in enumerate(subject_indices):
     print "index=", index
     print "subject_index=", subject_index
     # Generate output name
-    full_outfilename = os.path.join(OUT_DIR, 'r'+filename[0])
+    full_outfilename = os.path.join(OUT_DIR, 'r' + filename[0])
     print "Reducing {in_} to {out}".format(in_=full_infilename, out=full_outfilename)
     # Resample and save
     input_image = nibabel.load(full_infilename)
@@ -151,12 +173,12 @@ for (index, subject_index) in enumerate(subject_indices):
     masked_image_without_cerebellum = out_image.get_data()[binary_rmask_without_cerebellum]
     images_without_cerebellum[index, :] = masked_image_without_cerebellum
 
-
 # to check if it is sorted
 if not all(subject_indices[i] <= subject_indices[i + 1]
            for i in xrange(len(subject_indices) - 1)):
     raise ValueError("It is not sorted subjuects")
 
+# check if image contain NaN or exactly zero
 check_image(images)
 check_image(images_without_cerebellum)
 
--- 2013_imagen_anat_vgwas_spu/scripts/02_combine_snp_vox_cov.py
+++ 2013_imagen_anat_vgwas_spu/scripts/02_combine_snp_vox_cov.py
@@ -2,17 +2,10 @@
 """
 Created on Mon Nov 5th 2013
 
-@author: vf140245
+@author: jl237561
 
 This script perform :
-    - the writing in a hdf5 
-
-It uses a recent version of igutils.
-The git repository can be cloned at https://github.com:VincentFrouin/igutils.git
-
-Add the following in the script supposing you cloned the repos in ~/gits
-import sys
-sys.path.append('~/gits/igutils')
+    - combine snp data, voxel data, covariate data
 
 """
 import sys
@@ -37,13 +30,17 @@ def check_array_NaN(nparray):
     if np.isnan(nparray).any():
         raise ValueError("np.array contain NaN")
 
+
+BASE_DIR = '/neurospin/brainomics/2013_imagen_anat_vgwas_spu'
+
 # Input
-BASE_DIR = '/neurospin/brainomics/2013_imagen_bmi/'
-DATA_DIR = os.path.join(BASE_DIR, 'data')
-CLINIC_DIR = os.path.join(DATA_DIR, 'clinic')
+BASE_INPUT_DIR = os.path.join(BASE_DIR, '2013_imagen_bmi')
+DATA_DIR = os.path.join(BASE_INPUT_DIR, 'data')
+
 
 # Output files
-OUT_DIR = os.path.join(DATA_DIR, 'dataset_pa_prace')
+BASE_INPUT_DIR = os.path.join(BASE_DIR, 'data')
+OUT_DIR = BASE_INPUT_DIR
 OUT_HDF5_FILE = os.path.join(OUT_DIR, 'cache.hdf5')
 OUT_HDF5_FILE_FULRES=os.path.join(OUT_DIR, 'cache_full_res.hdf5')
 OUT_HDF5_FILE_FULRES_INTER=os.path.join(OUT_DIR, 'cache_full_res_inter.hdf5')
@@ -54,7 +51,10 @@ OUT_IMAGE_NPZ = os.path.join(OUT_DIR, 'image')
 OUT_IMAGE_NO_CERE_NPZ = os.path.join(OUT_DIR, 'images_without_cerebellum')
 OUT_TR_IMAGE_NPZ = os.path.join(OUT_DIR, 'tr_image')
 OUT_TR_IMAGE_NO_CERE_NPZ = os.path.join(OUT_DIR, 'tr_images_without_cerebellum')
-cfn = os.path.join(OUT_DIR, '1534bmi-vincent2.csv')
+COVARIATE_CSV = os.path.join(BASE_DIR, 'inputdata', '1534bmi-vincent2.csv')
+
+
+cfn = COVARIATE_CSV
 # load this file to check that there are 1534 common subjects accros
 #   - csv file
 #   - genotyping file
@@ -65,19 +65,18 @@ cov_header = covdata[0]
 covdata = covdata[1:]
 cov_subj = ["%012d" % int(i.split(',')[0]) for i in covdata]
 
-
-gfn = os.path.join(DATA_DIR, 'qc_sub_qc_gen_all_snps_common_autosome')
+# TODO: verify where is qc_sub_qc_gen_all_snps_common_autosome
+gfn = os.path.join(DATA_DIR, 'genetics', 'qc_sub_qc_gen_all_snps_common_autosome')
 genotype = ig.Geno(gfn)
 geno_subj = genotype.assayIID()
 geno_data = genotype.snpGenotypeAll()
 replace_array_value(geno_data)
-
+check_array_NaN(geno_data)
 
 nb_samples = len(set(cov_subj).intersection(set(geno_subj)))
 
 indices_cov_subj = np.in1d(np.asarray(cov_subj), np.asarray(geno_subj))
 indices_geno_subj = np.in1d(np.asarray(geno_subj), np.asarray(cov_subj))
-
 
 o1 = np.argsort(np.asarray(cov_subj)[indices_cov_subj])
 o2 = np.argsort(np.asarray(geno_subj)[indices_geno_subj])
@@ -92,30 +91,11 @@ covdata_table = np.asarray([i.split(',')[1:] for i in covdata])
 covdata_table = np.asarray(covdata_table)[indices_cov_subj][o1]
 covdata_table = covdata_table.astype("float64")
 
-# covdata_table = np.random.random((4,10))
-# indices_cov_subj = np.asarray([False, True, True, True])
-# geno_data = np.random.random((4,10))
-# indices_geno_subj = np.asarray([False, True, True, True])
-
 geno_data_table = np.asarray(geno_data)[indices_geno_subj][o2]
 
 np.savez(OUT_SNP_NPZ, geno_data_table)
 np.save(OUT_COV_NPY, covdata_table)
 np.savez(OUT_SNP_LIST_NPZ, genotype.snpList())
-
-# ==========================================================================
-# Load images and push into one HDF5 file
-# OUT_HDF5_FILE = "/tmp/data"
-# images = np.random.random((4, 50))
-# images_without_cerebellum = np.random.random((4, 50))
-# h5file = tables.openFile(OUT_HDF5_FILE, mode = "w", title = 'dataset_pa_prace')
-# atom = tables.Atom.from_dtype(images.dtype)
-# filters = tables.Filters(complib='zlib', complevel=5)
-# ds = h5file.createCArray(h5file.root, 'images', atom, images.shape, filters=filters)
-# ds[:] = images
-# ds = h5file.createCArray(h5file.root, 'images_without_cerebellum', atom, images_without_cerebellum.shape, filters=filters)
-# ds[:] = images_without_cerebellum
-# h5file.close()
 
 h5file = tables.openFile(OUT_HDF5_FILE_FULRES, mode = "r")
 images = h5file.getNode(h5file.root, 'images')
@@ -124,7 +104,6 @@ images_without_cerebellum = h5file.getNode(h5file.root, "images_without_cerebell
 images_without_cerebellum = np.asarray(images_without_cerebellum)[indices_cov_subj, :][o1]
 h5file.close()
 
-
 h5file = tables.openFile(OUT_HDF5_FILE_FULRES_INTER, mode = "w", title = 'dataset_pa_prace')
 atom = tables.Atom.from_dtype(images.dtype)
 ds = h5file.createCArray(h5file.root, 'images', atom, images.shape)
@@ -132,11 +111,5 @@ ds[:] = images
 ds = h5file.createCArray(h5file.root, 'images_without_cerebellum', atom, images_without_cerebellum.shape)
 ds[:] = images_without_cerebellum
 h5file.close()
-
-
-#np.savez(OUT_IMAGE_NPZ, images)
-#np.savez(OUT_TR_IMAGE_NPZ, np.transpose(images))
-#np.savez(OUT_IMAGE_NO_CERE_NPZ, images_without_cerebellum)
-#np.savez(OUT_TR_IMAGE_NO_CERE_NPZ, np.transpose(images_without_cerebellum))
 
 print "Images reduced and dumped"
