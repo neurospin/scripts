21ff22c76cfd567722266ff2bbf52837d81d0215
LaureHugo <hugolaure@free.fr>
2014-01-16 11:03:10 +0100

    ENH 2013_mescog/proj_wmh_patterns/01_clustering.Ward.py: added subjects id in dendrogram

--- 2013_mescog/proj_wmh_patterns/01_clustering.Ward.py
+++ 2013_mescog/proj_wmh_patterns/01_clustering.Ward.py
@@ -30,10 +30,8 @@ INPUT_DATASET_DIR = os.path.join(INPUT_BASE_DIR,
                                  "mescog", "proj_wmh_patterns")
 INPUT_DATASET = os.path.join(INPUT_DATASET_DIR,
                              "train.std.npy")
-#INPUT_SUBJECTS_DIR = os.path.join(INPUT_BASE_DIR,
-#                                  "mescog", "datasets")
-#INPUT_SUBJECTS = os.path.join(INPUT_SUBJECTS_DIR,
-#                              "CAD-WMH-MNI-subjects.without_outliers.txt")
+INPUT_TRAIN_SUBJECTS = os.path.join(INPUT_DATASET_DIR,
+                                    "train_subjects.txt")
 INPUT_MASK = os.path.join(INPUT_DATASET_DIR, "wmh_mask.nii")
 
 OUTPUT_BASE_DIR = "/neurospin/"
@@ -45,7 +43,8 @@ if not os.path.exists(OUTPUT_DIR):
 
 OUTPUT_DISTANCE_MATRIX = os.path.join(OUTPUT_DIR, "distance_matrix.npy")
 OUTPUT_DISTANCE_MATRIX_IMG = os.path.join(OUTPUT_DIR, "distance_matrix.png")
-OUTPUT_DISTANCE_MATRIX_BOXPLOT = os.path.join(OUTPUT_DIR, "distance_matrix.boxplot.png")
+OUTPUT_DISTANCE_MATRIX_BOXPLOT = os.path.join(OUTPUT_DIR,
+                                              "distance_matrix.boxplot.png")
 OUTPUT_LINKAGE = os.path.join(OUTPUT_DIR, "dendrogram.npy")
 OUTPUT_FULL_DENDROGRAM = os.path.join(OUTPUT_DIR, "dendrogram.full.svg")
 OUTPUT_LASTP_DENDROGRAM = os.path.join(OUTPUT_DIR, "dendrogram.lastp.svg")
@@ -68,6 +67,10 @@ n_clusters = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 X = np.load(INPUT_DATASET)
 n, p = s = X.shape
 print "Data loaded {s}".format(s=s)
+
+# Read subjects ID
+with open(INPUT_TRAIN_SUBJECTS) as f:
+    TRAIN_SUBJECTS_ID = np.array([int(l) for l in f.readlines()])
 
 # Read mask
 babel_mask = nibabel.load(INPUT_MASK)
@@ -96,14 +99,16 @@ np.save(OUTPUT_LINKAGE, Z)
 # Save dendrogram
 dendrogram_full_fig = plt.figure()
 R = scipy.cluster.hierarchy.dendrogram(Z,
-           color_threshold=1,
-           distance_sort='ascending')
+                                       color_threshold=1,
+                                       distance_sort='ascending',
+                                       labels=TRAIN_SUBJECTS_ID)
 dendrogram_full_fig.savefig(OUTPUT_FULL_DENDROGRAM)
 dendrogram_lastp_fig = plt.figure()
 R = scipy.cluster.hierarchy.dendrogram(Z,
-           color_threshold=1,
-           truncate_mode='lastp',
-           distance_sort='ascending')
+                                       color_threshold=1,
+                                       truncate_mode='lastp',
+                                       distance_sort='ascending',
+                                       labels=TRAIN_SUBJECTS_ID)
 dendrogram_lastp_fig.savefig(OUTPUT_LASTP_DENDROGRAM)
 
 # Cluster data
