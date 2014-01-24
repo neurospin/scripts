dd43aefb2047afdabf48f45480166538e1d10fc3
duchesnay <edouard.duchesnay@gmail.com>
2014-01-24 12:49:49 +0100

    mescog

--- 2013_mescog/proj_wmh_patterns/02_mulm_plot_PCA.R
+++ 2013_mescog/proj_wmh_patterns/02_mulm_plot_PCA.R
@@ -1,11 +1,12 @@
 WD="/neurospin/mescog/proj_wmh_patterns/PCA"
 setwd(WD)
-OUTPUT_CSV = "pc_learn_fr_x_clinic_mulm.csv"
+OUTPUT_MULM = "mulm_PCs_vs_clinic"
+OUTPUT_FIG_PCs_vs_clinic = "PCs_vs_clinic"
 
 pc = read.csv("pc_learn_fr.csv")
 clin = read.csv("dataset_clinic_niglob_20140121.csv")
 clin$ID = as.integer(sub("CAD_", "", clin$ID))
-D = merge(clin, pc, by=c("ID", "Site"), all=TRUE)
+D = merge(clin, pc, by=c("ID", "SITE"), all=TRUE)
 
 D$TMTB_TIME_DELTA = D$TMTB_TIME.M36 - D$TMTB_TIME
 D$MDRS_TOTAL_DELTA = D$MDRS_TOTAL.M36 - D$MDRS_TOTAL
@@ -16,8 +17,10 @@ D$LLV_DELTA = D$LLV.M36 - D$LLV
 D$LLcount_DELTA = D$LLcount.M36 - D$LLcount
 D$MBcount_DELTA = D$MBcount.M36 - D$MBcount
 
+levels(D$SITE) = c("FR", "GE")
+
 Dfr = D[D$SITE == "FR", ]
-DGE = D[D$SITE == "GR", ]
+DGE = D[D$SITE == "GE", ]
 
 TARGETS = c(
 "TMTB_TIME_DELTA", "TMTB_TIME.M36" ,"TMTB_TIME",
@@ -55,7 +58,80 @@ for(TARGET in TARGETS){
 rownames(RESULTS) = 1:(nrow(RESULTS))
 RESULTS = RESULTS[order(RESULTS$pval), ]
 
-write.csv(RESULTS, OUTPUT_CSV, row.names=FALSE)
+write.csv(RESULTS, OUTPUT_MULM, row.names=FALSE)
 
 stat = RESULTS[RESULTS$PC != 1 & RESULTS$pval < 0.05,]
 stat = stat[order(stat$pval),]
+
+
+##########################################################################################################################
+TARGETS = c(
+"TMTB_TIME",
+ #"TMTB_TIME_DELTA", "TMTB_TIME.M36" ,
+ "MDRS_TOTAL",
+#"MDRS_TOTAL_DELTA", "MDRS_TOTAL.M36",
+  "MRS",
+ #"MRS_DELTA", "MRS.M36",
+  #"BARTHEL_DELTA", "BARTHEL.M36", "BARTHEL",
+  "MMSE",
+  #"MMSE_DELTA", "MMSE.M36", 
+  "AGE_AT_INCLUSION", "SEX", "EDUCATION",
+ "LLcount", #"LLcount_DELTA", "LLcount.M36",
+  "LLV", #"LLV.M36", "LLV_DELTA", "LLVn"
+ "WMHV",#"WMHVn",
+  "MBcount", #"MBcount.M36", "MBcount_DELTA"
+  "BPF"
+  #"BRAINVOL",
+)
+library(reshape)
+library(ggplot2)
+ 
+D2 = D[c("ID","SITE","PC1","PC2", "PC3", TARGETS)]
+Dm = melt(D2, id=c("ID","SITE","PC1","PC2","PC3"))
+
+svg(paste(OUTPUT_FIG_PCs_vs_clinic, "PC1.svg", sep="_"), width = 10, height = 7)
+p = ggplot(Dm, aes(x=PC1, y=value)) + geom_point(aes(colour=SITE), alpha=.5, position = "jitter")  +  facet_wrap(~variable, scales="free") + scale_color_manual(values=c("blue", "red"))
+p = p + stat_smooth(formula=y~x, method="lm", aes(colour=SITE))
+print(p)
+
+svg(paste(OUTPUT_FIG_PCs_vs_clinic, "PC2.svg", sep="_"), width = 10, height = 7)
+p = ggplot(Dm, aes(x=PC2, y=value)) + geom_point(aes(colour=SITE), alpha=.5, position = "jitter")  +  facet_wrap(~variable, scales="free") + scale_color_manual(values=c("blue", "red"))
+p = p + stat_smooth(formula=y~x, method="lm", aes(colour=SITE))
+print(p)
+dev.off()
+
+svg(paste(OUTPUT_FIG_PCs_vs_clinic, "PC3.svg", sep="_"), width = 10, height = 7)
+p = ggplot(Dm, aes(x=PC3, y=value)) + geom_point(aes(colour=SITE), alpha=.5, position = "jitter")  +  facet_wrap(~variable, scales="free") + scale_color_manual(values=c("blue", "red"))
+p = p + stat_smooth(formula=y~x, method="lm", aes(colour=SITE))
+print(p)
+dev.off()
+
+TARGETS = c(
+  "MRS.M36",
+"MBcount.M36"
+)
+D2 = D[c("ID","SITE","PC1","PC2", "PC3", TARGETS)]
+Dm = melt(D2, id=c("ID","SITE","PC1","PC2","PC3"))
+
+svg("PC2_vs_MRSM36-MBcount.36.svg", width = 7, height = 4)
+p = ggplot(Dm, aes(x=PC2, y=value)) + geom_point(aes(colour=SITE), alpha=.5, position = "jitter")  +  facet_wrap(~variable, scales="free") + scale_color_manual(values=c("blue", "red"))
+p = p + stat_smooth(formula=y~x, method="lm", aes(colour=SITE))
+print(p)
+dev.off()
+
+
+TARGETS = c(
+  "MRS",
+  "MMSE", 
+  "MDRS_TOTAL"
+)
+D2 = D[c("ID","SITE","PC1","PC2", "PC3", TARGETS)]
+Dm = melt(D2, id=c("ID","SITE","PC1","PC2","PC3"))
+
+svg("PC3_vs_MRS-MMSE-MDRS_TOTAL.svg", width = 10.5, height = 4)
+p = ggplot(Dm, aes(x=PC3, y=value)) + geom_point(aes(colour=SITE), alpha=.5, position = "jitter")  +  facet_wrap(~variable, scales="free") + scale_color_manual(values=c("blue", "red"))
+p = p + stat_smooth(formula=y~x, method="lm", aes(colour=SITE))
+print(p)
+dev.off()
+
+
