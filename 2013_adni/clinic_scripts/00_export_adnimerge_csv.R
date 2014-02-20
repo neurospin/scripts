76afc1b754cf427037fd4e8459ecb57f4de8a9d6
Mathieu Dubois <mathieu.dubois@cea.fr>
2014-02-20 10:40:48 +0100

    WIP 2013_adni/clinic_scripts/00_export_adnimerge_csv.R: adapted to the new directory name.

--- 2013_adni/clinic_scripts/00_export_adnimerge_csv.R
+++ 2013_adni/clinic_scripts/00_export_adnimerge_csv.R
@@ -1,4 +1,4 @@
 require(ADNIMERGE)
-OUTPUT_DIR="/neurospin/brainomics/2013_adni_preprocessing/clinic"
+OUTPUT_DIR="/neurospin/brainomics/2013_adni/clinic"
 OUTPUT_PATH=file.path(OUTPUT_DIR, "adnimerge.csv")
-write.csv(adnimerge, OUTPUT_PATH)
+write.csv(adnimerge, OUTPUT_PATH, row.names=FALSE)
