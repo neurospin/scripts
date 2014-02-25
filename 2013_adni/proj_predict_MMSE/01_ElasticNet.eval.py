b0f5d324f40f6f4b0a93be5a311bbc14dd94b798
Mathieu Dubois <mathieu.dubois@cea.fr>
2014-02-25 17:41:10 +0100

    ENH 2013_adni/proj_predict_MMSE/01_ElasticNet.eval.py: refit a model with best parameters and create figures and CSV for r_squared.

--- 2013_adni/proj_predict_MMSE/01_ElasticNet.eval.py
+++ 2013_adni/proj_predict_MMSE/01_ElasticNet.eval.py
@@ -7,10 +7,12 @@ Created on Mon Feb 24 15:44:49 2014
 
 
 import os
+import itertools
 
 import pickle
 
 import numpy as np
+import matplotlib.pylab as plt
 import pandas as pd
 
 import sklearn.cross_validation
@@ -39,6 +41,8 @@ INPUT_MASK_FILE= os.path.join(INPUT_MASK_PATH,
 OUTPUT_PATH = INPUT_PATH
 
 OUTPUT_RSQUARED = os.path.join(OUTPUT_PATH, "r_squared.npy")
+OUTPUT_RSQUARED_CSV = os.path.join(OUTPUT_PATH, "r_squared.csv")
+OUTPUT_RSQUARED_FIG = os.path.join(OUTPUT_PATH, "r_squared.pdf")
 OUTPUT_OPT_L1_RATIO = os.path.join(OUTPUT_PATH, "opt_l1_ratio.npy")
 OUTPUT_ALL_GLOBAL_PENALIZATION = os.path.join(OUTPUT_PATH, "alphas.npy")
 # Will be used for TV penalized
@@ -109,9 +113,36 @@ np.save(OUTPUT_RSQUARED, r_squared)
 # Find the optimal parameters
 max_index = r_squared.argmax()
 max_pos = np.unravel_index(max_index, r_squared.shape)
-print l1_ratios[max_pos[0]], alphas[max_pos[1]]
-np.save(OUTPUT_OPT_L1_RATIO, l1_ratios[max_pos[0]])
-np.save(OUTPUT_OPT_GLOBAL_PENALIZATION, alphas[max_pos[1]])
+opt_l1_ratio = l1_ratios[max_pos[0]]
+opt_alpha = alphas[max_pos[1]]
+print "Best parameters:", opt_l1_ratio, opt_alpha
+np.save(OUTPUT_OPT_L1_RATIO, opt_l1_ratio)
+np.save(OUTPUT_OPT_GLOBAL_PENALIZATION, opt_alpha)
+is_opt = np.zeros(r_squared.shape, dtype=bool)
+is_opt[max_pos[0], max_pos[1]] = True
+
+# Create a panda dataframe & export to CSV
+index = pd.MultiIndex.from_tuples(list(itertools.product(l1_ratios, alphas)),
+                                  names=['l1_ratio', 'alpha'])
+r_squared_df = pd.DataFrame.from_items(
+    [('r_squared', r_squared.ravel()),
+     ('opt', is_opt.ravel())])
+r_squared_df.index = index
+r_squared_df.to_csv(OUTPUT_RSQUARED_CSV,
+                    header=True)
+
+# Display a plot of the r_squared
+fig = plt.figure()
+plt.rc('text', usetex=True)
+plt.matshow(r_squared, interpolation='none')
+plt.xticks(np.arange(0, len(alphas)),
+           ["${0:.3f}$".format(alpha) for alpha in alphas])
+plt.yticks(np.arange(0, len(l1_ratios)),
+           ["${0:.2f}$".format(l1_ratio) for l1_ratio in l1_ratios])
+plt.xlabel(r'$\ell_1$ ratio')
+plt.ylabel(r'$\alpha$')
+plt.colorbar()
+plt.savefig(OUTPUT_RSQUARED_FIG)
 
 #####################
 # Create beta image #
@@ -144,3 +175,26 @@ for l1_index, l1_ratio in enumerate(l1_ratios):
                                          affine=babel_mask.get_affine(),
                                          header=babel_mask.get_header().copy())
             im_out.to_filename(os.path.join(model_dir, "beta.nii"))
+
+#########################################################
+# Refit a model with optimal parameters on all the data #
+#########################################################
+
+print "Fitting model with optimal parameters"
+opt_model = sklearn.linear_model.ElasticNet(alpha=opt_alpha,
+                                            l1_ratio=opt_l1_ratio,
+                                            fit_intercept=False)
+opt_model.fit(X, y)
+with open(os.path.join(OUTPUT_PATH, "opt_model.pkl"), "w") as f:
+    pickle.dump(opt_model, f)
+arr = np.zeros(mask.shape)
+arr[binary_mask] = opt_model.coef_
+im_out = nibabel.Nifti1Image(arr,
+                             affine=babel_mask.get_affine(),
+                             header=babel_mask.get_header().copy())
+im_out.to_filename(os.path.join(OUTPUT_PATH, "opt_beta.nii"))
+y_pred = opt_model.predict(X)
+np.save(os.path.join(OUTPUT_PATH, "opt_beta.nii"), y_pred)
+opt_r_squared = sklearn.metrics.r2_score(y, y_pred)
+np.save(os.path.join(OUTPUT_PATH, "opt_r_squared.npy"),
+        opt_r_squared)
