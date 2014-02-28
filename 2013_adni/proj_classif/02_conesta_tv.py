2c1b42a6e48ac6bc15a8cdc60ba150e906210bd5
duchesnay <edouard.duchesnay@gmail.com>
2014-02-28 19:07:10 +0100

    ADNI classif AD

--- 2013_adni/proj_classif/02_conesta_tv.py
+++ 2013_adni/proj_classif/02_conesta_tv.py
@@ -30,6 +30,7 @@ from parsimony.utils import plot_map2d
 
 ## GLOBALS ==================================================================
 BASE_PATH = "/neurospin/brainomics/2013_adni/proj_classif"
+OUTPUT_PATH = os.path.join(BASE_PATH, "tv")
 INPUT_X_TRAIN_CENTER_FILE = os.path.join(BASE_PATH, "X_CTL_AD.train.center.npy")
 INPUT_X_TEST_CENTER_FILE = os.path.join(BASE_PATH, "X_CTL_AD.test.center.npy")
 INPUT_Y_TRAIN_FILE = os.path.join(BASE_PATH, "y_CTL_AD.train.npy")
@@ -39,20 +40,24 @@ INPUT_MASK_PATH = os.path.join(BASE_PATH,
                                "template_FinalQC_CTL_AD")
 INPUT_MASK = os.path.join(INPUT_MASK_PATH,
                               "mask.nii")
-mask_im = nibabel.load(INPUT_MASK)
-mask = mask_im.get_data() != 0
-A, n_compacts = tv.A_from_mask(mask)
 
 SRC_PATH = os.path.join(os.environ["HOME"], "git", "scripts", "2013_adni", "proj_classif")
 sys.path.append(SRC_PATH)
 import utils_proj_classif
 
-## ARGS ===================================================================
+
 MODE = "split"
-ratio_k, ratio_l, ratio_g = .1, .1, .8
+
+
+
+## ARGS ===================================================================
+#ratio_k, ratio_l, ratio_g = .1, .1, .8
 #ALPHAS = [100, 10, 1, .1]
-ALPHAS = [100, 50, 10, 5, 1, 0.5, 0.1]
-ALPHAS = " ".join([str(a) for a in ALPHAS])
+#ALPHAS = [5, 1, 0.5, 0.1]
+#ALPHAS = " ".join([str(a) for a in ALPHAS])
+ALPHAS = "5 1 0.5 0.1"
+RATIOS_LIST = "0.1 0.1 0.8; 0.0 0.2 0.8; 0.2 0.0 0.8"
+NBCORES = 8
 
 parser = optparse.OptionParser(description=__doc__)
 parser.add_option('--mode',
@@ -60,34 +65,31 @@ parser.add_option('--mode',
     "all" (train=train+test), "reduce" (default %s)' % MODE, default=MODE, type=str)
 parser.add_option('--alphas',
     help='alphas values (default %s)' % ALPHAS, default=ALPHAS, type=str)
-parser.add_option('--ratio_k',
-    help='l2 penalty ratio (default %f)' % ratio_k, default=ratio_k, type=float)
-parser.add_option('--ratio_l',
-    help='l1 penalty ratio (default %f)' % ratio_l, default=ratio_l, type=float)
-parser.add_option('--ratio_g',
-    help='tv penalty ratio (default %f)' % ratio_g, default=ratio_g, type=float)
+parser.add_option('--cores',
+    help='Nb cpu cores to use (default %i)' % NBCORES, default=NBCORES, type=int)
+parser.add_option('--ratios',
+    help='Ratio triplet separated by ";" (default %s)' % RATIOS_LIST, default=RATIOS_LIST, type=str)
+
 
 options, args = parser.parse_args(sys.argv)
 
 MODE = options.mode
 ALPHAS  = options.alphas
+RATIOS_LIST = options.ratios
 ALPHAS = [float(a) for a in ALPHAS.split()]
-ratio_k = options.ratio_k
-ratio_l = options.ratio_l
-ratio_g = options.ratio_g
+RATIOS_LIST = [[float(ratio) for ratio in ratios.split()]  for ratios in RATIOS_LIST.split(";")]
+PARAMS_LIST = [[alpha]+ratios for ratios in RATIOS_LIST for alpha in ALPHAS]
+NBCORES = options.cores
 
-#print MODE, ALPHAS, ratio_k, ratio_l, ratio_g
-OUTPUT_PATH = os.path.join(BASE_PATH, "tv")
-
-
+#print MODE, PARAMS_LIST, NBCORES
 
 ##############
-## Load data #
+## Load data
 ##############
 if MODE == "simu":
     OUTPUT_PATH = os.path.join(OUTPUT_PATH, "simu")
     n_samples = 500
-    shape = (500, 500, 1)
+    shape = (10, 10, 1)
     X3d, y, beta3d, proba = make_classification_struct(n_samples=n_samples,
             shape=shape, snr=5, random_seed=1)
     X = X3d.reshape((n_samples, np.prod(beta3d.shape)))
@@ -105,6 +107,8 @@ else:
     Xte = np.load(INPUT_X_TEST_CENTER_FILE)
     ytr = np.load(INPUT_Y_TRAIN_FILE)[:, np.newaxis]
     yte = np.load(INPUT_Y_TEST_FILE)[:, np.newaxis]
+    mask_im = nibabel.load(INPUT_MASK)
+    mask = mask_im.get_data() != 0
     A, n_compacts = tv.A_from_mask(mask)
     
 if MODE == "split":
@@ -122,14 +126,14 @@ weigths[ytr==1] = prop[1]
 
 
 ###############################
-# Iterate over Hyper-parameters
+# Mapper
 ###############################
 
-def mapper(alpha):
+def mapper(params):
+    alpha, ratio_k, ratio_l, ratio_g = params
     out_dir = os.path.join(OUTPUT_PATH,
                  "-".join([str(v) for v in (alpha, ratio_k, ratio_l, ratio_g)]))
     print "START:", out_dir
-    #alpha = 10
     np.asarray([np.sum(ytr == l) for l in np.unique(ytr)]) / float(ytr.size)
     time_curr = time.time()
     beta = None
@@ -144,23 +148,27 @@ def mapper(alpha):
     #if not os.path.exists(out_dir):
     #    os.makedirs(out_dir)
     time_curr = time.time()
+    tv.function = tv.A = None # Save disk space
     utils_proj_classif.save_model(out_dir, tv, beta, mask_im,
                                   y_pred_tv=y_pred_tv,
                                   y_true=yte)
 
+###############################
+# Execution
+###############################
 
-p = Pool(len(ALPHAS))
+print PARAMS_LIST
+p = Pool(NBCORES)
+p.map(mapper, PARAMS_LIST)
 
-p.map(mapper, ALPHAS)
-#Parallel(n_jobs=len(ALPHAS), verbose=True)(
-#    delayed(mapper) (alpha, ratio_k, ratio_l, ratio_g)
-#    for alpha in ALPHAS)
 
 #########################
 # Result: reduce
 #########################
 
 if MODE == "reduce":
+    #print MODE, ALPHAS, ratio_k, ratio_l, ratio_g
+    OUTPUT_PATH = os.path.join(BASE_PATH, "tv", MODE)
     y = dict()
     recall_tot = dict()
     models = dict()
@@ -170,14 +178,31 @@ if MODE == "reduce":
         key = os.path.basename(rep)
         print rep
         res = utils_proj_classif.load(rep)
+        mod = res['model']
+        #self.function = None
+        #self.A = None
+        #import pickle
+        #pickle.dump(self, open(os.path.join(rep, "model.pkl"), "w"))
         #rep = '/neurospin/brainomics/2013_adni/proj_predict_MMSE/tv/cv/2/100-0.1-0.4-0.5'
+        # BUG CORRECT START
+        arr = np.zeros(mask.shape)
+        arr[mask] = mod.beta.ravel()
+        im_out = nibabel.Nifti1Image(arr, affine=mask_im.get_affine())#, header=mask_im.get_header().copy())
+        im_out.to_filename(os.path.join(rep,"beta.nii"))
+        print np.all(nibabel.load(os.path.join(rep,"beta.nii")).get_data()[mask] == mod.beta.ravel())
+        # BUG CORRECT END
         y_pred = res["y_pred_tv"].ravel()
         y_true = res["y_true"].ravel()
         y[key] = dict(y_true=y_true, y_pred=y_pred)
         _, r, f, _ = precision_recall_fscore_support(y_true, y_pred, average=None)
         recall_tot[key] = r
         models[key] = res['model']
-    for k in recall_tot: print k, "\t", recall_tot[k]
+    r =list()
+    for k in recall_tot: r.append(k.split("-")+recall_tot[k].tolist())
+    import pandas as pd
+    res = pd.DataFrame(r, columns=["alpha", "l2_ratio", "l1_ratio", "tv_ratio",  "recall_0", "recall_1"])
+    res = res.sort("recall_1", ascending=False)
+    print res.to_string()
     #[m.weigths[:2] for m in models.values()]
     #key = '1-0.1-0.4-0.5'
     key = '0.1-0.1-0.4-0.5'
@@ -197,23 +222,12 @@ if MODE == "reduce":
         plot_map2d(tv.beta.reshape(shape), title=title, limits=[beta3d.min(), beta3d.max()])
         plt.show()
 
-# python 02_conesta_tv.py --alphas="10 5 1 0.5 0.1 0.05" --ratio_k=0.25 --ratio_l=0.1 --ratio_g=0.65
+# TV 0.9 => 0.6
+#python 02_conesta_tv.py --cores=8 --ratios="0.05 0.05 0.9; 0.0 0.1 0.9; 0.1 0.0 0.9; 0.1 0.1 0.8; 0.0 0.2 0.8; 0.2 0.0 0.8; 0.2 0.2 0.6; 0.4 0.0 0.6; 0.0 0.4 0.6"
+# TV 0.95 et 0.5
+#python 02_conesta_tv.py --cores=8 --ratios="0.025 0.025 0.9; 0.0 0.05 0.95; 0.05 0.0 0.9; 0.25 0.25 0.5; 0.5 0.0 0.5; 0.0 0.5 0.5
 
-
-
-#0.1-0.1-0.4-0.5 	[ 0.78947368  0.58064516]
-#50-0.1-0.1-0.8 	[ 0.31578947  0.75806452]
-#100-0.1-0.4-0.5 	[ 0.  1.]
-#10-0.1-0.4-0.5 	[ 0.71052632  0.41935484]
-#5-0.1-0.4-0.5 	[ 0.5         0.58064516]
-#50-0.1-0.4-0.5 	[ 0.  1.]
-#1-0.1-0.4-0.5 	[ 0.76315789  0.53225806]
-#0.5-0.1-0.4-0.5 	[ 0.78947368  0.58064516]
-#10-0.1-0.1-0.8 	[ 0.46052632  0.61290323]
-#5-0.1-0.1-0.8 	[ 0.63157895  0.41935484]
-#100-0.1-0.1-0.8 	[ 0.  1.]
-
-
+# Execution time
 #/neurospin/brainomics/2013_adni/proj_classif/tv/split/50-0.1-0.1-0.8 Time ellapsed: 1357.92806792 ite:1289, time:479.500000
 #/neurospin/brainomics/2013_adni/proj_classif/tv/split/10-0.1-0.1-0.8 Time ellapsed: 6142.82701111 ite:11019, time:3397.590000
 #/neurospin/brainomics/2013_adni/proj_classif/tv/split/5-0.1-0.1-0.8 Time ellapsed: 6218.87577105 ite:11401, time:3501.290000
