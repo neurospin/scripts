966448a44829fe3b22d06d904fe5fc2a1ca97bba
Mathieu Dubois <mathieu.dubois@cea.fr>
2014-06-03 18:54:24 +0200

    WIP 2014_pca_struct/dice5/01_struct_pca_l1.py: updated to the new protocol and changed parameters.

--- 2014_pca_struct/dice5/01_struct_pca_l1.py
+++ 2014_pca_struct/dice5/01_struct_pca_l1.py
@@ -5,7 +5,7 @@ Created on Wed May 28 12:08:39 2014
 @author: md238665
 
 Process dice5 datasets with our structured PCA.
-We use very few TV here.
+We use several values for TV.
 
 We generate a map_reduce configuration file for each dataset.
 
@@ -28,13 +28,12 @@ import dice5_pca
 INPUT_BASE_DIR = "/neurospin/brainomics/2014_pca_struct/dice5"
 INPUT_DIR = os.path.join(INPUT_BASE_DIR, "data")
 INPUT_STD_DATASET_FILE_FORMAT = "data_{alpha}.std.npy"
-INPUT_INDEX_FILE_FORMAT = "{subset}_indices.npy"
+INPUT_INDEX_FILE_FORMAT = "indices_{subset}.npy"
 INPUT_OBJECT_MASK_FILE_FORMAT = "mask_{o}.npy"
 
 INPUT_SHAPE = (100, 100, 1)
+INPUT_N_SUBSETS = 2
 INPUT_ALPHAS = np.linspace(0.1, 1, num=10)
-INPUT_TRAIN_INDICES = os.path.join(INPUT_DIR, "train_indices.npy")
-INPUT_TEST_INDICES = os.path.join(INPUT_DIR, "test_indices.npy")
 INPUT_BETA_FILE_FORMAT = "beta3d_{alpha}.std.npy"
 
 OUTPUT_DIR = os.path.join(INPUT_BASE_DIR, "struct_pca")
@@ -48,29 +47,43 @@ OUTPUT_DATASET_DIR = "{alpha}"
 
 N_COMP = 3
 # Global penalty
-STRUCTPCA_ALPHA = np.array([1, 5, 10])
+STRUCTPCA_ALPHA = [[1e-1], [5e-1], [1], [5], [10]]
 # Relative penalties
-STRUCTPCA_L1L2TV = np.array([0.1, 1e-3, 1e-6])
+STRUCTPCA_L1L2TV = [np.array([1, 1e-9, 1e-3]),
+                    np.array([1, 1e-9, 1e-2]),
+                    np.array([1, 1e-9, 1e-1])]
 
 #############
 # Functions #
 #############
 
-#def A_from_structure(structure_filepath):
-#
+def load_globals(config):
+    pass
+
+
+def resample(config, resample_nb):
+    import mapreduce as GLOBAL  # access to global variables
+    GLOBAL.DATA = GLOBAL.load_data(config["data"])
+    resample = config["resample"][resample_nb]
+    GLOBAL.DATA_RESAMPLED = {k: [GLOBAL.DATA[k][idx, ...] for idx in resample]
+                            for k in GLOBAL.DATA}
+
 
 def mapper(key, output_collector):
+    global INPUT_OBJECT_MASK_FILE_FORMAT, INPUT_BETA_FILE_FORMAT, N_COMP
     import mapreduce as GLOBAL # access to global variables:
     # GLOBAL.DATA
     # GLOBAL.DATA ::= {"X":[Xtrain, ytrain], "y":[Xtest, ytest]}
     # key: list of parameters
     ll1, ll2, ltv = key
-    # A matrices
-    X_train = GLOBAL.DATA["X"][0]
+
+    X_train = GLOBAL.DATA_RESAMPLED["X"][0]
     n, p = shape = X_train.shape
-    #print "X_train", shape
+
+    # A matrices
     Atv, n_compacts = parsimony.functions.nesterov.tv.A_from_shape(INPUT_SHAPE)
     Al1 = scipy.sparse.eye(p, p)
+
     model = pca_tv.PCA_SmoothedL1_L2_TV(n_components=N_COMP,
                                         l1=ll1, l2=ll2, ltv=ltv,
                                         Atv=Atv,
@@ -82,61 +95,86 @@ def mapper(key, output_collector):
     t0 = time.clock()
     model.fit(X_train)
     t1 = time.clock()
-    #print "X_test", GLOBAL.DATA["X"][1].shape
-    X_transform = model.transform(GLOBAL.DATA["X"][1])
-    ret = dict(X_train=X_train,
-               X_transform=X_transform,
-               model=model,
-               time=t1-t0)
-    output_collector.collect(key, ret)
 
-def reducer(key, values):
-    # key : string of intermediary key
-    # load return dict corresponding to mapper ouput. they need to be loaded.
-
-    values = [item.load() for item in values]
-    model = values[0]["model"]
+    # Transform data
+    X_test = GLOBAL.DATA_RESAMPLED["X"][1]
+    X_transform = model.transform(X_test)
 
     # Read masks & compute geometric metrics
-    V = model.V
-    global INPUT_OBJECT_MASK_FILE_FORMAT, INPUT_BETA_FILE_FORMAT
-    recall = []
-    precision = []
-    corr = []
-    for i in range(3):
+    V = model.components_.T
+    recall = np.zeros((N_COMP,))
+    precision = np.zeros((N_COMP,))
+    fscore = np.zeros((N_COMP,))
+    for i in range(N_COMP):
         filename = INPUT_OBJECT_MASK_FILE_FORMAT.format(o=i)
         full_filename = os.path.join(INPUT_DIR, filename)
         print full_filename
         mask = np.load(full_filename)
         #masks.append(mask)
         res = dice5_pca.dice_five_geometric_metrics(mask, V[:, i])
-        recall.append(res[0])
-        precision.append(res[1])
-        corr.append(res[2])
+        recall[i] = res[0]
+        precision[i] = res[1]
+        fscore[i] = res[2]
 
-    # Percentage of explained variance
-    X_train = values[0]["X_train"]
-    d2 = model.d**2
-    variance = np.sum(X_train**2)
-    evr2 = d2.cumsum()/variance
+    evr = np.zeros((N_COMP,))
+    for i in range(N_COMP):
+        # i first components
+        Vi = V[:, range(i+1)]
+        try:
+            evr[i-1] = dice5_pca.ratio_explained_variance(X_train, Vi)
+        except:
+            evr[i-1] = np.nan
 
-    evr  = []
-    for i in range(1,4):
-        evr.append(dice5_pca.ratio_explained_variance(X_train, V[:, 0:i]))
+    ret = dict(X_transform=X_transform,
+               model=model,
+               recall=recall,
+               precision=precision,
+               fscore=fscore,
+               evr=evr,
+               time=t1-t0)
 
-    time = values[0]["time"]
+    output_collector.collect(key, ret)
+
+
+def reducer(key, values):
+    global N_COMP
+    # key : string of intermediary key
+    # load return dict corresponding to mapper ouput. they need to be loaded.]
+    values = [item.load() for item in values]
+
+    models = [item["model"] for item in values]
+    precisions = np.vstack([item["precision"] for item in values])
+    recalls = np.vstack([item["recall"] for item in values])
+    fscores = np.vstack([item["fscore"] for item in values])
+    evr = np.vstack([item["evr"] for item in values])
+    times = [item["time"] for item in values]
+
+    # Average precision/recall across folds for each group
+    av_precision = precisions.mean(axis=0)
+    av_recall = recalls.mean(axis=0)
+    av_fscore = fscores.mean(axis=0)
+    av_evr = evr.mean(axis=0)
+
+    # Compute correlations of components between folds
+    correlation = np.zeros((N_COMP,))
+    comp0 = models[0].components_.T
+    comp1 = models[1].components_.T
+    for i in range(N_COMP):
+        correlation[i] = dice5_pca.abs_correlation(comp0[:, i], comp1[:, i])
+
     scores = dict(key=key,
-                  recall_0=recall[0], recall_1=recall[1],
-                  recall_2=recall[2], recall_mean=np.mean(recall),
-                  precision_0=precision[0], precision_1=precision[1],
-                  precision_2=precision[2], precision_mean=np.mean(precision),
-                  corr_0=corr[0], corr_1=corr[1],
-                  corr_2=corr[2], corr_mean=np.mean(corr),
-                  evr_witten_0=evr[0], evr_witten_1=evr[1],
-                  evr_witten_2=evr[2],
-                  evr_0=evr2[0],evr_1=evr2[1],
-                  evr_2=evr2[2],
-                  time=time)
+                  recall_0=av_recall[0], recall_1=av_recall[1],
+                  recall_2=av_recall[2], recall_mean=np.mean(av_recall),
+                  precision_0=av_precision[0], precision_1=av_precision[1],
+                  precision_2=av_precision[2], precision_mean=np.mean(av_precision),
+                  fscore_0=av_fscore[0], fscore_1=av_fscore[1],
+                  fscore_2=av_fscore[2], fscore_mean=np.mean(av_fscore),
+                  correlation_0=correlation[0], correlation_1=correlation[1],
+                  correlation_2=correlation[2], correlation_mean=np.mean(correlation),
+                  evr_0=av_evr[0], evr_1=av_evr[1],
+                  evr_2=av_evr[2],
+                  time=np.mean(times))
+
     return OrderedDict(sorted(scores.items(), key=lambda t: t[0]))
 
 #################
@@ -146,11 +184,17 @@ def reducer(key, values):
 if __name__ == '__main__':
 
     # Read indices
-    train_indices = np.load(INPUT_TRAIN_INDICES).tolist()
-    test_indices = np.load(INPUT_TEST_INDICES).tolist()
+    indices = []
+    for i in range(INPUT_N_SUBSETS):
+        filename = INPUT_INDEX_FILE_FORMAT.format(subset=i)
+        full_filename = os.path.join(INPUT_DIR, filename)
+        indices.append(np.load(full_filename).tolist())
+    rev_indices = indices[::-1]
 
     # Parameter grid
-    params = [(alpha * STRUCTPCA_L1L2TV).tolist() for alpha in STRUCTPCA_ALPHA]
+    params = [(alpha * rel_val).tolist()
+              for rel_val in STRUCTPCA_L1L2TV
+              for alpha in STRUCTPCA_ALPHA]
 
     # Create a mapreduce config file for each dataset
     for alpha in INPUT_ALPHAS:
@@ -169,7 +213,8 @@ if __name__ == '__main__':
 
         config = dict(data=dict(X=dataset_full_filename),
                       params=params,
-                      resample=[[train_indices, test_indices]],
+                      resample=[indices,
+                                rev_indices],
                       map_output=output_dir,
                       user_func=user_func_filename,
                       ncore=4,
@@ -178,4 +223,4 @@ if __name__ == '__main__':
                       reduce_output=os.path.join(output_dir, "results.csv"))
         config_full_filename = os.path.join(output_dir, "config.json")
         json.dump(config, open(config_full_filename, "w"))
-        print "mapreduce.py --mode map --config %s" % config_full_filename
+        print "mapreduce.py -m %s" % config_full_filename
