ebc613aaef82792ca5f34aef3c45459f280968fa
Helene Lajous <helene.lajous@cea.fr>
2014-06-13 14:43:01 +0200

    UPD: Evaluation of Enet algo for alpha equal to 1 with SL

--- 2013_imagen_bmi/scripts/test_Enet_SL.py
+++ 2013_imagen_bmi/scripts/test_Enet_SL.py
@@ -148,7 +148,7 @@ if __name__ == "__main__":
     ##   1) pathes
     NFOLDS = 5
     ## 2) cv index and parameters to test
-    cv = [[tr.tolist(), te.tolist()] for tr,te in KFold(n, n_folds=NFOLDS)]    
+    cv = [[tr.tolist(), te.tolist()] for tr,te in KFold(n, n_folds=NFOLDS, shuffle=True, random_state=2505)]    
     #params = [[alpha, l1_ratio] for alpha in np.arange(1., 5.5, 0.5) for l1_ratio in np.arange(0.1, 1., .2)]
     alpha = 1
     params = [[alpha, l1_ratio] for l1_ratio in np.arange(0.1, 1., .2)]
