3fcaab9e864cd1e27d823dda0efaa3ad4b348a5f
Christophe Launay <crislaunay@gmail.com>
2014-07-21 16:42:51 +0200

    WIP : 2014_bd_dwi build dataset and population

--- 2014_bd_dwi/00_build_dataset.py
+++ 2014_bd_dwi/00_build_dataset.py
@@ -8,21 +8,22 @@ import os
 import nibabel as nib
 import numpy as np
 import pandas as pd
- 
+
 #loading files
 BASE_PATH =  "/volatile/share/2014_bd_dwi"
 
 #BASE_PATH =  "/volatile/share/2014_bd_dwi/all_FA/nii/stats"
- 
- 
-INPUT_TBSS = os.path.join(BASE_PATH, "clinic", "filelist_tbss.txt")
+
+
+INPUT_IMAGE_INDEX = os.path.join(BASE_PATH, "clinic", "image_list.txt")
 INPUT_IMAGE = os.path.join(BASE_PATH, "all_FA/nii/stats/all_FA.nii.gz")
 INPUT_CSV_BASE = os.path.join(BASE_PATH, "clinic", "BD_clinic.csv")
-INPUT_CSV_NEW = os.path.join(BASE_PATH, "bd_dwi_enettv", "population.csv")
 OUTPUT = os.path.join(BASE_PATH, "bd_dwi_enettv")
 if not os.path.exists(OUTPUT):
     os.makedirs(OUTPUT)
 OUTPUT_CSV = os.path.join(OUTPUT, "population.csv")
+OUTPUT_X = os.path.join(OUTPUT, "X.npy")
+OUTPUT_Y = os.path.join(OUTPUT, "Y.npy")
 
 MASK_FILENAME =  os.path.join(BASE_PATH, "all_FA/nii/stats/mask.nii.gz") #mask use to filtrate our images
 FA_THRESHOLD = 0.05
@@ -32,9 +33,10 @@ ATLAS_LABELS_RM = [0, 13, 2, 8]  # cortex, trunc
 
 #############################################################################
 ## Build population file
-tbss = open(INPUT_TBSS).readlines()
-tmp = list()
-for l in tbss:
+
+# Read image order and convert to match the ID in clinic file
+image_id = list()
+for l in open(INPUT_IMAGE_INDEX).readlines():
     l = l.replace("\n", "")
     l = l.replace("P_S_", "")
     if l.find("C_") == 0:
@@ -45,52 +47,55 @@ for l in tbss:
         l = l.replace("M_H_C6HK", "HK_0")
     if l.find("M_Z_") == 0:  # M_Z_103843 => MZ103843
         l = l.replace("M_Z_", "MZ")
-    tmp.append(l)
+    image_id.append(l)
 
-tbss = tmp
-clinic = pd.read_csv(INPUT_CSV_BASE)
+# Create a dataframe of image position
+images = pd.DataFrame(range(len(image_id)), columns=['SLICE'],
+                      index=image_id)
 
-tbss2 = None
-empty = pd.DataFrame([[0]*6], columns=["ID","BD_HC", "SCANNER", "AGEATMRI", "SEX", "with_clinic"])
-for ID in tbss: 
-    match = clinic.ID == ID
-    if np.sum(match) == 1:
-        t = clinic[match][["ID","BD_HC", "SCANNER", "AGEATMRI", "SEX"]]
-        t["with_clinic"] = 1
-    else:
-        t = empty.copy()
-        print "ID",ID, "matched ", np.sum(match), "clinic"
-    if tbss2 is None:
-        tbss2 = t
-    else:
-        tbss2 = tbss2.append(t, ignore_index=True)
+# Read some columns clinic data
+clinic = pd.read_csv(INPUT_CSV_BASE,
+                     index_col = 0)
+clinic = clinic[["BD_HC", "SCANNER", "AGEATMRI", "SEX"]]
 
+# Merge dataframes
+population = pd.merge(clinic, images,
+                      right_index = True,
+                      left_index = True)
+assert(population.shape[0] == 194)
 
-# ID which didnt match that we have to add to tbss2
+# Print ID of subjects for which clinic or image is missing
+in_clinic_not_in_image = clinic.index[~clinic.index.isin(images.index)]
+for ID in in_clinic_not_in_image:
+    print "ID", ID, "matched 0 images"
+assert(len(in_clinic_not_in_image)==10)
+#ID 204776 matched 0 images
+#ID 209861 matched 0 images
+#ID 211297 matched 0 images
+#ID 212485 matched 0 images
+#ID 213141 matched 0 images
+#ID 23012 matched 0 images
+#ID 27308 matched 0 images
+#ID 31108 matched 0 images
+#ID C_cn00018 matched 0 images
+#ID MZ150743 matched 0 images
 
-#ID C_gb100352 matched  0 clinic
-#ID C_pb090397 matched  0 clinic
-#ID 023012 matched  0 clinic
-#ID 027308 matched  0 clinic
-#ID 031108 matched  0 clinic
-#ID 214469 matched  0 clinic
+in_image_not_in_clinic = images.index[~images.index.isin(clinic.index)]
+for ID in in_image_not_in_clinic:
+    print "ID", ID, "matched 0 clinic"
+assert(len(in_image_not_in_clinic)==6)
+#ID 023012 matched 0 clinic
+#ID C_pb090397 matched 0 clinic
+#ID 031108 matched 0 clinic
+#ID C_gb100352 matched 0 clinic
+#ID 027308 matched 0 clinic
+#ID 214469 matched 0 clinic
 
+population.to_csv(OUTPUT_CSV)
 
-n_in_clinic_without_image = np.sum(np.logical_not(clinic.ID.isin(tbss)))
-
-print "n_tbss", len(tbss), "n_clinic", clinic.shape[0], \
-"n_in_clinic_without_image", n_in_clinic_without_image
-
-assert len(tbss2) == 200
-
-tbss2.to_csv(OUTPUT_CSV)
-
-# 
+#
 #############################################################################
 ## Build dataset
-
-image = list()
-
 
 ## Threshold FA mean map
 images4d = nib.load(INPUT_IMAGE)
@@ -105,7 +110,7 @@ babel_mask = nib.load(MASK_FILENAME)
 shape = image_arr.shape
 fa_mean = np.mean(image_arr, axis=3)
 mask = fa_mean > FA_THRESHOLD
-  
+
 # ATLAS, remove: cortex, trunc
 atlas = nib.load(ATLAS_FILENAME)
 assert np.all(images4d.get_affine() == atlas.get_affine())
@@ -115,66 +120,46 @@ for label_rm in ATLAS_LABELS_RM:
 #registration of mask
 out_im = nib.Nifti1Image(mask.astype(int), affine=images4d.get_affine())
 out_im.to_filename(MASK_FILENAME)
+# Check that the stored image is the same than mask
 babel_mask = nib.load(MASK_FILENAME)
 assert np.all(mask == (babel_mask.get_data() != 0))
 print mask.sum()
 
+n_voxel_in_mask = np.count_nonzero(mask)
+assert(n_voxel_in_mask == 507383)
+
 #application of mask on all the images
 
-i = 0
-subject = pd.read_csv(INPUT_CSV_NEW, header=0)
+n, _ = population.shape
 
-ntot = 0
+Ytot = np.asarray(population.BD_HC, dtype='float64').reshape(n, 1)
+Ztot = population[["AGEATMRI", "SEX"]].as_matrix()
 
-for i, SID in enumerate(subject["ID"]):
-    cur = subject.iloc[i]
-    if cur.with_clinic == 0:
-        i = i + 1
-    else:
-        ntot = ntot + 1
-        i = i + 1
-
-Ytot = np.zeros((ntot, 1))
-Xtot = np.zeros((ntot, 507383))
-
-i = 0
-k = 0
-
-Ztot = np.zeros((ntot, 2))
-
-while k < len(Ytot) and i < 194:
-    cur = subject.iloc[k]
-    print("k = ", k)
-    if cur.with_clinic == 0:
-        k = k + 1
-    else:
-        print("BD_HC = %i",cur.BD_HC)
-        image.append(image_arr[:, :, :, k].ravel())
-        Xtot[i, :] = np.vstack(image)[:, mask.ravel()]
-        del image[0]
-        Ytot[i] = cur.BD_HC
-        Ztot[:, :] = np.asarray(cur[["AGEATMRI", "SEX"]]).ravel()
-        i = i + 1
-        k = k + 1
+Xtot = np.zeros((n, n_voxel_in_mask))
+for i, ID in enumerate(population.index):
+    cur = population.iloc[i]
+    slice_index = cur.SLICE
+    image = image_arr[:, :, :, slice_index]
+    Xtot[i, :] = image[mask]
 
 ## sans le cs
 Xtot = np.hstack([Ztot, Xtot])
-assert Xtot.shape == (194, 507385)
+assert Xtot.shape == (n, n_voxel_in_mask+2)
 
 Xtot -= Xtot.mean(axis = 0)
 Xtot /= Xtot.std(axis = 0)
 
 
 n, p = Xtot.shape
-np.save(os.path.join(OUTPUT, "Xtot.npy"), Xtot)
-fh = open(os.path.join(OUTPUT, "Xtot.npy").replace("npy", "txt"), "w")
-fh.write('shape = (%i, %i): Intercept + Age + Gender + %i voxels' % \
+np.save(OUTPUT_X, Xtot)
+fh = open(os.path.join(OUTPUT, "X.npy").replace("npy", "txt"), "w")
+fh.write('shape = (%i, %i): Age + Gender + %i voxels' % \
     (n, p, mask.sum()))
 fh.close()
 
 
 
-np.save(os.path.join(OUTPUT, "Ytot.npy"), Ytot)
+np.save(OUTPUT_Y, Ytot)
 
 ## avec le cs
 
