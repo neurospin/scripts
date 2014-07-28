c57b08fe6957669207aec3c54349ca4117c47b45
duchesnay <edouard.duchesnay@gmail.com>
2014-07-28 13:49:35 +0200

    ADNI: predict MMSE MCIc vs CTL

---
+++ 2013_adni/MMSE-MCIc-CTL/00_create_population.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: edouard.duchesnay@cea.fr
+
+Creates a CSV file for the population.
+=> intersection of subject_list.txt and adnimerge_baseline.csv of in CTL or AD
+
+"""
+import os
+import numpy as np
+import pandas as pd
+
+#import proj_classif_config
+GROUP_MAP = {'CTL': 0, 'MCIc': 1}
+
+BASE_PATH = "/neurospin/brainomics/2013_adni"
+INPUT_CLINIC_FILENAME = os.path.join(BASE_PATH, "clinic", "adnimerge_baseline.csv")
+INPUT_SUBJECTS_LIST_FILENAME = os.path.join(BASE_PATH,
+                                   "templates",
+                                   "template_FinalQC",
+                                   "subject_list.txt")
+
+OUTPUT_CSV = os.path.join(BASE_PATH,
+                          "MMSE-MCIc-CTL",
+                          "population.csv")
+
+if not os.path.exists(os.path.dirname(OUTPUT_CSV)):
+        os.makedirs(os.path.dirname(OUTPUT_CSV))
+
+# Read clinic data
+clinic = pd.read_csv(INPUT_CLINIC_FILENAME)
+
+# Read subjects with image
+input_subjects = pd.read_table(INPUT_SUBJECTS_LIST_FILENAME, sep=" ",
+                               header=None)
+input_subjects = [x[:10] for x in input_subjects[1]]
+
+# intersect with subject with image
+clinic = clinic[clinic["PTID"].isin(input_subjects)]
+assert  clinic.shape == (456, 94)
+
+# Extract sub-population 
+# MCIc = MCI at bl converion to AD within 800 days
+TIME_TO_CONV = 800#365 * 2
+mcic_m = (clinic["DX.bl"] == "MCI") &\
+       (clinic.CONV_TO_AD < TIME_TO_CONV) &\
+       (clinic["DX.last"] == "AD") &\
+       (np.logical_not(clinic["MMSE.800days"].isnull()))
+assert np.sum(mcic_m) == 81
+
+mcic = clinic[mcic_m][['PTID', 'AGE', 'PTGENDER', "DX.bl", "MMSE.bl", "MMSE.800days"]]
+mcic["DX"] = "MCIc"
+print "Mean MMMSE decrease in MCIc=", np.mean(mcic["MMSE.bl"] - mcic["MMSE.800days"])
+# Mean MMMSE decrease in MCIc= 4.02469135802
+
+# CTL: CTL at bl no converion to AD
+ctl_m = (clinic["DX.bl"] == "CTL") &\
+        (clinic["DX.last"] == "CTL") &\
+        (np.logical_not(clinic["MMSE.800days"].isnull()))
+assert np.sum(ctl_m) == 120
+
+ctl = clinic[ctl_m][['PTID', 'AGE', 'PTGENDER', "DX.bl", "MMSE.bl", "MMSE.800days"]]
+ctl["DX"] = "CTL"
+print "Mean MMMSE decrease in CTL=", np.mean(ctl["MMSE.bl"] - ctl["MMSE.800days"])
+# Mean MMMSE decrease in CTL= 0.0416666666667
+
+pop = pd.concat([mcic, ctl])
+assert len(pop) == 201
+
+# Map group
+pop['DX.num'] = pop["DX"].map(GROUP_MAP)
+
+# Save population information
+pop.to_csv(OUTPUT_CSV, index=False)
