3071a65c12ac2c6d91fa3956faa81dd5ca8f4fa0
Helene Lajous <helene.lajous@cea.fr>
2014-08-06 14:43:39 +0200

    2013_imagen_bmi: useful tools for the project.

--- 2013_imagen_bmi/scripts/Tools/extract_BMI_SNPs.py
+++ 2013_imagen_bmi/scripts/Tools/extract_BMI_SNPs.py
@@ -5,30 +5,31 @@ Created on Mon Jun  2 13:43:10 2014
 @author: vf140245
 """
 
-import os, sys
+import os
+import sys
 
 sys.path.append(os.path.join('/home/vf140245', 'gits', 'mycaps/nsap/caps'))
 from genim.genibabel import connect, load_from_genes
 
 
-# List from graff paper Nature 2012
+# Main genes associated to BMI according to the literature
 # qualified name eGene NCBI
-bmi_gene_list = ['SEC16B', 'TNNI3K', 'PTBP2', 'NEGR1', 'LYPLAL1', 'LZTR2',
- 'LRP1B', 'TMEM18', 'POMC', 'FANCL', 'CADM2', 'SLC39A8', 'FLJ3577', 'HMGCR',
- 'NCR3', 'AIF1', 'BAT2', 'NUDT3', 'TFAP2B', 'MSRA', 'LRRN6C', 'LMX1B',
- 'BDNF', 'MTCH2', 'RPL27A', 'TUB', 'FAIM2', 'MTIF3', 'NRXN3', 'PRKD1',
- 'MAP2K5', 'GPRC5B', 'ADCY9', 'SH2B1', 'APOB48', 'FTO', 'MC4R', 'QPCTL',
- 'KCTD15', 'TMEM160', 'PRL', 'PTER', 'MAF', 'NPC1']
+bmi_gene_list = ['BDNF', 'CADM2', 'COL4A3BP', 'ETV5', 'FAIM2', 'FANCL', 'FTO',
+                 'GIPR', 'GNPDA2', 'GPRC5B', 'HMGCR', 'KCTD15', 'LMX1B',
+                 'LRP1B', 'LINGO2', 'MAP2K5', 'MC4R', 'MTCH2', 'MTIF3',
+                 'NEGR1', 'NPC1', 'NRXN3', 'NTRK2', 'NUDT3', 'POC5', 'POMC',
+                 'PRKD1', 'PRL', 'PTBP2', 'PTER', 'QPCTL', 'RPL27A', 'SEC16B',
+                 'SH2B1', 'SLC39A8', 'SREBF2', 'TFAP2B', 'TMEM160', 'TMEM18',
+                 'TNNI3K', 'TOMM40', 'ZNF608']
 
 if __name__ == "__main__":
     bioresDB = connect(server='mart.cea.fr', user='admin', passwd='alpine')
-    
+
     snps_dict, void_gene, df = load_from_genes(bmi_gene_list,
-                                               study='IMAGEN', 
-                                               bioresDB = bioresDB)
-    #examples                         
-    a = df.loc[[u'000037509984', u'000044836688', u'000063400084'],:].values                         
-    b = df.loc[[u'000037509984', u'000063400084', u'000044836688'],:].values
-    subj_list = df.index   #subjects 
-    snp_list = df.columns  #snps
-    
+                                               study='IMAGEN',
+                                               bioresDB=bioresDB)
+    #examples
+    a = df.loc[[u'000037509984', u'000044836688', u'000063400084'], :].values
+    b = df.loc[[u'000037509984', u'000063400084', u'000044836688'], :].values
+    subj_list = df.index    # subjects
+    snp_list = df.columns   # snps
