d23345326eab1374da53c2b43b26397f465e18fd
Helene Lajous <helene.lajous@cea.fr>
2014-08-19 09:47:23 +0200

    2013_imagen_bmi: List all SNPs from genes known to be associated to BMI in a .csv file.

--- 2013_imagen_bmi/scripts/Tools/extract_BMI_SNPs.py
+++ 2013_imagen_bmi/scripts/Tools/extract_BMI_SNPs.py
@@ -3,6 +3,13 @@
 Created on Mon Jun  2 13:43:10 2014
 
 @author: vf140245
+
+This script returns the list of all SNPs extracted from genes known to be
+associated to BMI.
+
+OUTPUT: csv file
+    "/neurospin/brainomics/2013_imagen_bmi/data/BMI_SNPs.csv"
+
 """
 
 import os
@@ -28,8 +35,20 @@ if __name__ == "__main__":
     snps_dict, void_gene, df = load_from_genes(bmi_gene_list,
                                                study='IMAGEN',
                                                bioresDB=bioresDB)
-    #examples
-    a = df.loc[[u'000037509984', u'000044836688', u'000063400084'], :].values
-    b = df.loc[[u'000037509984', u'000063400084', u'000044836688'], :].values
-    subj_list = df.index    # subjects
-    snp_list = df.columns   # snps
+    # Pathnames
+    BASE_PATH = '/neurospin/brainomics/2013_imagen_bmi/'
+    DATA_PATH = os.path.join(BASE_PATH, 'data')
+
+    # SNPs considered: SNPs from genes known to be associated to BMI
+    BMI_SNPs = df.columns
+
+    # Write SNPs in a .csv file
+    BMI_SNPs.to_csv(os.path.join(DATA_PATH, 'BMI_SNPs.csv'))
+    print "SNPs_IMAGEN saved to BMI_SNPs.csv"
+
+
+#    #examples
+#    a = df.loc[[u'000037509984', u'000044836688', u'000063400084'], :].values
+#    b = df.loc[[u'000037509984', u'000063400084', u'000044836688'], :].values
+#    subj_list = df.index    # subjects
+#    snp_list = df.columns   # snps
