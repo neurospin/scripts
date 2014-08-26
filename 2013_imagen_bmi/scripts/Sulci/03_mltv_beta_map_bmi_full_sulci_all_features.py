0e67c8cb528ef74b9815ff1cb5c9e076246d087e
Helene Lajous <helene.lajous@cea.fr>
2014-08-26 12:10:43 +0200

    2013_imagen_bmi: Beta coefficients after linear fitting of BMI on all sulci features by ElasticNet algorithm.

--- 2013_imagen_bmi/scripts/Sulci/03_mltv_beta_map_bmi_full_sulci_all_features.py
+++ 2013_imagen_bmi/scripts/Sulci/03_mltv_beta_map_bmi_full_sulci_all_features.py
@@ -65,61 +65,61 @@ def load_full_sulci_all_features_bmi_data(cache):
         # Sulci features
         sulci_df_qc = pd.io.parsers.read_csv(os.path.join(QC_PATH,
                                                           'sulci_df_qc.csv'),
-                              sep=',',
-                              usecols=['mainmorpho_F.C.M._left.surface',
-                                       'mainmorpho_F.C.M._right.surface',
-                                       'mainmorpho_S.Pe.C._left.surface',
-                                       'mainmorpho_S.Pe.C._right.surface',
-                                       'mainmorpho_S.C._left.surface',
-                                       'mainmorpho_S.C._right.surface',
-                                       'mainmorpho_F.Coll._left.surface',
-                                       'mainmorpho_F.Coll._right.surface',
-                                       #
-                                       'mainmorpho_F.C.M._left.depthMax',
-                                       'mainmorpho_F.C.M._right.depthMax',
-                                       'mainmorpho_S.Pe.C._left.depthMax',
-                                       'mainmorpho_S.Pe.C._right.depthMax',
-                                       'mainmorpho_S.C._left.depthMax',
-                                       'mainmorpho_S.C._right.depthMax',
-                                       'mainmorpho_F.Coll._left.depthMax',
-                                       'mainmorpho_F.Coll._right.depthMax',
-                                       #
-                                       'mainmorpho_F.C.M._left.depthMean',
-                                       'mainmorpho_F.C.M._right.depthMean',
-                                       'mainmorpho_S.Pe.C._left.depthMean',
-                                       'mainmorpho_S.Pe.C._right.depthMean',
-                                       'mainmorpho_S.C._left.depthMean',
-                                       'mainmorpho_S.C._right.depthMean',
-                                       'mainmorpho_F.Coll._left.depthMean',
-                                       'mainmorpho_F.Coll._right.depthMean',
-                                       #
-                                       'mainmorpho_F.C.M._left.length',
-                                       'mainmorpho_F.C.M._right.length',
-                                       'mainmorpho_S.Pe.C._left.length',
-                                       'mainmorpho_S.Pe.C._right.length',
-                                       'mainmorpho_S.C._left.length',
-                                       'mainmorpho_S.C._right.length',
-                                       'mainmorpho_F.Coll._left.length',
-                                       'mainmorpho_F.Coll._right.length',
-                                       #
-                                       'mainmorpho_F.C.M._left.GM_thickness',
-                                       'mainmorpho_F.C.M._right.GM_thickness',
-                                       'mainmorpho_S.Pe.C._left.GM_thickness',
-                                       'mainmorpho_S.Pe.C._right.GM_thickness',
-                                       'mainmorpho_S.C._left.GM_thickness',
-                                       'mainmorpho_S.C._right.GM_thickness',
-                                       'mainmorpho_F.Coll._left.GM_thickness',
-                                       'mainmorpho_F.Coll._right.GM_thickness',
-                                       #
-                                       'mainmorpho_F.C.M._left.opening',
-                                       'mainmorpho_F.C.M._right.opening',
-                                       'mainmorpho_S.Pe.C._left.opening',
-                                       'mainmorpho_S.Pe.C._right.opening',
-                                       'mainmorpho_S.C._left.opening',
-                                       'mainmorpho_S.C._right.opening',
-                                       'mainmorpho_F.Coll._left.opening',
-                                       'mainmorpho_F.Coll._right.opening'
-                                       ])
+                              sep=',')
+#                              usecols=['mainmorpho_F.C.M._left.surface',
+#                                       'mainmorpho_F.C.M._right.surface',
+#                                       'mainmorpho_S.Pe.C._left.surface',
+#                                       'mainmorpho_S.Pe.C._right.surface',
+#                                       'mainmorpho_S.C._left.surface',
+#                                       'mainmorpho_S.C._right.surface',
+#                                       'mainmorpho_F.Coll._left.surface',
+#                                       'mainmorpho_F.Coll._right.surface',
+#                                       #
+#                                       'mainmorpho_F.C.M._left.depthMax',
+#                                       'mainmorpho_F.C.M._right.depthMax',
+#                                       'mainmorpho_S.Pe.C._left.depthMax',
+#                                       'mainmorpho_S.Pe.C._right.depthMax',
+#                                       'mainmorpho_S.C._left.depthMax',
+#                                       'mainmorpho_S.C._right.depthMax',
+#                                       'mainmorpho_F.Coll._left.depthMax',
+#                                       'mainmorpho_F.Coll._right.depthMax',
+#                                       #
+#                                       'mainmorpho_F.C.M._left.depthMean',
+#                                       'mainmorpho_F.C.M._right.depthMean',
+#                                       'mainmorpho_S.Pe.C._left.depthMean',
+#                                       'mainmorpho_S.Pe.C._right.depthMean',
+#                                       'mainmorpho_S.C._left.depthMean',
+#                                       'mainmorpho_S.C._right.depthMean',
+#                                       'mainmorpho_F.Coll._left.depthMean',
+#                                       'mainmorpho_F.Coll._right.depthMean',
+#                                       #
+#                                       'mainmorpho_F.C.M._left.length',
+#                                       'mainmorpho_F.C.M._right.length',
+#                                       'mainmorpho_S.Pe.C._left.length',
+#                                       'mainmorpho_S.Pe.C._right.length',
+#                                       'mainmorpho_S.C._left.length',
+#                                       'mainmorpho_S.C._right.length',
+#                                       'mainmorpho_F.Coll._left.length',
+#                                       'mainmorpho_F.Coll._right.length',
+#                                       #
+#                                       'mainmorpho_F.C.M._left.GM_thickness',
+#                                       'mainmorpho_F.C.M._right.GM_thickness',
+#                                       'mainmorpho_S.Pe.C._left.GM_thickness',
+#                                       'mainmorpho_S.Pe.C._right.GM_thickness',
+#                                       'mainmorpho_S.C._left.GM_thickness',
+#                                       'mainmorpho_S.C._right.GM_thickness',
+#                                       'mainmorpho_F.Coll._left.GM_thickness',
+#                                       'mainmorpho_F.Coll._right.GM_thickness',
+#                                       #
+#                                       'mainmorpho_F.C.M._left.opening',
+#                                       'mainmorpho_F.C.M._right.opening',
+#                                       'mainmorpho_S.Pe.C._left.opening',
+#                                       'mainmorpho_S.Pe.C._right.opening',
+#                                       'mainmorpho_S.C._left.opening',
+#                                       'mainmorpho_S.C._right.opening',
+#                                       'mainmorpho_F.Coll._left.opening',
+#                                       'mainmorpho_F.Coll._right.opening'
+#                                       ]
 
         # Set the new dataframe index: subjects ID in the right format
         sulci_df_qc = sulci_df_qc.set_index(sulci_index)
@@ -132,8 +132,11 @@ def load_full_sulci_all_features_bmi_data(cache):
         # Add one cofound since sulci follows a power law
         clinical_df['tiv2'] = pow(clinical_df['tiv_gaser'], 2)
 
-        clinical_cofounds = ['Gender de Feuil2', 'ImagingCentreCity',
-                             'tiv_gaser', 'tiv2', 'mean_pds']
+        clinical_cofounds = ['Gender de Feuil2',
+                             'ImagingCentreCity',
+                             'tiv_gaser',
+                             'tiv2',
+                             'mean_pds']
 
         clinical_df = clinical_df[clinical_cofounds]
 
@@ -205,8 +208,8 @@ if __name__ == "__main__":
 
     # Elasticnet algorithm via Pylearn-Parsimony
     print "Elasticnet algorithm"
-    alpha = 0.5
-    l1_ratio = 0.8
+    alpha = 0.1
+    l1_ratio = 0.3
     # BMI has been residualized with 2 categorical covariables
     # (Gender and ImagingCentreCity - 8 columns) and 3 ordinal variables
     # (tiv_gaser, tiv² and mean_pds - 3 columns)
