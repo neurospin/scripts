db47b74bfc83ba5d05df52bf53828569d749cbc9
Mathieu Dubois <mathieu.dubois@cea.fr>
2014-09-03 16:01:05 +0200

    WIP 2014_bd_dwi: corrected plot script.

--- 2014_bd_dwi/03_plots_results.py
+++ 2014_bd_dwi/03_plots_results.py
@@ -12,55 +12,69 @@ import pandas as pd
 import matplotlib.pylab as plt
 
 
-import utilities_plots as UP
+from brainomics import plot_utilities as UP
 
 
 ################
 # Input/Output #
 ################
 
-INPUTDIR = "/volatile/share/2014_bd_dwi"
-INPUT_RESULTS = os.path.join(INPUTDIR, "bd_dwi_enettv_csi_old/results_CSI.csv")
+INPUT_BASE_DIR = "/neurospin/brainomics/2014_bd_dwi"
+INPUT_DIR = os.path.join(INPUT_BASE_DIR, "bd_dwi_enettv_csi")
+INPUT_RESULTS = os.path.join(INPUT_DIR, "results_CSI.csv")
 
-OUTPUTDIR = os.path.join(INPUTDIR, "figures")
-if not os.path.exists(OUTPUTDIR):
-    os.makedirs(OUTPUTDIR)
+OUTPUT_DIR = os.path.join(INPUT_DIR, "figures")
+if not os.path.exists(OUTPUT_DIR):
+    os.makedirs(OUTPUT_DIR)
 
 
 # Parameters
 
 krange = [100, 1000, 10000, 100000, -1]
-alphas = [.01, .05, .1 , .5, 1.]
+alphas = [.01, .05, .1, .5, 1.]
 ratios = np.array([[1., 0., 1], [0., 1., 1], [.5, .5, 1], [.9, .1, 1],
                        [.1, .9, 1], [.01, .99, 1], [.001, .999, 1]])
-                       
-                       
-                      
-# script
-results = pd.read_csv(INPUT_RESULTS)
-#x_data = results[["k", "a", "l1", "l2", "tv", ]]
-#y_data = results[["k", "a", "l1", "l2", "recall_mean", ]]
-#
-#x_data.
-#for k in krange :
-#    plt.figure()
-#    for a in alphas :
+
+# Functions
+
 
 def find_nearest(a, a0):
     "Element in nd array `a` closest to the scalar value `a0`"
     idx = np.abs(a - a0).argmin()
     return a.flat[idx]
 
+
 def l1_ratio(l1, l2, tv):
     pos_val = np.array([1., 0., .5, .1, .9, .01, .99, .001, .999])
     if (tv == 1.0):
         return np.nan
     if ((l1 + l2) != 0.0):
-        approx_ratio = l1/(l1 + l2)
-        return find_nearest(pos_val, approx_ratio) 
+        approx_ratio = l1 / (l1 + l2)
+        return find_nearest(pos_val, approx_ratio)
 
-results['l1_ratio'] = np.vectorize(l1_ratio)(results['l1'], results['l2'], results['tv'])
+
+# script
+results = pd.read_csv(INPUT_RESULTS)
+# Compute l1_ratio
+results['l1_ratio'] = np.vectorize(l1_ratio)(results['l1'],
+                                             results['l2'],
+                                             results['tv'])
 
 r1 = results.loc[results.k == 1000]
-UP.f(r1, 'recall_mean', tv_pen='tv')
+handles = UP.mapreduce_plot(r1, 'recall_mean', tv_ratio='tv')
+for val, handle in handles.items():
+    handle.savefig(os.path.join(OUTPUT_DIR, str(val) + '.png'))
 plt.show()
+
+## Check some values
+#col = r1[['l1_ratio', 'recall_mean']]
+#for alpha in alphas:
+#    alpha_loc = r1.a == alpha
+#    print "alpha:", alpha
+#    for tv_ratio in sorted(r1.tv.unique()):
+#        tv_ratio_loc = r1.tv == tv_ratio
+#        print "tv:", tv_ratio
+#        data = col.loc[alpha_loc & tv_ratio_loc]
+#        print data.sort('l1_ratio')
+#        print data.sort('recall_mean')
+#        raw_input()
