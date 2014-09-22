8d351c2a8432b2edd7429908bc19a7bedb44e079
Helene Lajous <helene.lajous@cea.fr>
2014-09-22 14:21:20 +0200

    2013_imagen_bmi: Compute the mean of the images obtained after new_segment segmentation for the six tissue classes and normalize it to build the new TPM that will be used later to segment T1-weighted images using Gaser (Hervé Lemaître contribution).

--- 2013_imagen_bmi/scripts/Tools/Template/02_build_homogeneous_tpm.py
+++ 2013_imagen_bmi/scripts/Tools/Template/02_build_homogeneous_tpm.py
@@ -6,6 +6,7 @@ Created on Fri Sep  5 17:01:26 2014
 
 Compute the mean of the images obtained after new_segment segmentation (wmc)
 for the six tissue classes to build the new TPM.
+
 This TPM is then normalized and saved under:
 /neurospin/tmp/hl/TPM.nii
 
