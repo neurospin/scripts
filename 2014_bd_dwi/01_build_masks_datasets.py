63418c8307e146f4af0621fba7858f3630022ee0
Mathieu Dubois <mathieu.dubois@cea.fr>
2014-09-25 12:20:00 +0200

    WIP 2014_bd_dwi: finished the refactored script 01_build_masks_datasets.py
    All interresting case are treated and give the same results. Old cases are commented out.
    
    Remove old scripts.

--- 2014_bd_dwi/01_build_masks_datasets.py
+++ 2014_bd_dwi/01_build_masks_datasets.py
@@ -19,9 +19,11 @@ import pandas as pd
 BASE_PATH = "/neurospin/brainomics/2014_bd_dwi"
 
 INPUT_IMAGES = os.path.join(BASE_PATH, "all_FA/nii/stats/all_FA.nii")
+INPUT_SK_IMAGES = os.path.join(BASE_PATH, "all_FA/nii/stats/all_FA_skeletonised.nii")
 INPUT_CSV = os.path.join(BASE_PATH, "population.csv")
-# Image used to improve our mask
-INPUT_ATLAS = "HarvardOxford-sub-maxprob-thr0-1mm.nii.gz"
+# Atlas used to improve our mask
+INPUT_ATLAS = "/usr/share/data/harvard-oxford-atlases/HarvardOxford/HarvardOxford-sub-maxprob-thr0-1mm.nii.gz"
+
 OUTPUT_DIR = os.path.join(BASE_PATH, "datasets")
 if not os.path.exists(OUTPUT_DIR):
     os.makedirs(OUTPUT_DIR)
@@ -32,6 +34,8 @@ OUTPUT_STD_MASK = os.path.join(OUTPUT_DIR,
                                "mask.nii.gz")
 OUTPUT_TRUNC_MASK = os.path.join(OUTPUT_DIR,
                                  "mask_trunc.nii.gz")
+OUTPUT_SKEL_MASK = os.path.join(OUTPUT_DIR,
+                                "mask_skel.nii.gz")
 
 ###############################################################################
 # Parameters                                                                  #
@@ -45,6 +49,7 @@ TRUNC_MASK_SLICE = range(71)
 ###############################################################################
 # Functions                                                                   #
 ###############################################################################
+
 
 def create_mask(images, atlas, fa_threshold,
                 output_file,
@@ -84,15 +89,15 @@ def create_dataset_from_mask(images, population, mask_file,
     #n_images = images.shape[-1]
     n = population.shape[0]
     # Open mask
-    print "Reading mask:", mask_file
+    #print "Reading mask:", mask_file
     babel_mask = nib.load(mask_file)
     mask = babel_mask.get_data()
     bin_mask = mask != 0
     n_voxels_in_mask = np.count_nonzero(mask)
-    print "Number of voxels in mask:", n_voxels_in_mask
+    #print "Number of voxels in mask:", n_voxels_in_mask
 
     # Apply mask on images & center
-    print "Application of mask on all the images & centering"
+    #print "Application of mask on all the images & centering"
     masked_images = np.zeros((n, n_voxels_in_mask))
     for i, ID in enumerate(population.index):
         cur = population.iloc[i]
@@ -122,7 +127,8 @@ def create_dataset_from_mask(images, population, mask_file,
 
     # Store dataset and description
     np.save(output_x, Xtot)
-    covar_names = cat_covar_df.columns.tolist()  + cont_covar_df.columns.tolist()
+    covar_names = cat_covar_df.columns.tolist() + \
+                  cont_covar_df.columns.tolist()
     fh = open(output_x_desc, "w")
     fh.write('shape = (%i, %i): ' % (n, p))
     if add_intercept:
@@ -137,8 +143,10 @@ def create_dataset_from_mask(images, population, mask_file,
 ###############################################################################
 
 print "Reading data"
-images4d = nib.load(INPUT_IMAGES)
-images = images4d.get_data()
+babel_images = nib.load(INPUT_IMAGES)
+images = babel_images.get_data()
+babel_skel_images = nib.load(INPUT_SK_IMAGES)
+skel_images = babel_skel_images.get_data()
 
 population = pd.read_csv(INPUT_CSV,
                          index_col=0)
@@ -146,8 +154,13 @@ n, _ = population.shape
 
 # Load atlas
 atlas = nib.load(INPUT_ATLAS)
-assert np.all(images4d.get_affine() == atlas.get_affine())
+assert np.all(babel_images.get_affine() == atlas.get_affine())
+assert np.all(babel_skel_images.get_affine() == atlas.get_affine())
 
+
+print '###################'
+print '# Creating target #'
+print '###################'
 # Create Y (same for all the cases)
 Ytot = np.asarray(population.BD_HC, dtype='float64').reshape(n, 1)
 np.save(OUTPUT_Y, Ytot)
@@ -156,30 +169,53 @@ np.save(OUTPUT_Y, Ytot)
 # Create masks                                                                #
 ###############################################################################
 
+print '##################'
+print '# Creating masks #'
+print '##################'
+
 # First mask: simple threshold on pixel values
 print "Creating:", OUTPUT_STD_MASK
 create_mask(images, atlas, FA_THRESHOLD, OUTPUT_STD_MASK)
 
-# Second mask: simple threshold on pixel values
+# Second mask: threshold + slice
 print "Creating:", OUTPUT_TRUNC_MASK
 create_mask(images, atlas, FA_THRESHOLD, OUTPUT_TRUNC_MASK,
             slices=TRUNC_MASK_SLICE)
+
+# Third mask: threshold on skeletonized images
+print "Creating:", OUTPUT_SKEL_MASK
+create_mask(skel_images, atlas, FA_THRESHOLD, OUTPUT_SKEL_MASK)
 
 ###############################################################################
 # Create datasets                                                             #
 ###############################################################################
 
-# First case: no intercept, using only SEX and AGE, standard mask
-output_x = os.path.join(OUTPUT_DIR,
-                        "X.npy")
-output_x_desc = output_x.replace("npy", "txt")
-print "Creating", output_x
-create_dataset_from_mask(images, population, OUTPUT_STD_MASK,
-                         population[["SEX"]], population[["AGEATMRI"]],
-                         output_x, output_x_desc,
-                         add_intercept=False)
+print '#####################'
+print '# Creating datasets #'
+print '#####################'
 
-# Second case: intercept, using only SEX, SCANNER and AGE, standard mask
+## First case: no intercept, SEX and AGE, standard mask
+## This case is no longer of interest. It's keep here just for record.
+#output_x = os.path.join(OUTPUT_DIR,
+#                        "X_nointercept.npy")
+#output_x_desc = output_x.replace("npy", "txt")
+#print "Creating", output_x
+#create_dataset_from_mask(images, population, OUTPUT_STD_MASK,
+#                         population[["SEX"]], population[["AGEATMRI"]],
+#                         output_x, output_x_desc,
+#                         add_intercept=False)
+#
+## Second case: intercept, SEX and AGE, standard mask
+## This case is no longer of interest. It's keep here just for record.
+#output_x = os.path.join(OUTPUT_DIR,
+#                        "X.npy")
+#output_x_desc = output_x.replace("npy", "txt")
+#print "Creating", output_x
+#create_dataset_from_mask(images, population, OUTPUT_STD_MASK,
+#                         population[["SEX"]], population[["AGEATMRI"]],
+#                         output_x, output_x_desc)
+
+# Third case: intercept, SEX, SCANNER and AGE, standard mask
 output_x = os.path.join(OUTPUT_DIR,
                         "X_site.npy")
 output_x_desc = output_x.replace("npy", "txt")
@@ -190,7 +226,32 @@ create_dataset_from_mask(images, population, OUTPUT_STD_MASK,
                                      "SCANNER#2",
                                      "SCANNER#3"]],
                          population[["AGEATMRI"]],
-                         output_x, output_x_desc,
-                         add_intercept=False)
+                         output_x, output_x_desc)
+
+# Fourth case: intercept, SEX, SCANNER and AGE, truncated mask
+output_x = os.path.join(OUTPUT_DIR,
+                        "X_trunc.npy")
+output_x_desc = output_x.replace("npy", "txt")
+print "Creating", output_x
+create_dataset_from_mask(images, population, OUTPUT_TRUNC_MASK,
+                         population[["SEX",
+                                     "SCANNER#1",
+                                     "SCANNER#2",
+                                     "SCANNER#3"]],
+                         population[["AGEATMRI"]],
+                         output_x, output_x_desc)
+
+# Fifth case: skletonized images, SEX, SCANNER and AGE, skel mask
+output_x = os.path.join(OUTPUT_DIR,
+                        "X_skel.npy")
+output_x_desc = output_x.replace("npy", "txt")
+print "Creating", output_x
+create_dataset_from_mask(skel_images, population, OUTPUT_SKEL_MASK,
+                         population[["SEX",
+                                     "SCANNER#1",
+                                     "SCANNER#2",
+                                     "SCANNER#3"]],
+                         population[["AGEATMRI"]],
+                         output_x, output_x_desc)
 
 ###############################################################################
