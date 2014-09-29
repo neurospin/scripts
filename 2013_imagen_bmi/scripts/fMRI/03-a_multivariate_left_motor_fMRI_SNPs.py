d1e54e94ba73ef5626edf49218523bdd1f3439cc
Helene Lajous <helene.lajous@cea.fr>
2014-09-29 10:08:00 +0200

    2013_imagen_bmi: Multivariate association analysis between left motor tasks fMRI and SNPs at the intersection between BMI-associated SNPs referenced in the literature and SNPs read by the Illumina platform.

--- 2013_imagen_bmi/scripts/fMRI/03-a_multivariate_left_motor_fMRI_SNPs.py
+++ 2013_imagen_bmi/scripts/fMRI/03-a_multivariate_left_motor_fMRI_SNPs.py
@@ -26,21 +26,17 @@ INPUT:
     GCA_motor_left_images.npy'
     unfolded fMRI images for left motor tasks saved in an array-like format
 
-METHOD: MUOLS
+METHOD: Search for the optimal set of hyperparameters (alpha, l1_ratio) that
+        maximizes the correlation between predicted BMI values via the Linear
+        Model  and true ones using Elastic Net algorithm, mapreduce and
+        cross validation.
+--NB: Computation involves to send jobs to Gabriel.--
 
-NB: Subcortical features and covariates are centered-scaled.
+NB: Covariates are centered-scaled.
 
 OUTPUT:
-- '/neurospin/brainomics/2013_imagen_bmi/data/fMRI_GCA_motor_left/Results/
-    MULM_left_motor_fMRI_SNPs_after_Bonferroni_correction.txt'
-    Since we focus here on 22 SNPs, we keep the probability-values
-    p < (0.05 / 22) that meet a significance threshold of 0.05 after
-    Bonferroni correction.
-
-- '/neurospin/brainomics/2013_imagen_bmi/data/fMRI_GCA_motor_left/Results/
-    MUOLS_left_motor_fMRI_SNPs_beta_values_df.csv'
-    Beta values from the General Linear Model run on SNPs for fMRI left motor
-    tasks.
+- the Mapper returns predicted and true values of BMI, model estimators.
+- the Reducer returns R2 scores between prediction and true values.
 
 """
 
