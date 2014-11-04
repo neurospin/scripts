30340cae8395fd9c6c610d7ef89a22122814e436
duchesnay <edouard.duchesnay@gmail.com>
2014-11-04 12:17:23 +0100

    ADNI MCI - CTL - FS permutations

---
+++ 2013_adni/MCIc-CTL-FS/04_rndperm_tvenet_cs.py
@@ -0,0 +1,273 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 30 20:03:12 2014
+
+@author: edouard.duchesnay@cea.fr
+"""
+
+import os
+import json
+from collections import OrderedDict
+import numpy as np
+from sklearn.cross_validation import StratifiedKFold
+from sklearn.metrics import roc_auc_score, precision_recall_fscore_support
+from sklearn.feature_selection import SelectKBest
+from parsimony.estimators import LogisticRegressionL1L2TV
+import parsimony.functions.nesterov.tv as tv_helper
+from brainomics import array_utils
+from statsmodels.stats.inter_rater import fleiss_kappa
+
+NFOLDS = 5
+NRNDPERMS = 1000
+
+
+def load_globals(config):
+    import mapreduce as GLOBAL  # access to global variables
+    GLOBAL.DATA = GLOBAL.load_data(config["data"])
+    import brainomics.mesh_processing as mesh_utils
+    mesh_coord, mesh_triangles = mesh_utils.mesh_arrays(config["structure"]["mesh"])
+    mask = np.load(config["structure"]["mask"])
+    GLOBAL.mesh_coord, GLOBAL.mesh_triangles, GLOBAL.mask = mesh_coord, mesh_triangles, mask
+    A, _ = tv_helper.nesterov_linear_operator_from_mesh(GLOBAL.mesh_coord, GLOBAL.mesh_triangles, GLOBAL.mask)
+    GLOBAL.A = A
+    GLOBAL.CONFIG = config
+
+def resample(config, resample_nb):
+    import mapreduce as GLOBAL  # access to global variables
+    #GLOBAL.DATA = GLOBAL.load_data(config["data"])
+    resample = config["resample"][resample_nb]
+    if resample is not None:
+        #GLOBAL.DATA_RESAMPLED = {k: [GLOBAL.DATA[k][idx, ...] for idx in resample]
+        #                    for k in GLOBAL.DATA}
+        # TODO permute first y then apply CV
+        rnd_state = np.random.get_state()
+        #yp = np.random.permutation(GLOBAL.DATA['y'])
+        np.random.seed(resample_nb)
+        GLOBAL.DATA_RESAMPLED = dict(
+            X=[GLOBAL.DATA['X'][idx, ...]
+                for idx in resample],
+            y=[np.random.permutation(GLOBAL.DATA['y'][idx, ...])
+                for idx in resample])
+        np.random.set_state(rnd_state)
+    else:  # resample is None train == test
+        GLOBAL.DATA_RESAMPLED = {k: [GLOBAL.DATA[k] for idx in [0, 1]]
+                            for k in GLOBAL.DATA}
+
+def mapper(key, output_collector):
+    import mapreduce as GLOBAL # access to global variables:
+        #raise ImportError("could not import ")
+    # GLOBAL.DATA, GLOBAL.STRUCTURE, GLOBAL.A
+    # GLOBAL.DATA ::= {"X":[Xtrain, ytrain], "y":[Xtest, ytest]}
+    # key: list of parameters
+    Xtr = GLOBAL.DATA_RESAMPLED["X"][0]
+    Xte = GLOBAL.DATA_RESAMPLED["X"][1]
+    ytr = GLOBAL.DATA_RESAMPLED["y"][0]
+    yte = GLOBAL.DATA_RESAMPLED["y"][1]
+    print key, "Data shape:", Xtr.shape, Xte.shape, ytr.shape, yte.shape
+    # STRUCTURE = GLOBAL.STRUCTURE
+    #alpha, ratio_l1, ratio_l2, ratio_tv, k = key
+    #key = np.array(key)
+    penalty_start = GLOBAL.CONFIG["penalty_start"]
+    class_weight="auto" # unbiased
+    alpha = float(key[0])
+    l1, l2, tv, k = alpha * float(key[1]), alpha * float(key[2]), alpha * float(key[3]), key[4]
+    print "l1:%f, l2:%f, tv:%f, k:%i" % (l1, l2, tv, k)
+    if k != -1:
+        k = int(k)
+        aov = SelectKBest(k=k)
+        aov.fit(Xtr[..., penalty_start:], ytr.ravel())
+        mask = GLOBAL.mask != 0
+        mask[mask] = aov.get_support()
+        #print mask.sum()
+        A, _ = tv_helper.nesterov_linear_operator_from_mesh(GLOBAL.mesh_coord, GLOBAL.mesh_triangles, mask)
+        Xtr_r = np.hstack([Xtr[:, :penalty_start], Xtr[:, penalty_start:][:, aov.get_support()]])
+        Xte_r = np.hstack([Xte[:, :penalty_start], Xte[:, penalty_start:][:, aov.get_support()]])
+    else:
+        mask = np.ones(Xtr.shape[0], dtype=bool)
+        Xtr_r = Xtr
+        Xte_r = Xte
+        A = GLOBAL.A
+    mod = LogisticRegressionL1L2TV(l1, l2, tv, A, penalty_start=penalty_start,
+                                   class_weight=class_weight)
+    mod.fit(Xtr_r, ytr)
+    y_pred = mod.predict(Xte_r)
+    proba_pred = mod.predict_probability(Xte_r)
+    ret = dict(y_pred=y_pred, y_true=yte, beta=mod.beta,  mask=mask, proba_pred=proba_pred)
+    if output_collector:
+        output_collector.collect(key, ret)
+    else:
+        return ret
+
+def reducer(key, values):
+    # key : string of intermediary key
+    # load return dict correspondning to mapper ouput. they need to be loaded.
+    # DEBUG
+    #import glob, mapreduce
+    #values = [mapreduce.OutputCollector(p) for p in glob.glob("/neurospin/brainomics/2013_adni/AD-CTL/results/*/0.1_0.0_0.0_1.0_-1.0/")]
+    #values = [mapreduce.OutputCollector(p) for p in glob.glob("/home/ed203246/tmp/MCIc-MCInc_cs/results/*/0.1_0.0_0.0_1.0_-1.0/")]
+    # values = [mapreduce.OutputCollector(p) for p in glob.glob("/home/ed203246/tmp/MCIc-CTL_cs/results/*/0.1_0.0_1.0_0.0_-1.0/")]
+    # values = [mapreduce.OutputCollector(p) for p in glob.glob("/home/ed203246/tmp/MCIc-CTL_cs/results/*/0.1_0.0_0.5_0.5_-1.0/")]
+    # Compute sd; ie.: compute results on each folds
+    #values = [item.load() for item in values]
+    print "key", key
+    values = [item.load() for item in values[1:]]
+    recall_mean_std = np.std([np.mean(precision_recall_fscore_support(
+        item["y_true"].ravel(), item["y_pred"])[1]) for item in values]) / np.sqrt(len(values))
+    y_true = np.concatenate([item["y_true"].ravel() for item in values])
+    y_pred = np.concatenate([item["y_pred"].ravel() for item in values])
+    y_post = np.concatenate([item["proba_pred"].ravel() for item in values])
+    p, r, f, s = precision_recall_fscore_support(y_true, y_pred, average=None)
+    n_ite = None
+    auc = roc_auc_score(y_true, y_post) #area under curve score.
+    betas = np.hstack([item["beta"] for item in values]).T
+
+    ## Compute beta similarity measures
+
+    # Correlation
+    R = np.corrcoef(betas)
+    R = R[np.triu_indices_from(R, 1)]
+    # Fisher z-transformation / average
+    z_bar = np.mean(1. / 2. * np.log((1 + R) / (1 - R)))
+    # bracktransform
+    r_bar = (np.exp(2 * z_bar) - 1) /  (np.exp(2 * z_bar) + 1)
+
+    # threshold betas to compute fleiss_kappa and DICE
+    try:
+        betas_t = np.vstack([array_utils.arr_threshold_from_norm2_ratio(betas[i, :], .99)[0] for i in xrange(betas.shape[0])])
+        print "--", np.sqrt(np.sum(betas_t ** 2, 1)) / np.sqrt(np.sum(betas ** 2, 1))
+        print np.allclose(np.sqrt(np.sum(betas_t ** 2, 1)) / np.sqrt(np.sum(betas ** 2, 1)), [0.99]*5,
+                           rtol=0, atol=1e-02)
+    
+        # Compute fleiss kappa statistics
+        beta_signed = np.sign(betas_t)
+        table = np.zeros((beta_signed.shape[1], 3))
+        table[:, 0] = np.sum(beta_signed == 0, 0)
+        table[:, 1] = np.sum(beta_signed == 1, 0)
+        table[:, 2] = np.sum(beta_signed == -1, 0)
+        fleiss_kappa_stat = fleiss_kappa(table)
+    
+        # Paire-wise Dice coeficient
+        beta_n0 = betas_t != 0
+        ij = [[i, j] for i in xrange(5) for j in xrange(i+1, 5)]
+        #print [[idx[0], idx[1]] for idx in ij]
+        dice_bar = np.mean([float(np.sum(beta_signed[idx[0], :] == beta_signed[idx[1], :])) /\
+             (np.sum(beta_n0[idx[0], :]) + np.sum(beta_n0[idx[1], :]))
+             for idx in ij])
+    except:
+        dice_bar = fleiss_kappa_stat = 0.
+
+#    R = np.corrcoef(betas)
+#    beta_cor_mean = np.mean(R[np.triu_indices_from(R, 1)])
+    a, l1, l2 , tv , k = key#[float(par) for par in key.split("_")]
+    scores = OrderedDict()
+    scores['a'] = a
+    scores['l1'] = l1
+    scores['l2'] = l2
+    scores['tv'] = tv
+    left = float(1-tv)
+    if left == 0: left = 1.
+    scores['l1l2_ratio'] = l1 / left
+    scores['recall_0'] = r[0]
+    scores['recall_1'] = r[1]
+    scores['recall_mean'] = r.mean()
+    scores['recall_mean_std'] = recall_mean_std
+    scores['auc'] = auc
+#    scores['beta_cor_mean'] = beta_cor_mean
+    scores['precision_0'] = p[0]
+    scores['precision_1'] = p[1]
+    scores['precision_mean'] = p.mean()
+    scores['f1_0'] = f[0]
+    scores['f1_1'] = f[1]
+    scores['f1_mean'] = f.mean()
+    scores['support_0'] = s[0]
+    scores['support_1'] = s[1]
+#    scores['corr']= corr
+    scores['beta_r'] = str(R)
+    scores['beta_r_bar'] = r_bar
+    scores['beta_fleiss_kappa'] = fleiss_kappa_stat
+    scores['beta_dice_bar'] = dice_bar
+    scores['n_ite'] = n_ite
+    scores['k'] = k
+    scores['key'] = key
+    return scores
+
+
+if __name__ == "__main__":
+    WD = "/neurospin/brainomics/2013_adni/MCIc-CTL-FS"
+    #BASE = "/neurospin/tmp/brainomics/testenettv"
+    #WD_CLUSTER = WD.replace("/neurospin/brainomics", "/neurospin/tmp/brainomics")
+    #print "Sync data to %s/ " % os.path.dirname(WD)
+    #os.system('rsync -azvu %s %s/' % (BASE, os.path.dirname(WD)))
+    INPUT_DATA_X = 'X.npy'
+    INPUT_DATA_y = 'y.npy'
+    STRUCTURE = dict(mesh="lrh.pial.gii", mask="mask.npy")
+    if not os.path.exists(WD): os.makedirs(WD)
+    os.chdir(WD)
+
+    #############################################################################
+    ## Create config file
+    y = np.load(INPUT_DATA_y)
+    if os.path.exists("config_rndperm.json"):
+        inf = open("config_rndperm.json", "r")
+        old_conf = json.load(inf)
+        rndperm = old_conf["resample"]
+        inf.close()
+    else:
+        rndperm = [[tr.tolist(), te.tolist()] for perm in xrange(NRNDPERMS)
+            for tr, te in StratifiedKFold(y.ravel(), n_folds=NFOLDS)]
+    params = \
+        [(0.01, 0.0, 1.0, 0.0, -1.0), # l2
+        (0.01, 0.0, 0.5, 0.5, -1.0),  # l2tv
+        (0.01, 1.0, 0.0, 0.0, -1.0),  # l1
+        (0.01, 0.5, 0.0, 0.5, -1.0),  # l1tv
+        (0.01, 0.0, 0.0, 1.0, -1.0),  # tv
+        (0.01, 0.5, 0.5, 0.0, -1.0),  # l1l2
+        (0.01, 0.35, 0.35, 0.3, -1.0)] #l1l2tv
+
+    user_func_filename = os.path.join(os.environ["HOME"],
+        "git", "scripts", "2013_adni", "MCIc-CTL-FS",
+        "04_rndperm_tvenet_cs.py")
+    #print __file__, os.path.abspath(__file__)
+    print "user_func", user_func_filename
+    #import sys
+    #sys.exit(0)
+    # Use relative path from config.json
+    config = dict(data=dict(X=INPUT_DATA_X, y=INPUT_DATA_y),
+                  params=params, resample=rndperm,
+                  structure=STRUCTURE,
+                  map_output="rndperm",
+                  penalty_start = 2,
+                  user_func=user_func_filename,
+                  reduce_group_by='params',
+                  reduce_output="MCIc-CTL-FS_rndperm.csv")
+    json.dump(config, open(os.path.join(WD, "config_rndperm.json"), "w"))
+
+    #############################################################################
+    # Build utils files: sync (push/pull) and PBS
+    import brainomics.cluster_gabriel as clust_utils
+    sync_push_filename, sync_pull_filename, WD_CLUSTER = \
+        clust_utils.gabriel_make_sync_data_files(WD)
+    cmd = "mapreduce.py --map  %s/config_rndperm.json" % WD_CLUSTER
+    clust_utils.gabriel_make_qsub_job_files(WD, cmd)
+    #############################################################################
+    # Sync to cluster
+    print "Sync data to gabriel.intra.cea.fr: "
+    os.system(sync_push_filename)
+    #############################################################################
+    print "# Start by running Locally with 2 cores, to check that everything os OK)"
+    print "Interrupt after a while CTL-C"
+    print "mapreduce.py --map %s/config_rndperm.json --ncore 2" % WD
+    print "# 1) Log on gabriel:"
+    print 'ssh -t gabriel.intra.cea.fr'
+    print "# 2) Run one Job to test"
+    print "qsub -I"
+    print "cd %s" % WD_CLUSTER
+    print "./job_Global_long.pbs"
+    print "# 3) Run on cluster"
+    print "qsub job_Global_long.pbs"
+    print "# 4) Log out and pull Pull"
+    print "exit"
+    print sync_pull_filename
+    #############################################################################
+    print "# Reduce"
+    print "mapreduce.py --reduce %s/config_rndperm.json" % WD
