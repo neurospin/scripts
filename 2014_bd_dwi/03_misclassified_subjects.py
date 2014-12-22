7553fb3c4224620383cc0f755442561bec8908a3
Mathieu Dubois <mathieu.dubois@cea.fr>
2014-12-22 18:19:10 +0100

    ENH 2014_bd_dwi/03_misclassified_subjects.py: use all the datasets.

--- 2014_bd_dwi/03_misclassified_subjects.py
+++ 2014_bd_dwi/03_misclassified_subjects.py
@@ -22,17 +22,22 @@ import mapreduce
 
 INPUT_BASE_DIR = "/neurospin/brainomics/2014_bd_dwi/"
 INPUT_POPULATION = os.path.join(INPUT_BASE_DIR, "population.csv")
-INPUT_DIR = os.path.join(INPUT_BASE_DIR, "enettv_bd_dwi_trunc")
-INPUT_CONFIG_PATH = os.path.join(INPUT_DIR, "config.json")
 
-INPUT_PRED_FILE_NAME = "y_pred.npz"
-INPUT_TRUE_FILE_NAME = "y_true.npz"
+INPUT_DATASET_DIR_FORMAT = "enettv_bd_dwi_{dataset}"
+
+INPUT_DATASETS = ["site",
+                  "skel",
+                  "trunc"]
+
+INPUT_CONFIG_FILENAME = "config.json"
+INPUT_PRED_FILENAME = "y_pred.npz"
+INPUT_TRUE_FILENAME = "y_true.npz"
 
 ##########
 # Output #
 ##########
 
-OUTPUT_DIR = INPUT_DIR
+OUTPUT_DIR = INPUT_BASE_DIR
 OUTPUT_MISSCLASSIF_FILE = os.path.join(OUTPUT_DIR,
                                        "missclassified.csv")
 
@@ -52,38 +57,66 @@ OUTPUT_MISSCLASSIF_FILE = os.path.join(OUTPUT_DIR,
 population = pd.io.parsers.read_csv(INPUT_POPULATION)
 subjects_id = population.ID
 
-# Load jobs
-config = json.load(open(INPUT_CONFIG_PATH))
-jobs = mapreduce._build_job_table(config)
-#results_dir = os.path.join(INPUT_DIR, config["map_output"])
-params_list = [tuple(p) for p in config["params"]]
-resamples = config["resample"]
+missclassif = None
+for dataset in INPUT_DATASETS:
+    dataset_dir = INPUT_DATASET_DIR_FORMAT.format(dataset=dataset)
+    dataset_full_dir = os.path.join(INPUT_BASE_DIR,
+                                    dataset_dir)
 
-# Group by params
-groups = jobs.groupby("params")
+    # Load config and create jobs
+    config_full_filename = os.path.join(dataset_full_dir,
+                                        INPUT_CONFIG_FILENAME)
+    config = json.load(open(config_full_filename))
+    jobs = mapreduce._build_job_table(config)
+    params_list = [tuple(p) for p in config["params"]]
+    resamples_list = config["resample"]
 
-# Reload data and count misclassifications per subject
-# We skip resample[0] (whole population)
-missclassif = pd.DataFrame(index=subjects_id,
-                           columns=params_list,
-                           dtype='int')
-for params, param_jobs in groups:
-    for i, job in param_jobs.iterrows():
-        if job["resample_index"] != 0:
-            params = job.params
-            resample_index = job.resample_index
-            full_results_dir = os.path.join(INPUT_DIR,
-                                            job["output dir"])
-            y_pred_file = os.path.join(full_results_dir,
-                                       INPUT_PRED_FILE_NAME)
-            #print "Loading", y_pred_file
-            y_pred = np.load(y_pred_file)['arr_0']
-            y_true_file = os.path.join(full_results_dir,
-                                       INPUT_TRUE_FILE_NAME)
-            #print "Loading", y_true_file
-            y_true = np.load(y_true_file)['arr_0']
-            subjects = subjects_id[resamples[resample_index][1]]
-            missclassif[params].loc[subjects] = (y_pred != y_true)
+    # Create dataframe
+    if missclassif is None:
+        # Create index (dataset + params)
+        # Assume that all datasets have the same parameters
+        index_levels = [(dataset_, ) + params
+                        for dataset_ in INPUT_DATASETS
+                        for params in params_list]
+        index = pd.MultiIndex.from_tuples(index_levels,
+                                          names=['dataset',
+                                                 'a', 'tv', 'l1', 'l2',
+                                                 'k'])
+        missclassif = pd.DataFrame(columns=subjects_id,
+                                   index=index,
+                                   dtype='bool')
 
-# We store it in the reverse order for easier visualization with localc
-missclassif.T.to_csv(OUTPUT_MISSCLASSIF_FILE)
+    # Group by params
+    groups = jobs.groupby("params")
+
+    # Reload data and count misclassifications per subject
+    # We skip resample[0] (whole population)
+    for params, param_jobs in groups:
+        for i, job in param_jobs.iterrows():
+            param_res = pd.DataFrame(index=subjects_id,
+                                     columns=['classif'],
+                                     dtype='bool')
+            if job["resample_index"] != 0:
+                params = job.params
+                index = (dataset, ) + params
+                resample_index = job.resample_index
+                subjects = subjects_id[resamples_list[resample_index][1]]
+
+                full_results_dir = os.path.join(dataset_full_dir,
+                                                job["output dir"])
+
+                y_pred_file = os.path.join(full_results_dir,
+                                           INPUT_PRED_FILENAME)
+                print "Loading", y_pred_file
+                y_pred = np.load(y_pred_file)['arr_0']
+
+                y_true_file = os.path.join(full_results_dir,
+                                           INPUT_TRUE_FILENAME)
+                print "Loading", y_true_file
+                y_true = np.load(y_true_file)['arr_0']
+
+                param_res.loc[subjects] = (y_pred != y_true)
+            missclassif.loc[index] = param_res['classif']
+
+# Store results
+missclassif.to_csv(OUTPUT_MISSCLASSIF_FILE)
