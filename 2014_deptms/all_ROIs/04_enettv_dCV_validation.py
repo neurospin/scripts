c133364a3b5b6e265b235d1682af4ad0a9b4b68f
Clémence PINAUD <clemence.pinaud@cea.fr>
2015-01-13 10:40:41 +0100

    2014_deptms: config model validation in double cross validation

--- 2014_deptms/all_ROIs/04_enettv_dCV_validation.py
+++ 2014_deptms/all_ROIs/04_enettv_dCV_validation.py
@@ -8,22 +8,20 @@ import os
 import json
 import numpy as np
 import pandas as pd
-from sklearn.cross_validation import StratifiedKFold
 import nibabel
 from sklearn.metrics import precision_recall_fscore_support
 from sklearn.metrics import roc_auc_score
-from sklearn.feature_selection import SelectKBest
 from parsimony.estimators import LogisticRegressionL1L2TV
 from parsimony.algorithms.utils import Info
 import parsimony.functions.nesterov.tv as tv_helper
 import shutil
 from scipy.stats import binom_test
-import json
 
 from collections import OrderedDict
 
 
 NFOLDS = 10
+
 
 def load_globals(config):
     import mapreduce as GLOBAL  # access to global variables
@@ -172,27 +170,27 @@ if __name__ == "__main__":
 
     penalty_start = 3
 
-    #########################################################################
-    ## Read ROIs csv
-    atlas = []
-    dict_rois = {}
-    df_rois = pd.read_csv(INPUT_ROIS_CSV)
-    for i, ROI_name_aal in enumerate(df_rois["ROI_name_aal"]):
-        cur = df_rois[df_rois.ROI_name_aal == ROI_name_aal]
-        label_ho = cur["label_ho"].values[0]
-        atlas_ho = cur["atlas_ho"].values[0]
-        roi_name = cur["ROI_name_deptms"].values[0]
-        if ((not cur.isnull()["atlas_ho"].values[0])
-            and (not cur.isnull()["ROI_name_deptms"].values[0])):
-            if not roi_name in dict_rois:
-                labels = np.asarray(label_ho.split(), dtype="int")
-                dict_rois[roi_name] = [labels]
-                dict_rois[roi_name].append(atlas_ho)
-
-    rois = list(set(df_rois["ROI_name_deptms"].values.tolist()))
-    rois = [x for x in rois if str(x) != 'nan']
-    rois.append("brain")  # add whole brain to rois
-
+#
+#########################################################################
+#    ## Read ROIs csv
+#    atlas = []
+#    dict_rois = {}
+#    df_rois = pd.read_csv(INPUT_ROIS_CSV)
+#    for i, ROI_name_aal in enumerate(df_rois["ROI_name_aal"]):
+#        cur = df_rois[df_rois.ROI_name_aal == ROI_name_aal]
+#        label_ho = cur["label_ho"].values[0]
+#        atlas_ho = cur["atlas_ho"].values[0]
+#        roi_name = cur["ROI_name_deptms"].values[0]
+#        if ((not cur.isnull()["atlas_ho"].values[0])
+#            and (not cur.isnull()["ROI_name_deptms"].values[0])):
+#            if not roi_name in dict_rois:
+#                labels = np.asarray(label_ho.split(), dtype="int")
+#                dict_rois[roi_name] = [labels]
+#                dict_rois[roi_name].append(atlas_ho)
+#
+#    rois = list(set(df_rois["ROI_name_deptms"].values.tolist()))
+#    rois = [x for x in rois if str(x) != 'nan']
+#    rois.append("brain")  # add whole brain to rois
     #########################################################################
     ## Build config file for all couple (Modality, roi)
 
@@ -200,77 +198,78 @@ if __name__ == "__main__":
     print "Modality: ", modality
     DATA_MODALITY_PATH = os.path.join(DATASET_PATH, modality)
 
-    for roi in rois:
-        print "ROI", roi
+#    for roi in rois:
+    roi = "Roiho-cingulumAnt"
+    print "ROI", roi
 
-        WD = os.path.join(OUTPUT_ENETTV, modality + '_' + roi)
+    WD = os.path.join(OUTPUT_ENETTV, modality + '_' + roi)
 
-        if not os.path.exists(WD):
-            os.makedirs(WD)
-        # copy X, y, mask file names in the current directory
-        if not os.path.isfile(os.path.join(WD, 'X.npy')):
-            INPUT_DATA_X = os.path.join(DATA_MODALITY_PATH,
-                                        'X_' + modality + '_' + roi + '.npy')
-            shutil.copy2(INPUT_DATA_X, os.path.join(WD, 'X.npy'))
-        if not os.path.isfile(os.path.join(WD, 'y.npy')):
-            INPUT_DATA_y = os.path.join(DATA_MODALITY_PATH,
-                                        'y.npy')
-            shutil.copy2(INPUT_DATA_y, os.path.join(WD, 'y.npy'))
-        if not os.path.isfile(os.path.join(WD, 'mask.nii')):
-            INPUT_MASK = os.path.join(DATA_MODALITY_PATH,
-                                     'mask_' + modality + '_' + roi + '.nii')
-            shutil.copy2(INPUT_MASK, os.path.join(WD, 'mask.nii'))
+    if not os.path.exists(WD):
+        os.makedirs(WD)
+    # copy X, y, mask file names in the current directory
+    if not os.path.isfile(os.path.join(WD, 'X.npy')):
+        INPUT_DATA_X = os.path.join(DATA_MODALITY_PATH,
+                                    'X_' + modality + '_' + roi + '.npy')
+        shutil.copy2(INPUT_DATA_X, os.path.join(WD, 'X.npy'))
+    if not os.path.isfile(os.path.join(WD, 'y.npy')):
+        INPUT_DATA_y = os.path.join(DATA_MODALITY_PATH,
+                                    'y.npy')
+        shutil.copy2(INPUT_DATA_y, os.path.join(WD, 'y.npy'))
+    if not os.path.isfile(os.path.join(WD, 'mask.nii')):
+        INPUT_MASK = os.path.join(DATA_MODALITY_PATH,
+                                 'mask_' + modality + '_' + roi + '.nii')
+        shutil.copy2(INPUT_MASK, os.path.join(WD, 'mask.nii'))
 
-        #################################################################
-        ## Read config file selection
-        
-        config_selection_file = os.path.join(WD, "config_dCV_selection.json")
+    #################################################################
+    ## Read config file selection
 
-        # open config file
-        config_selection = json.load(open(config_selection_file))
-        # get resample index for each fold
-        resample = config_selection["resample"]
-        resample = np.asarray(resample)
-        # cv=[[tr, te] for each fold]
-        cv = [[i + 1, resample[1 + (NFOLDS + 1) * i][1],
-               resample[1 + (NFOLDS + 1) * i][2]] \
-                            for i in xrange(NFOLDS)]
-        cv.insert(0, None)  # first fold is None
+    config_selection_file = os.path.join(WD, "config_dCV_selection.json")
 
-        # Summary_file
-        ##############
-        summary_file = os.path.join(WD, config_selection["output_summary"])
-        summary = pd.read_csv(summary_file)
+    # open config file
+    config_selection = json.load(open(config_selection_file))
+    # get resample index for each fold
+    resample = config_selection["resample"]
+    resample = np.asarray(resample)
+    # cv=[[tr, te] for each fold]
+    cv = [[i + 1, resample[1 + (NFOLDS + 1) * i][1],
+           resample[1 + (NFOLDS + 1) * i][2]] \
+                        for i in xrange(NFOLDS)]
+    cv.insert(0, None)  # first fold is None
 
-        criteria = ['recall_mean', 'min_recall', 'max_pvalue_recall',
-            'accuracy', 'pvalue_accuracy']
-        model = {}
-        for i, criterium in enumerate(criteria):
-            model[criterium] = summary['param_opt_' + i]
+    # Summary_file
+    ##############
+    summary_file = os.path.join(WD, config_selection["output_summary"])
+    summary = pd.read_csv(summary_file)
 
-        user_func_filename = os.path.abspath(__file__)
-        print "user_func", user_func_filename
-        config = dict(data=config_selection['data'],
-                      params=criteria, model=model,
-                      resample=cv,
-                      structure=config_selection['structure'],
-                      map_output=config_selection['map_output_validation',
-                      user_func=user_func_filename,
-                      reduce_group_by="criteria",
-                      output_validation="results_dCV_validation.csv",
-                      penalty_start=config_selection['penalty_start'],
-                      prob_class1=config_selection['prob_class1'],
-                      roi=roi)
-        json.dump(config, open(os.path.join(WD, "config_dCV_validation.json"),
-                               "w"))
+    criteria = ['recall_mean', 'min_recall', 'max_pvalue_recall',
+        'accuracy', 'pvalue_accuracy']
+    model = {}
+    for i, criterium in enumerate(criteria):
+        model[criterium] = summary['param_opt_' + str(i + 1)].values.tolist()
 
-        #################################################################
-        # Build utils files: sync (push/pull) and PBS
-        import brainomics.cluster_gabriel as clust_utils
-        sync_push_filename, sync_pull_filename, WD_CLUSTER = \
-            clust_utils.gabriel_make_sync_data_files(WD)
-        cmd = "mapreduce.py --map  %s/config_dCV_validation.json" % WD_CLUSTER
-        clust_utils.gabriel_make_qsub_job_files(WD, cmd)
+    user_func_filename = os.path.abspath(__file__)
+    print "user_func", user_func_filename
+    config = dict(data=config_selection['data'],
+                  params=criteria, model=model,
+                  resample=cv,
+                  structure=config_selection['structure'],
+                  map_output=config_selection['map_output_validation'],
+                  user_func=user_func_filename,
+                  reduce_group_by="params",
+                  output_validation="results_dCV_validation.csv",
+                  penalty_start=config_selection['penalty_start'],
+                  prob_class1=config_selection['prob_class1'],
+                  roi=roi)
+    json.dump(config, open(os.path.join(WD, "config_dCV_validation.json"),
+                           "w"))
+
+    #################################################################
+    # Build utils files: sync (push/pull) and PBS
+    import brainomics.cluster_gabriel as clust_utils
+    sync_push_filename, sync_pull_filename, WD_CLUSTER = \
+        clust_utils.gabriel_make_sync_data_files(WD)
+    cmd = "mapreduce.py --map  %s/config_dCV_validation.json" % WD_CLUSTER
+    clust_utils.gabriel_make_qsub_job_files(WD, cmd)
         ################################################################
 #        # Sync to cluster
 #        print "Sync data to gabriel.intra.cea.fr: "
