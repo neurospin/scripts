4f2943c783a58cf96c9a67670687bb2972975a27
Vincent Frouin <vincent.frouin@cea.fr>
2015-03-02 11:15:03 +0100

    ADD: full example

---
+++ 2015_hippo_l1_gl_ovl/example2.py
@@ -0,0 +1,82 @@
+#! /usr/bin/env python
+##########################################################################
+# Brainomics - Copyright (C) CEA, 2013
+# Distributed under the terms of the CeCILL-B license, as published by
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+# for details.
+##########################################################################
+
+# System import
+import pandas
+import pickle
+import numpy as np
+
+#######################
+# get Enigma2 dataset
+#######################
+fin = ('/neurospin/brainomics/2015_hippo_l1_gl_ovl/data/'
+       'imagen_subcortCov_NP.csv')
+df = pandas.DataFrame.from_csv(fin, sep=' ', index_col=False)
+iid_fid = ["%012d" % int(i) for i in df['IID']]
+iid_fid = pandas.DataFrame(np.asarray([iid_fid, iid_fid]).T,
+                           columns=['FID', 'IID'])
+#######################
+# get phenotype Lhippo
+#######################
+Lhippo = df[['Lhippo']].join(iid_fid)
+Lhippo = Lhippo.set_index(iid_fid['IID'])
+
+#######################
+# get phenotype Lhippo
+#######################
+covariate = iid_fid
+covariate = covariate.join(pandas.get_dummies(df['ScanningCentre'],
+                                              prefix='Centre')[range(7)])
+covariate = covariate.join(df[['Age', 'Sex', 'ICV', 'AgeSq']])
+covariate = covariate.set_index(iid_fid['IID'])
+
+#######################
+# get phenotype Lhippo
+#######################
+fname = '/neurospin/brainomics/2015_hippo_l1_gl_ovl/data/synapticAll.pickle'
+f = open(fname)
+genodata = pickle.load(f)
+f.close()
+#######################
+# read geno data
+########################
+iid_fid = ["%012d" % int(i) for i in genodata.fid]
+iid_fid = pandas.DataFrame(np.asarray([iid_fid, iid_fid]).T,
+                           columns=['FID', 'IID'])
+rsname = genodata.get_meta()[0].tolist()
+geno = pandas.DataFrame(genodata.data, columns=rsname)
+geno = geno.join(iid_fid)
+geno = geno.set_index(iid_fid['IID'])
+
+
+#######################
+# Perform subseting
+########################
+indx = list(set(Lhippo['IID']).intersection(
+            set(covariate['IID'])).intersection(
+            set(geno['IID'])))
+
+covariate = covariate.loc[indx]
+geno = geno.loc[indx]
+Lhippo = Lhippo.loc[indx]
+
+
+#######################
+# get the usual matrices
+########################
+Y = Lhippo['Lhippo'].as_matrix()
+tmp = list(covariate.columns)
+tmp.remove('FID')
+tmp.remove('IID')
+tmp.remove('AgeSq')
+Cov = covariate[tmp].as_matrix()
+tmp = list(geno.columns)
+tmp.remove('FID')
+tmp.remove('IID')
+X = geno[tmp].as_matrix()
