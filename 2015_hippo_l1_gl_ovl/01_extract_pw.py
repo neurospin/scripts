bc785609b06da9d2f4a34244aaab3cacb8404594
Thomas Gareau <thomas.gareau@cea.fr>
2015-03-31 15:31:09 +0200

    Revise picke generation (01_extract_pw.py) and plotting function in 20_eval_pw.py.

--- 2015_hippo_l1_gl_ovl/01_extract_pw.py
+++ 2015_hippo_l1_gl_ovl/01_extract_pw.py
@@ -20,35 +20,127 @@ from genibabel.gtypes import Pathways
 logging.basicConfig(level=logging.INFO)
 
 
-#fn = ('/neurospin/brainomics/bio_resources/genesets/'
-#      'msigdb-v3.1/c7.go-synaptic.symbols.gmt')
-fn = ('/neurospin/brainomics/bio_resources/genesets/'
-      'msigdb-v3.1/c2.cp.kegg.v3.1.symbols.gmt')
-origin = "msigdb-v3.1",
-compound_name = 'c2.cp.kegg'
-pws = Pathways(fn=fn, origin=origin, compound_name=compound_name)
+""" Usage: Each part generates a pickle file, uncomment the part(s) you need
+"""
 
-#list patwhways names
-print '====================================================================='
-print 'pathways names contained in this set'
-for pw in pws:
-    print pw['name']
+############################################################################
+#################### generate new_synapticAll.pickle #######################
+############################################################################
+#fn = "/neurospin/brainomics/bio_resources/genesets/msigdb-v3.1/c7.go-synaptic.symbols.gmt"
+#compound_name = "c7.go-synaptic"
+#origin = "msigdb-v3.1"
+#pws = Pathways(fn=fn, origin=origin, compound_name=compound_name)
+#
+#pws.filter(min_gen=5)
+#
+#login = raw_input("Login on the imagen2 server:")
+#password = getpass.getpass("Password on the imagen2 server:")
+#
+#genodata = imagen_genotype_measure(login, password, pathways=pws)
+#
+#fname = "/neurospin/brainomics/2015_hippo_l1_gl_ovl/data/new_synapticAll.pickle"
+#f = open(fname, 'wb')
+#pickle.dump(genodata, f)
+#f.close()
 
-Pathways.filter(pws, min_gen=5,)
-#list patwhways names:
-print '====================================================================='
-print 'pathways names contained in this set limited to 10'
-for pw in pws:
-    print pw['name']
 
-# create a complete set
-login = raw_input("Login on the imagen2 server:")
-password = getpass.getpass("Password on the imagen2 server:")
-#genodata = imagen_genotype_measure_alt(login, password, status="qc",
-#                                    gene_names=list(geneset))
-genodata = imagen_genotype_measure(login, password, pathways=pws)
+############################################################################
+#################### generate new_synaptic10.pickle ########################
+############################################################################
+#fn = "/neurospin/brainomics/bio_resources/genesets/msigdb-v3.1/c7.go-synaptic.symbols.gmt"
+#compound_name = "c7.go-synaptic"
+#origin = "msigdb-v3.1"
+#pws = Pathways(fn=fn, origin=origin, compound_name=compound_name)
+#
+#pws.filter(min_gen=5)
+#pws._entry = pws._entry[:10]
+#
+#login = raw_input("Login on the imagen2 server:")
+#password = getpass.getpass("Password on the imagen2 server:")
+#
+#genodata = imagen_genotype_measure(login, password, pathways=pws)
+#
+#fname = "/neurospin/brainomics/2015_hippo_l1_gl_ovl/data/new_synaptic10.pickle"
+#f = open(fname, 'wb')
+#pickle.dump(genodata, f)
+#f.close()
 
-fname = '/neurospin/brainomics/2015_hippo_l1_gl_ovl/data/kegg.pickle'
-f = open(fname, 'wb')
-pickle.dump(genodata, f)
-f.close()
+
+############################################################################
+#################### generate new_synaptic5.pickle #########################
+############################################################################
+#fn = "/neurospin/brainomics/bio_resources/genesets/msigdb-v3.1/c7.go-synaptic.symbols.gmt"
+#compound_name = "c7.go-synaptic"
+#origin = "msigdb-v3.1"
+#pws = Pathways(fn=fn, origin=origin, compound_name=compound_name)
+#
+#pws.filter(min_gen=5)
+#pws._entry = pws._entry[:5]
+#
+#login = raw_input("Login on the imagen2 server:")
+#password = getpass.getpass("Password on the imagen2 server:")
+#
+#genodata = imagen_genotype_measure(login, password, pathways=pws)
+#
+#fname = "/neurospin/brainomics/2015_hippo_l1_gl_ovl/data/new_synaptic5.pickle"
+#f = open(fname, 'wb')
+#pickle.dump(genodata, f)
+#f.close()
+
+
+############################################################################
+#################### generate new_synaptic2.pickle #########################
+############################################################################
+#fn = "/neurospin/brainomics/bio_resources/genesets/msigdb-v3.1/c7.go-synaptic.symbols.gmt"
+#compound_name = "c7.go-synaptic"
+#origin = "msigdb-v3.1"
+#pws = Pathways(fn=fn, origin=origin, compound_name=compound_name)
+#
+#pws.filter(min_gen=5)
+#pws._entry = pws._entry[:2]
+#
+#login = raw_input("Login on the imagen2 server:")
+#password = getpass.getpass("Password on the imagen2 server:")
+#
+#genodata = imagen_genotype_measure(login, password, pathways=pws)
+#
+#fname = "/neurospin/brainomics/2015_hippo_l1_gl_ovl/data/new_synaptic2.pickle"
+#f = open(fname, 'wb')
+#pickle.dump(genodata, f)
+#f.close()
+
+
+############################################################################
+####################### generate kegg.pickle ###############################
+############################################################################
+
+#fn = '/neurospin/brainomics/bio_resources/genesets/msigdb-v3.1/c2.cp.kegg.v3.1.symbols.gmt'
+#origin = "msigdb-v3.1"
+#compound_name = 'c2.cp.kegg'
+#pws = Pathways(fn=fn, origin=origin, compound_name=compound_name)
+#
+##list patwhways names
+#print '====================================================================='
+#print 'pathways names contained in this set'
+#for pw in pws:
+#    print pw['name']
+#
+#pws.filter(min_gen=5)
+#
+##list patwhways names:
+#print '====================================================================='
+#print 'pathways names contained in this set limited to 10'
+#for pw in pws:
+#    print pw['name']
+#
+## create a complete set
+#login = raw_input("Login on the imagen2 server:")
+#password = getpass.getpass("Password on the imagen2 server:")
+##genodata = imagen_genotype_measure_alt(login, password, status="qc",
+##                                    gene_names=list(geneset))
+#genodata = imagen_genotype_measure(login, password, pathways=pws)
+#
+#fname = '/neurospin/brainomics/2015_hippo_l1_gl_ovl/data/kegg.pickle'
+#f = open(fname, 'wb')
+#pickle.dump(genodata, f)
+#f.close()
--- 2015_hippo_l1_gl_ovl/20_eval_pw.py
+++ 2015_hippo_l1_gl_ovl/20_eval_pw.py
@@ -15,72 +15,168 @@ import matplotlib.pyplot as plt
 from matplotlib import patches
 
 
-def genomic_plot(beta, genodata, selPathWay=None):
+def genomic_plot(beta, genodata, pw_index=None):
     """matplotlib plot of the estimated weights in genomic context
 
     Parameters:
     -----------
-    beta: float array(should be same len as genodta.data.shape[1])
+    beta: float array(should be same len as genodata.data.shape[1])
     genodata: the genotyping data and information
-    selPW: int to sel the PW of interes
+    pw_index: int, index of pathway of interest
     """
-    #int.
-    color = '#e0eee0'
 
-    groups_descr = genodata.get_meta_pws()
-    groups_name = groups_descr.keys()
-    groups = [list(groups_descr[n]) for n in groups_name]
-    chrList = numpy.unique(genodata.get_meta()[1])
-    selPW = numpy.zeros(genodata.data.shape[1])
-    #si selection
-    if selPathWay is not None:
-        selPW[groups_descr[groups_name[selPathWay]]] = 1.
-    lig = len(chrList) % 2 and (len(chrList) / 2 + 1) or len(chrList) / 2
-    for i, chrom in enumerate(chrList):
-        print " ", chrom
-        # prepare subplot for chrom
-        ax = plt.subplot(lig, 2, i)
+    """ indexes_of_pw: dict that maps pathway name to indexes of snp measures
+        associated to the pathway (indexes of columns in genodata)
+    """
+    indexes_of_pw = genodata.get_meta_pws()
+    pw_names      = indexes_of_pw.keys()
+    
+    chrom_names   = list(set(genodata.get_meta()[1])) # kill redundancy
+    chrom_to_int  = lambda x: 23 if x == "X" else 24 if x == "Y" else int(x)
+    chrom_names   = sorted(chrom_names, key=chrom_to_int)
+    nb_chroms     = len(chrom_names)
+    
+    # if selected pathway (pw)
+    if pw_index is not None:
+        pw_name     = pw_names[pw_index]
+        _pw_indexes = indexes_of_pw[pw_name]
+        pw_gnames   = set(numpy.asarray(genodata.get_meta()[2])[_pw_indexes])
+    
+    nb_rows = nb_chroms / 2 if nb_chroms % 2 == 0 else nb_chroms / 2 + 1
+    
+    y_max = 1.3*max(beta) # 10% bigger that max to keep space for gene name
+    y_min = 0
+
+    # set background of figure as white
+    fig = plt.figure()
+    fig.patch.set_facecolor("white")
+    
+    import matplotlib.gridspec as gridspec
+    gs = gridspec.GridSpec(nb_rows, 2)
+    
+    for i, chrom_name in enumerate(chrom_names):
+        
+        ax = plt.subplot(gs[int(i/2), i%2]) # row-ordered
+        #ax = plt.subplot(gs[i % nb_rows, int(i/nb_rows)]) # column-ordered
+        
         ax.set_yticklabels(())
         ax.set_xticklabels(())
-        ax.text(.10, 0.10, chrom,
-                horizontalalignment='center',
-                transform=ax.transAxes)
-        ax.title.set_visible(False)
-        plt.ylim(-1, 1)
-        # now get the data
-        indx = numpy.where(numpy.asarray(genodata.get_meta()[1]) == chrom)[0]
-        py = beta[indx]
-        if selPW is not None:
-            blackIndx = numpy.where(selPW[indx] == 0.)[0]
-            redIndx = numpy.where(selPW[indx] != 0.)[0]
-        px = range(len(py))
-        plt.xlim(numpy.min(px) - 0.5, numpy.max(px) + 0.5)
-        if len(blackIndx) != 0:
-            plt.vlines(numpy.asarray(px)[blackIndx],
-                       [0], numpy.asarray(py)[blackIndx], lw=2, colors='black')
-        if len(redIndx) != 0:
-            plt.vlines(numpy.asarray(px)[redIndx],
-                       [0], numpy.asarray(py)[redIndx], lw=2, colors='red')
-        # now plot the gene frontier
-        allGenesLag = numpy.asarray(genodata.get_meta()[2])[indx]
-        geneSet = set(allGenesLag)
-        for g in geneSet:
-            geneLag = numpy.where(allGenesLag == g)[0]
-            if numpy.sum(selPW[indx][geneLag]) != 0.:  # gene bel. to sel'ed pw
-                color = '#ffefd5'
-            else:
-                color = '#e0eee0'
-            px = [numpy.min(geneLag) - 0.5, numpy.max(geneLag) + 0.5]
-            py = [1, 1]
-            plt.vlines(px, [-1], [1], lw=1, colors='white')
-            p = patches.Rectangle((px[0], -1), px[1] - px[0], 2, color=color)
-            ax.add_patch(p)
-            ax.text(int(numpy.mean(px)), -0.5, g,
-                horizontalalignment='center',
-                fontsize=8, color='green')
 
-    plt.subplots_adjust(left=0.01, right=0.99, top=0.99,
-                        bottom=0.01, wspace=0.01)
+        ax.text(1.0, 0.5, chrom_name, horizontalalignment="left",
+                verticalalignment="center", fontsize=14, transform=ax.transAxes)
+        
+        #ax.title.set_visible(False)
+        plt.ylim(y_min, y_max)
+        ax.axhline(0, color="black", lw=2)
+
+        # row label with chromosome names in genodata
+        _chrom_names  = numpy.asarray(genodata.get_meta()[1])
+        
+        # chrom_indexes: columns in genodata for the current chromosome
+        chrom_indexes = numpy.where(_chrom_names == chrom_name)[0]
+        
+        # gene names associated to the current chromosome in genodata
+        chrom_gnames = numpy.asarray(genodata.get_meta()[2])[chrom_indexes]
+        
+        print chrom_name, ": ", len(chrom_indexes)
+
+        ys       = (beta[chrom_indexes]) # absolute values
+        nb_snps  = len(ys)
+        nb_genes = len(set(chrom_gnames))
+        
+        total_space     = 1.0 # 100%
+        total_intergene_space = 0.2 * total_space # 20% of space accorded to separate genes
+        intergene_space = total_intergene_space / (nb_genes + 1.0)
+        intersnp_space  = (total_space - total_intergene_space) / (nb_snps-nb_genes)
+
+        #import pdb
+        #pdb.set_trace()
+
+        xs_snps        = []
+        xs_genes       = []       # list of tuples (x start, x end, name)
+        xs_intergenes  = []       # list of tuples (x start, x end)
+        previous_gname = "blabla" # dumb initialization
+        gene_start     = intergene_space # initialization: start of gene
+        
+        """ pw_indexes: indexes of snps of the pathway of interest in xs, ys 
+            (for the current chromosome)
+        """
+        pw_indexes = []
+        
+        """ Building xs_snps, xs_intergenes, xs_genes:
+            abscisses of snps, intergene regions and genes
+        """
+        for j in range(len(ys)):
+            gname = chrom_gnames[j]
+            if gname in pw_gnames:
+                pw_indexes.append(j)
+            if gname != previous_gname:
+                previous_x = xs_snps[-1] if xs_snps else 0
+                next_x     = previous_x + intergene_space
+                xs_snps.append(next_x)
+                xs_intergenes.append((previous_x, next_x))
+                if previous_x != 0:
+                    xs_genes.append((gene_start, previous_x, previous_gname))
+                gene_start     = next_x # new gene just started
+                previous_gname = gname  # for next iteration
+            else:
+                previous_x = xs_snps[-1]
+                next_x     = previous_x + intersnp_space
+                xs_snps.append(next_x)
+        
+        xs_genes.append((gene_start, next_x, gname))
+        xs_intergenes.append((next_x, total_space))
+
+        plt.xlim(0, total_space)
+        
+        assert len(xs_snps) == len(ys)
+        
+        plt.vlines(xs_snps, [0], ys, colors="black", linewidth=1)
+        
+        xs_selected = [xs_snps[_i] for _i in pw_indexes]
+        ys_selected = [ys[_i]      for _i in pw_indexes]
+        
+        assert len(xs_selected) == len(ys_selected)
+        
+        plt.vlines(xs_selected, [0], ys_selected, linewidth=1, colors="red")
+        
+        """ intergene patches """
+        for intergene_start, intergene_end in xs_intergenes:
+            ax.add_patch(patches.Rectangle((intergene_start, y_min),
+                                           intergene_end - intergene_start,
+                                           y_max - y_min, fill=False,
+                                           color="red", hatch="/"))        
+
+        """ To avoid overlapping of gene names we use four different heights
+            for gene names (y-axis positions). This generator returns 
+            alternatively the four positions on the y-axis.
+        """
+        def alternate_gene_name_position(): # up and down to avoid overlapping
+            while True:
+                yield  y_max*.8
+                yield  y_min - 0.15*y_max
+                yield  y_max*1
+                #yield -1.9
+        
+        
+        """ gene patches and names """
+        position = alternate_gene_name_position() # generator to get y position
+        for gene_start, gene_end, gene_name in xs_genes:
+
+            # color: yellow for the genes in the pathway of interest else green
+            color = "yellow" if gene_name in pw_gnames else "#e0eee0"
+            
+            ax.add_patch(patches.Rectangle((gene_start, y_min),
+                                           gene_end - gene_start,
+                                           y_max - y_min, color=color))
+
+            ax.text(numpy.mean((gene_start, gene_end)), position.next(),
+                    gene_name, horizontalalignment="center", fontsize=8, color="blue")
+
+#    plt.subplots_adjust(left=0.01, right=0.99, top=0.99,
+#                        bottom=0.01, wspace=0.005)
+    plt.subplots_adjust(left=0.01, right=0.98, top=0.98,
+                        bottom=0.02, wspace=0.03, hspace=0.3) 
     plt.show()
 
 
@@ -125,9 +221,9 @@ def score_genodata(beta, genodata):
     return bestpaw_asof_snp, bestpaw_asof_gene
 
 #load prepared data for the project (see exemple_pw.py)
-fname = '/neurospin/brainomics/2015_hippo_l1_gl_ovl/data/synaptic10.pickle'
-fname = '/neurospin/brainomics/2015_hippo_l1_gl_ovl/data/synapticAll.pickle'
-fname = '/neurospin/brainomics/2015_hippo_l1_gl_ovl/data/kegg.pickle'
+#fname = '/neurospin/brainomics/2015_hippo_l1_gl_ovl/data/new_synaptic10.pickle'
+fname = '/neurospin/brainomics/2015_hippo_l1_gl_ovl/data/new_synapticAll.pickle'
+#fname = '/neurospin/brainomics/2015_hippo_l1_gl_ovl/data/kegg.pickle'
 f = open(fname)
 genodata = pickle.load(f)
 f.close()
