c13dcfc87971f12131c8b6136c0ddac9c551808e
Mathieu Dubois <mathieu.dubois@cea.fr>
2015-08-19 17:39:58 +0200

    [ENH] Generate and store masks only once. Minor variable renaming.

--- 2014_pca_struct/dice5/00_generate_data.py
+++ 2014_pca_struct/dice5/00_generate_data.py
@@ -29,8 +29,9 @@ import dice5_data
 ################
 
 OUTPUT_BASE_DIR = "/neurospin/brainomics/2014_pca_struct/dice5/data"
-
-OUTPUT_DIR_FORMAT = os.path.join(OUTPUT_BASE_DIR, "data_{s[0]}_{s[1]}_{snr}")
+OUTPUT_MASK_DIR = "/neurospin/brainomics/2014_pca_struct/dice5/data/masks"
+OUTPUT_DATA_DIR_FORMAT = os.path.join(OUTPUT_BASE_DIR,
+                                      "data_{s[0]}_{s[1]}_{snr}")
 OUTPUT_DATASET_FILE = "data.npy"
 OUTPUT_STD_DATASET_FILE = "data.std.npy"
 OUTPUT_BETA_FILE = "beta3d.std.npy"
@@ -53,6 +54,24 @@ SNRS = dice5_data.ALL_SNRS
 # Code #
 ########
 
+# Generate objects and masks (independant of the SNR)
+if not os.path.exists(OUTPUT_MASK_DIR):
+    os.makedirs(OUTPUT_MASK_DIR)
+# All objects
+objects = dice5.dice_five_with_union_of_pairs(dice5_data.SHAPE)
+# We only use union12, d3, union45
+_, _, d3, _, _, union12, union45, _ = objects
+sub_objects = [union12, union45, d3]
+full_mask = np.zeros(dice5_data.SHAPE, dtype=bool)
+for i, o in enumerate(sub_objects):
+    mask = o.get_mask()
+    full_mask += mask
+    filename = OUTPUT_OBJECT_MASK_FILE_FORMAT.format(i=i)
+    full_filename = os.path.join(OUTPUT_MASK_DIR, filename)
+    np.save(full_filename, mask)
+full_filename = os.path.join(OUTPUT_MASK_DIR, OUTPUT_MASK_FILE)
+np.save(full_filename, full_mask)
+
 # Generate data for all the SNR values
 for snr in SNRS:
     model = dice5_data.create_model(snr)
@@ -60,10 +79,9 @@ for snr in SNRS:
                                 shape=dice5_data.SHAPE,
                                 model=model,
                                 random_seed=dice5_data.SEED)
-    objects = dice5.dice_five_with_union_of_pairs(dice5_data.SHAPE)
     # Save data and scaled data
-    output_dir = OUTPUT_DIR_FORMAT.format(s=dice5_data.SHAPE,
-                                          snr=snr)
+    output_dir = OUTPUT_DATA_DIR_FORMAT.format(s=dice5_data.SHAPE,
+                                               snr=snr)
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     X = X3d.reshape(dice5_data.N_SAMPLES, np.prod(dice5_data.SHAPE))
@@ -76,20 +94,6 @@ for snr in SNRS:
     # Save beta
     full_filename = os.path.join(output_dir, OUTPUT_BETA_FILE)
     np.save(full_filename, beta3d)
-
-    # Generate mask with the last objects since they have the same geometry
-    # We only use union12, d3, union45
-    _, _, d3, _, _, union12, union45, _ = objects
-    sub_objects = [union12, union45, d3]
-    full_mask = np.zeros(dice5_data.SHAPE, dtype=bool)
-    for i, o in enumerate(sub_objects):
-        mask = o.get_mask()
-        full_mask += mask
-        filename = OUTPUT_OBJECT_MASK_FILE_FORMAT.format(i=i)
-        full_filename = os.path.join(output_dir, filename)
-        np.save(full_filename, mask)
-    full_filename = os.path.join(output_dir, OUTPUT_MASK_FILE)
-    np.save(full_filename, full_mask)
 
     # Compute l1_max for this dataset
     l1_max = pca_tv.PCA_L1_L2_TV.l1_max(X_std)
