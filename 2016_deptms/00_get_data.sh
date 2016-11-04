8498be11d81904881640aa694053355a2906658b
duchesnay <edouard.duchesnay@gmail.com>
2016-11-04 13:53:28 +0100

    2016_deptms: convert analyze to niftii

--- 2016_deptms/00_get_data.sh
+++ 2016_deptms/00_get_data.sh
@@ -1,25 +1,30 @@
 # Copy T1 images
 # ==============
 
-cp /volatile/duchesnay/data/11Deprim.tgz /neurospin/brainomics/2016_deptms/data_original/
-cd /volatile/duchesnay/data/
+#cp /volatile/duchesnay/data/11Deprim.tgz /neurospin/brainomics/2016_deptms/data_original/
+#cd /volatile/duchesnay/data/
+
+WD="/neurospin/brainomics/2016_deptms/data_original"
+cd $WD
 tar xzvf 11Deprim.tgz
 
 cd 11Deprim/data/mri
 ls */*.img|wc
-     34      34     572
-output="/neurospin/brainomics/2016_deptms/data_original"
+#     34      34     572
+output="$WD/t1"
 ls */*.img|while read input_img ; do 
         subject=$(dirname $input_img)
-        output_dirname="$output/$subject"
+        #output_dirname="$output/$subject"
+        output_dirname=$output
         tmp=$(basename $input_img)
         input_basename="${tmp%.img}"
-        mkdir ${output_dirname}
+        #mkdir ${output_dirname}
         cp $subject/$subject.* ${output_dirname}
+        fsl5.0-fslchfiletype NIFTI "${output_dirname}/$subject.img"
         #$input_basename;
 done
 
-ls ${output}/*/*.img|wc
+ls ${output}/*.nii|wc
      34      34    2204
 
 
@@ -33,7 +38,6 @@ import pandas as pd
 
 INPUT='/neurospin/brainomics/2014_deptms/base_data/clinic/deprimPetInfo.csv'
 OUTPUT='/neurospin/brainomics/2016_deptms/data_original'
-pop = pd.read_csv(INPUT, sep="\t")
 
 data = pd.read_csv(INPUT, sep='\t')
 
@@ -41,4 +45,10 @@ data["subject"] = [f.replace('smwc1', '').replace('.img', '') for f in data.MRIm
 
 np.all([os.path.exists(os.path.join(OUTPUT, s, s+".img")) for s in data.subject])
 data.to_csv(os.path.join(OUTPUT, 'deptms_info.csv'), index=False)
+
+
+## Convert to niftii
+#WD="/neurospin/brainomics/2016_deptms/data_original"
+#cd $WD
+#ls *.img|while read input_img ; do fsl5.0-fslchfiletype NIFTI $input_img ; done
 
