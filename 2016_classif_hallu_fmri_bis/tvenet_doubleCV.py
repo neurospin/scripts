a5c2031b28afe4251489c70eea3967f276ec2841
Amicie DE PIERREFEU <ad247405@is222241.intra.cea.fr>
2017-06-16 11:31:41 +0200

    scripts update

--- 2016_classif_hallu_fmri_bis/tvenet_doubleCV.py
+++ 2016_classif_hallu_fmri_bis/tvenet_doubleCV.py
@@ -17,32 +17,28 @@ import os
 import json
 import numpy as np
 from sklearn.cross_validation import StratifiedKFold
-import nibabel
 from sklearn.metrics import precision_recall_fscore_support
-from sklearn.feature_selection import SelectKBest
-from parsimony.estimators import LogisticRegressionL1L2TV
-import parsimony.functions.nesterov.tv as tv_helper
-import brainomics.image_atlas
-import parsimony.algorithms as algorithms
-import parsimony.datasets as datasets
-import parsimony.functions.nesterov.tv as nesterov_tv
 import parsimony.estimators as estimators
 import parsimony.algorithms as algorithms
 import parsimony.utils as utils
+from parsimony.utils.linalgs import LinearOperatorNesterov
 from scipy.stats import binom_test
 from collections import OrderedDict
 from sklearn import preprocessing
-from sklearn.metrics import roc_auc_score, recall_score
+from sklearn.metrics import roc_auc_score
 import pandas as pd
-from collections import OrderedDict
 import shutil
+from brainomics import array_utils
+import mapreduce
+from statsmodels.stats.inter_rater import fleiss_kappa
 
-BASE_PATH="/neurospin/brainomics/2016_classif_hallu_fmri_bis"
-#WD = os.path.join(BASE_PATH,"results/multivariate_analysis","logistic_regression_tv/model_selection")
 WD = '/neurospin/brainomics/2016_classif_hallu_fmri_bis/2017/results_10000ite'
 def config_filename(): return os.path.join(WD,"config_dCV.json")
 def results_filename(): return os.path.join(WD,"results_dCV.xlsx")
 #############################################################################
+NFOLDS_OUTER = 37
+NFOLDS_INNER = 5
+penalty_start = 0
 
 
 def load_globals(config):
@@ -94,9 +90,10 @@ def mapper(key, output_collector):
 
 
 
-def scores(key, paths, config, ret_y=False):
+def scores(key, paths, config, as_dataframe=False):
     import mapreduce
-    print (key)
+    print(key)
+    assert len(paths) == NFOLDS_INNER or len(paths) == NFOLDS_OUTER, "Failed for key %s" % key
     values = [mapreduce.OutputCollector(p) for p in paths]
     values = [item.load() for item in values]
     y_true = [item["y_true"].ravel() for item in values]
@@ -105,21 +102,61 @@ def scores(key, paths, config, ret_y=False):
     y_pred = np.concatenate(y_pred)
     prob_pred = [item["proba_pred"].ravel() for item in values]
     prob_pred = np.concatenate(prob_pred)
+
+    # Prediction performances
     p, r, f, s = precision_recall_fscore_support(y_true, y_pred, average=None)
     auc = roc_auc_score(y_true, prob_pred) #area under curve score.
-    betas = np.hstack([item["beta"] for item in values]).T
-    # threshold betas to compute fleiss_kappa and DICE
-    import array_utils
-    betas_t = np.vstack([array_utils.arr_threshold_from_norm2_ratio(betas[i, :], .99)[0] for i in range(betas.shape[0])])
+
+    # P-values
     success = r * s
     success = success.astype('int')
-    accuracy = (r[0] * s[0] + r[1] * s[1])
-    accuracy = accuracy.astype('int')
     prob_class1 = np.count_nonzero(y_true) / float(len(y_true))
-    pvalue_recall0 = binom_test(success[0], s[0], 1 - prob_class1)
-    pvalue_recall1 = binom_test(success[1], s[1], prob_class1)
-    pvalue_accuracy = binom_test(accuracy, s[0] + s[1], p=0.5)
+    pvalue_recall0_true_prob = binom_test(success[0], s[0], 1 - prob_class1,alternative = 'greater')
+    pvalue_recall1_true_prob = binom_test(success[1], s[1], prob_class1,alternative = 'greater')
+    pvalue_recall0_unknwon_prob = binom_test(success[0], s[0], 0.5,alternative = 'greater')
+    pvalue_recall1_unknown_prob = binom_test(success[1], s[1], 0.5,alternative = 'greater')
+    pvalue_recall_mean = binom_test(success[0]+success[1], s[0] + s[1], p=0.5,alternative = 'greater')
+
+
+    # Beta's measures of similarity
+    betas = np.hstack([item["beta"][penalty_start:, :] for item in values]).T
+
+    # Correlation
+    R = np.corrcoef(betas)
+    #print R
+    R = R[np.triu_indices_from(R, 1)]
+    # Fisher z-transformation / average
+    z_bar = np.mean(1. / 2. * np.log((1 + R) / (1 - R)))
+    # bracktransform
+    r_bar = (np.exp(2 * z_bar) - 1) /  (np.exp(2 * z_bar) + 1)
+
+    # threshold betas to compute fleiss_kappa and DICE
+    try:
+        betas_t = np.vstack([array_utils.arr_threshold_from_norm2_ratio(betas[i, :], .99)[0] for i in range(betas.shape[0])])
+        #print "--", np.sqrt(np.sum(betas_t ** 2, 1)) / np.sqrt(np.sum(betas ** 2, 1))
+        #print(np.allclose(np.sqrt(np.sum(betas_t ** 2, 1)) / np.sqrt(np.sum(betas ** 2, 1)), [0.99]*5,
+        #                   rtol=0, atol=1e-02))
+
+        # Compute fleiss kappa statistics
+        beta_signed = np.sign(betas_t)
+        table = np.zeros((beta_signed.shape[1], 3))
+        table[:, 0] = np.sum(beta_signed == 0, 0)
+        table[:, 1] = np.sum(beta_signed == 1, 0)
+        table[:, 2] = np.sum(beta_signed == -1, 0)
+        fleiss_kappa_stat = fleiss_kappa(table)
+
+        # Paire-wise Dice coeficient
+        ij = [[i, j] for i in range(betas.shape[0]) for j in range(i+1, betas.shape[0])]
+        dices = list()
+        for idx in ij:
+            A, B = beta_signed[idx[0], :], beta_signed[idx[1], :]
+            dices.append(float(np.sum((A == B)[(A != 0) & (B != 0)])) / (np.sum(A != 0) + np.sum(B != 0)))
+        dice_bar = np.mean(dices)
+    except:
+        dice_bar = fleiss_kappa_stat = 0
+
     scores = OrderedDict()
+    scores['key'] = key
     try:
         a, l1, l2 , tv  = [float(par) for par in key.split("_")]
         scores['a'] = a
@@ -131,19 +168,27 @@ def scores(key, paths, config, ret_y=False):
         scores['l1_ratio'] = float(l1) / left
     except:
         pass
-    scores['recall_mean'] = r.mean()
     scores['recall_0'] = r[0]
     scores['recall_1'] = r[1]
-    scores['accuracy'] = accuracy/ float(len(y_true))
-    scores['pvalue_recall_0'] = pvalue_recall0
-    scores['pvalue_recall_1'] = pvalue_recall1
-    scores['pvalue_accuracy'] = pvalue_accuracy
-    scores['max_pvalue_recall'] = np.maximum(pvalue_recall0, pvalue_recall1)
     scores['recall_mean'] = r.mean()
-    scores['auc'] = auc
+    scores["auc"] = auc
+    scores['pvalue_recall0_true_prob_one_sided'] = pvalue_recall0_true_prob
+    scores['pvalue_recall1_true_prob_one_sided'] = pvalue_recall1_true_prob
+    scores['pvalue_recall0_unknwon_prob_one_sided'] = pvalue_recall0_unknwon_prob
+    scores['pvalue_recall1_unknown_prob_one_sided'] = pvalue_recall1_unknown_prob
+    scores['pvalue_recall_mean'] = pvalue_recall_mean
     scores['prop_non_zeros_mean'] = float(np.count_nonzero(betas_t)) / \
-                                    float(np.prod(betas_t.shape))
-    scores['param_key'] = key
+                                    float(np.prod(betas.shape))
+    scores['beta_r_bar'] = r_bar
+    scores['beta_fleiss_kappa'] = fleiss_kappa_stat
+    scores['beta_dice_bar'] = dice_bar
+
+    scores['beta_dice'] = str(dices)
+    scores['beta_r'] = str(R)
+
+    if as_dataframe:
+        scores = pd.DataFrame([list(scores.values())], columns=list(scores.keys()))
+
     return scores
 
 
@@ -152,7 +197,10 @@ def reducer(key, values):
     os.chdir(os.path.dirname(config_filename()))
     config = json.load(open(config_filename()))
     paths = glob.glob(os.path.join(config['map_output'], "*", "*", "*"))
-    #paths = [p for p in paths if not p.count("0.8_-1")]
+    param_config_set = set([mapreduce.dir_from_param_list(p) for p in config['params']])
+    assert len(paths) / len(param_config_set) == len(config['resample']), "Nb run per param is not the one excpected"
+    paths.sort()
+    # assert len(paths) == 6603
 
     def close(vec, val, tol=1e-4):
         return np.abs(vec - val) < tol
@@ -163,15 +211,15 @@ def reducer(key, values):
             groups[p.split("/")[pos]].append(p)
         return groups
 
-    def argmaxscore_bygroup(data, groupby='fold', param_key="param_key", score="auc"):
+    def argmaxscore_bygroup(data, groupby='fold', param_key="key", score="recall_mean"):
         arg_max_byfold = list()
         for fold, data_fold in data.groupby(groupby):
             assert len(data_fold) == len(set(data_fold[param_key]))  # ensure all  param are diff
             arg_max_byfold.append([fold, data_fold.ix[data_fold[score].argmax()][param_key], data_fold[score].max()])
         return pd.DataFrame(arg_max_byfold, columns=[groupby, param_key, score])
 
-    print ('## Refit scores')
-    print ('## ------------')
+    print('## Refit scores: cv*/refit/*')
+    print('## -------------------------')
     byparams = groupby_paths([p for p in paths if not p.count("cvnested") and not p.count("refit/refit") ], 3)
     byparams_scores = {k:scores(k, v, config) for k, v in byparams.items()}
 
@@ -180,40 +228,133 @@ def reducer(key, values):
     columns = list(byparams_scores[list(byparams_scores.keys())[0]].keys())
     scores_refit = pd.DataFrame(data, columns=columns)
 
-    print ('## doublecv scores by outer-cv and by params')
-    print ('## -----------------------------------------')
+    print('## doublecv scores by outer-cv and by params: cv*/cvnested*/*')
+    print('## -----------------------------------------')
     data = list()
     bycv = groupby_paths([p for p in paths if p.count("cvnested") and not p.count("refit/cvnested")  ], 1)
     for fold, paths_fold in bycv.items():
-        print (fold)
+        print(fold)
         byparams = groupby_paths([p for p in paths_fold], 3)
         byparams_scores = {k:scores(k, v, config) for k, v in byparams.items()}
         data += [[fold] + list(byparams_scores[k].values()) for k in byparams_scores]
     scores_dcv_byparams = pd.DataFrame(data, columns=["fold"] + columns)
+    assert np.all(np.array([g.shape[0] for d, g in scores_dcv_byparams.groupby('fold')]) == 84)
 
-    rm = (scores_dcv_byparams.prop_non_zeros_mean > 0.5)
-    np.sum(rm)
-    scores_dcv_byparams = scores_dcv_byparams[np.logical_not(rm)]
-    l1l2tv = scores_dcv_byparams[(scores_dcv_byparams.l1 != 0) & (scores_dcv_byparams.tv != 0)]
+    # Different settings
 
-    print ('## Model selection')
-    print ('## ---------------')
-    l1l2tv = argmaxscore_bygroup(l1l2tv); l1l2tv["method"] = "l1l2tv"
-    scores_argmax_byfold = l1l2tv
-    print ('## Apply best model on refited')
-    print ('## ---------------------------')
-    print (l1l2tv)
-    print ([row for index, row in l1l2tv.iterrows()])
-    scores_l1l2tv = scores("nestedcv", [os.path.join(config['map_output'], row["fold"], "refit", row["param_key"]) for index, row in l1l2tv.iterrows()], config)
-    scores_cv = pd.DataFrame([
-                  ["l1l2tv"] + list(scores_l1l2tv.values())], columns=["method"] + list(scores_l1l2tv.keys()))
-    print((list(scores_l1l2tv.values()) ))
+    l1l2tv_all = scores_dcv_byparams
+
+    l1l2tv_reduced = scores_dcv_byparams[
+        (close(scores_dcv_byparams.a, 0.01) | close(scores_dcv_byparams.a, 0.1) | close(scores_dcv_byparams.a, 1.00)) &
+        (close(scores_dcv_byparams.l1_ratio, 0.1) | close(scores_dcv_byparams.l1_ratio, 0.9)) &
+        (close(scores_dcv_byparams.tv, 0.2) | close(scores_dcv_byparams.tv, 0.8))]
+    assert np.all(np.array([g.shape[0] for d, g in l1l2tv_reduced.groupby('fold')]) == 12)
+    assert l1l2tv_reduced.shape[0] == 444
+
+    l1l2tv_ridge_reduced = scores_dcv_byparams[
+        (close(scores_dcv_byparams.a, 0.01) | close(scores_dcv_byparams.a, 0.1) | close(scores_dcv_byparams.a, 1.00)) &
+        (close(scores_dcv_byparams.l1_ratio, 0.1)) &
+        (close(scores_dcv_byparams.tv, 0.2) | close(scores_dcv_byparams.tv, 0.8))]
+    assert np.all(np.array([g.shape[0] for d, g in l1l2tv_ridge_reduced.groupby('fold')]) == 6)
+    assert l1l2tv_ridge_reduced.shape[0] == 222
+
+    l1l2tv_lasso_reduced = scores_dcv_byparams[
+        (close(scores_dcv_byparams.a, 0.01) | close(scores_dcv_byparams.a, 0.1) | close(scores_dcv_byparams.a, 1.00)) &
+        (close(scores_dcv_byparams.l1_ratio, 0.9)) &
+        (close(scores_dcv_byparams.tv, 0.2) | close(scores_dcv_byparams.tv, 0.8))]
+    assert np.all(np.array([g.shape[0] for d, g in l1l2tv_lasso_reduced.groupby('fold')]) == 6)
+    assert l1l2tv_lasso_reduced.shape[0] == 222
+
+    l1l2_reduced = scores_dcv_byparams[
+        (close(scores_dcv_byparams.a, 0.01) | close(scores_dcv_byparams.a, 0.1)| close(scores_dcv_byparams.a, 1.00)) &
+        (close(scores_dcv_byparams.l1_ratio, 0.1) | close(scores_dcv_byparams.l1_ratio, 0.9)) &
+        (close(scores_dcv_byparams.tv, 0))]
+    assert np.all(np.array([g.shape[0] for d, g in l1l2_reduced.groupby('fold')]) == 6)
+    assert l1l2_reduced.shape[0] == 222
+
+#    l1l2_ridge_reduced = scores_dcv_byparams[
+#        (close(scores_dcv_byparams.a, 0.01) | close(scores_dcv_byparams.a, 0.1)| close(scores_dcv_byparams.a, 1.00)) &
+#        (close(scores_dcv_byparams.l1_ratio, 0.1)) &
+#        (close(scores_dcv_byparams.tv, 0))]
+#    assert np.all(np.array([g.shape[0] for d, g in l1l2_ridge_reduced.groupby('fold')]) == 3)
+#    assert l1l2_ridge_reduced.shape[0] == 111
+#
+#    l1l2_lasso_reduced = scores_dcv_byparams[
+#        (close(scores_dcv_byparams.a, 0.01) | close(scores_dcv_byparams.a, 0.1)| close(scores_dcv_byparams.a, 1.00)) &
+#        (close(scores_dcv_byparams.l1_ratio, 0.9)) &
+#        (close(scores_dcv_byparams.tv, 0))]
+#    assert np.all(np.array([g.shape[0] for d, g in l1l2_lasso_reduced.groupby('fold')]) == 3)
+#    assert l1l2_lasso_reduced.shape[0] == 111
+    print('## Model selection')
+    print('## ---------------')
+    l1l2tv_all = argmaxscore_bygroup(l1l2tv_all); l1l2tv_all["method"] = "l1l2tv_all"
+
+    l1l2tv_reduced = argmaxscore_bygroup(l1l2tv_reduced); l1l2tv_reduced["method"] = "l1l2tv_reduced"
+
+    l1l2tv_ridge_reduced = argmaxscore_bygroup(l1l2tv_ridge_reduced); l1l2tv_ridge_reduced["method"] = "l1l2tv_ridge_reduced"
+
+    l1l2tv_lasso_reduced = argmaxscore_bygroup(l1l2tv_lasso_reduced); l1l2tv_lasso_reduced["method"] = "l1l2tv_lasso_reduced"
+
+    #l1l2_reduced = argmaxscore_bygroup(l1l2_reduced); l1l2_reduced["method"] = "l1l2_reduced"
+
+    #l1l2_ridge_reduced = argmaxscore_bygroup(l1l2_ridge_reduced); l1l2_ridge_reduced["method"] = "l1l2_ridge_reduced"
+
+    #l1l2_lasso_reduced = argmaxscore_bygroup(l1l2_lasso_reduced); l1l2_lasso_reduced["method"] = "l1l2_lasso_reduced"
+
+    scores_argmax_byfold = pd.concat([l1l2tv_all,
+                                      l1l2tv_reduced,
+                                      l1l2tv_ridge_reduced,
+                                      l1l2tv_lasso_reduced])
+    #, l1l2_lasso_reduced]) #l1l2_ridge_reduced,
+
+    print('## Apply best model on refited')
+    print('## ---------------------------')
+    l1l2tv_all = scores("l1l2tv_all",
+                               [os.path.join(config['map_output'], row["fold"], "refit", row["key"])
+                                   for index, row in l1l2tv_all.iterrows()],
+                                config, as_dataframe=True)
+
+    l1l2tv_reduced = scores("l1l2tv_reduced",
+                            [os.path.join(config['map_output'], row["fold"], "refit", row["key"])
+                                for index, row in l1l2tv_reduced.iterrows()],
+                             config, as_dataframe=True)
+
+    l1l2tv_ridge_reduced = scores("l1l2tv_ridge_reduced",
+                                   [os.path.join(config['map_output'], row["fold"], "refit", row["key"])
+                                       for index, row in l1l2tv_ridge_reduced.iterrows()],
+                                    config, as_dataframe=True)
+
+    l1l2tv_lasso_reduced = scores("l1l2tv_lasso_reduced",
+                                   [os.path.join(config['map_output'], row["fold"], "refit", row["key"])
+                                       for index, row in l1l2tv_lasso_reduced.iterrows()],
+                                    config, as_dataframe=True)
+
+#    l1l2_reduced = scores("l1l2_reduced",
+#                                 [os.path.join(config['map_output'], row["fold"], "refit", row["key"])
+#                                     for index, row in l1l2_reduced.iterrows()],
+#                                 config, as_dataframe=True)
+#
+#    l1l2_ridge_reduced = scores("l1l2_ridge_reduced",
+#                                 [os.path.join(config['map_output'], row["fold"], "refit", row["key"])
+#                                     for index, row in l1l2_ridge_reduced.iterrows()],
+#                                 config, as_dataframe=True)
+#
+#    l1l2_lasso_reduced = scores("l1l2_lasso_reduced",
+#                                 [os.path.join(config['map_output'], row["fold"], "refit", row["key"])
+#                                     for index, row in l1l2_lasso_reduced.iterrows()],
+#                                 config, as_dataframe=True)
+
+    scores_cv = pd.concat([l1l2tv_all,
+                           l1l2tv_reduced,
+                           l1l2tv_ridge_reduced,
+                           l1l2tv_lasso_reduced,
+                           ])
+
     with pd.ExcelWriter(results_filename()) as writer:
-        scores_refit.to_excel(writer, sheet_name='scores_refit', index=False)
-        scores_dcv_byparams.to_excel(writer, sheet_name='scores_dcv_byparams', index=False)
-        scores_argmax_byfold.to_excel(writer, sheet_name='scores_argmax_byfold', index=False)
-        scores_cv.to_excel(writer, sheet_name='scores_cv', index=False)
-
+        scores_refit.to_excel(writer, sheet_name='cv_by_param', index=False)
+        scores_dcv_byparams.to_excel(writer, sheet_name='cv_cv_byparam', index=False)
+        scores_argmax_byfold.to_excel(writer, sheet_name='cv_argmax', index=False)
+        scores_cv.to_excel(writer, sheet_name='dcv', index=False)
 ##############################################################################
 
 
