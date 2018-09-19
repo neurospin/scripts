acaae952d6543050e40397ce2c24a1199cbc9842
STOUT Jacques <JS247994@intra.cea.fr>
2018-09-19 11:20:52 +0200

    Updates to smooth final pipeline

--- 2017_bipli_lithium_imaging/launch_reconstruct_TPI_all.m
+++ 2017_bipli_lithium_imaging/launch_reconstruct_TPI_all.m
@@ -1,47 +1,55 @@
 function launch_reconstruct_TPI_all(projectdir)%,subjectdir,subjectnumber,reconstructfile,Pythonexe )
 
     %launch_reconstruct_TPI('C:\Users\js247994\Documents\Bipli2\Test8\Raw','C:\Users\js247994\Documents\Bipli2\Test8\Processed','C:\Users\js247994\Documents\Bipli2\BipliPipeline\scripts\2017_bipli_lithium_imaging\ReconstructionTPI','C:\Python27\python.exe')
-    projectdir='/neurospin/ciclops/projects/BIPLi7/ClinicalData';
-    pythonexe='python';
-    raw_dir=fullfile(projectdir,'Raw_Data','2018*');
-    currentfolder=pwd;
-    reconstructfile=fullfile(pwd,'reconstructionTPI','ProcessData.py');
-    reconstructfile=fullfile('/home/js247994/DocumentsN2/2017_bipli_lithium_imaging','reconstructionTPI','ProcessData.py');
+    if ~exist('Pythonexe','var')
+        pythonexe='python';
+        if ~exist('Codedir','var')
+            codedir=fullfile(char(pwd));
+        end
+    end
+    
+    projectdir='V:\projects\BIPLi7\ClinicalData';
+    raw_dir=fullfile(projectdir,'Raw_Data','2*');
+%    reconstructfile=fullfile('/home/js247994/DocumentsN2/2017_bipli_lithium_imaging','ReconstructionTPI','ProcessData.py');
     listsubj=dir(raw_dir);
 
-    for i = 1:numel(listsubj)
-        subjname=listsubj(i).name;
-        raw_subjdir=fullfile(projectdir,'Raw_Data',subjname,'twix7T');
-        proc_subjdir=fullfile(projectdir,'Processed_Data',subjname);
-        fieldmap_file=fullfile(proc_subjdir,'Field_mapping','fieldmap_final.nii');
-        if ~exist(fieldmap_file,'file')
-            spm_alignmentfile=fullfile(currentfolder,'info_pipeline','fieldmapwritespm.mat');
-            ref_im=dir(fullfile(projectdir,'*.nii'));
-            ref_im=fullfile(projectdir,string(ref_im.name));
-            prepare_fieldmap(projectdir,subjname,spm_alignmentfile,ref_im);
-        end
-        listdat=dir(raw_subjdir);
-        for j=1:numel(listdat)
-            ok=strfind(listdat(j).name,'TPI');
-            if ~isempty(ok)  
-                ProcessedTPIpath=fullfile(proc_subjdir,'TPI','Reconstruct_gridding','01-Raw');
-                if ~exist(ProcessedTPIpath,'dir')
-                    mkdir(ProcessedTPIpath)
+    excelT1s=fullfile(pwd,'info_pipeline','T1vals.xlsx');
+    if exist(excelT1s,'file')
+        [~,~,raw]=xlsread(excelT1s);
+        for l=1:size(raw,1)
+            for m=1:size(raw,2)
+                if strcmp('subj',raw(l,m))
+                    subjcol=m;
+                    subjstartl=l+1;
+                elseif strcmp('T1 vals',raw(l,m))
+                    T1col=m;
                 end
-                Tpifilename=listdat(j).name;
-                Tpifilepath=fullfile(raw_subjdir,char(Tpifilename));
-                deg=strfind(Tpifilename,'deg');
-                degval=(Tpifilename(deg-2:deg-1));
-                if i<10
-                    subjectnumber=strcat('0',int2str(i));
-                elseif i>=10
-                    subjectnumber=int2str(i);
-                end
-                TPIresultname=strcat('Patient',(subjectnumber),'_',degval,'deg.nii');
-                Reconstructpath=fullfile(char(ProcessedTPIpath),char(TPIresultname));
-                codelaunch=strcat(pythonexe,{' '},reconstructfile,{' --i '},Tpifilepath,{' --fieldmap '}, fieldmap_file, {' --NSTPI --s --FISTA_CSV --o '},Reconstructpath);
-                disp('hi');
             end
         end
+    end      
+    %try to get T1 values from excel file  
+    
+    for i = 1:numel(listsubj)
+        subjname=listsubj(i).name;    
+        
+        for l=subjstartl:size(raw,1)
+            if strcmp(raw(l,subjcol),subjname)
+                T1val=raw(l,T1col);
+                T1val=T1val{1};
+            end
+        end
+        
+        if i<10
+            subjectnumber=strcat('0',int2str(i));
+        elseif i>=10
+            subjectnumber=int2str(i);
+        end       
+        %maybe one day will be changed to actually include the value from
+        %the file?
+        T1val=3.947000;
+        launch_reconstruct_TPI(projectdir,subjname,codedir,T1val,pythonexe,subjectnumber)
+        %Check if the subject number of the patient is available, if not try to
+        %count the order of patients to get the right number
+
     end
 end
--- 2017_bipli_lithium_imaging/run_Compute_Quantif.m
+++ 2017_bipli_lithium_imaging/run_Compute_Quantif.m
@@ -1,143 +1,51 @@
-function run_Compute_Quantif(Rawdatdir,subject,Processeddir,subjectnumber,Codedir,Pythonexe)
-% run_Compute_Quantif('C:\Users\js247994\Documents\Bipli2\Biplisubjects','C:\Users\js247994\Documents\ReconstructionTPI','C:\Program Files (x86)\Python36-32\python.exe')
+function run_Compute_Quantif(Processeddir,subject,T1val,Codedir,Pythonexe)
+    % run_Compute_Quantif('C:\Users\js247994\Documents\Bipli2\Biplisubjects','C:\Users\js247994\Documents\ReconstructionTPI','C:\Program Files (x86)\Python36-32\python.exe')
 
+    %Launch the reconstruction of the dat files found in raw to the processed
+    %images and placed them in right location (should perhaps eventually be
+    %updated to also include FISTA?
+    %reconstructfile=fullfile(char(Codedir),'ProcessData.py');
+    %launch_reconstruct_TPI(Subjectdirtwix,Subjectdirresult,subjectnumber,reconstructfile,Pythonexe );
 
-%try to get T1 values from excel file
-excelT1s=fullfile(pwd,'info_pipeline','T1vals.xlsx');
-if exist(excelT1s,'file')
-    [~,~,raw]=xlsread(excelT1s);
-    for l=1:size(raw,1)
-        for m=1:size(raw,2)
-            if strcmp('subj',raw(l,m))
-                subjcol=m;
-                subjstartl=l+1;
-            elseif strcmp('T1 vals',raw(l,m))
-                T1col=m;
+    Subjectdirp=fullfile(Processeddir,subject);
+
+    filesdirin=fullfile(Subjectdirp,'TPI','Reconstruct_gridding','01-Raw');
+    filesdirout=fullfile(Subjectdirp,'TPI','Reconstruct_gridding','02-PostQuantif');
+    filesdone=[];
+    filesnii=dir(fullfile(filesdirin,'*.nii'));
+
+    ComputeVFAfile=fullfile(char(Codedir),'ReconstructionTPI','ComputeDensity3D_clinic.py');
+    ComputeQuantifile=fullfile(char(Codedir),'ReconstructionTPI','ComputeQuantif_clinic.py');
+
+    for file1=filesnii'
+        degloc=strfind(file1.name,'deg');
+        over=0;
+        for filesdon=filesdone'
+            if strcmp(file1.name,filesdon')
+                over=1;
             end
         end
-    end
-end
-
-
-if ~exist('Pythonexe','var')
-    Pythonexe='python';
-    if ~exist('Codedir','var')
-        Codedir=fullfile(char(pwd),'ReconstructionTPI');
-    end
-end
-
-%Check if the subject number of the patient is available, if not try to
-%count the order of patients to get the right number
-if ~exist('subjectnumber','var')
-    S = dir(Rawdatdir);
-    S=S(~ismember({S.name},{'.','..'}));
-    %nbsubjects=sum([S.isdir]);
-    i=0;
-    for subj=S'
-        if subj.isdir==1
-            i=i+1;
-                if subject==subj.name
-                    if ( i > 9 ) 
-                    subjectnumber =  int2str(i) ;
-                    else
-                        subjectnumber =  [ '0', int2str(i) ] ;
+        if ~isempty(degloc)
+            file1path=fullfile(filesdirin,file1.name);
+            deg1=file1.name(degloc-2:degloc-1);   
+            if ~over   
+                for file2=filesnii'
+                    VFA=~strcmp(file1.name,file2.name) && strcmp(file1.name(degloc:end),file2.name(degloc:end)); %If there is another file with the same name but a different degree value, it is treated as the second VFA file
+                    if VFA
+                        deg2=file2.name(degloc-2:degloc-1);
+                        Computedniipath=fullfile(filesdirout,strcat(file1.name(1:degloc-3),file1.name(degloc+4:end)));
+                        file2path=fullfile(filesdirin,file2.name);
+                        codelaunchVFA=strcat({'"'},Pythonexe,{'" '},ComputeVFAfile,{' --i1 '},file1path,{' --i2 '},file2path,{' --deg1 '},deg1,{' --deg2 '},deg2,{' --t1 '}, num2str(T1val), {' --v --o '},Computedniipath);
+                        status = system(codelaunchVFA{1});
+                        filesdone=[filesdone;file1.name;file2.name];
                     end
                 end
-        end
-    end
-end
-
-if ~exist(Processeddir,'dir')
-    mkdir(char(Processeddir))
-    mkdir(fullfile(char(Processeddir,subject)))
-else
-    if ~exist(fullfile(char(Processeddir),char(subject)),'dir')
-        mkdir(fullfile(char(Processeddir),char(subject)));
-    end
-end
-
-Subjectdirr=fullfile(char(Rawdatdir),char(subject));
-Subjectdirtwix=fullfile(Subjectdirr,'twix7T');
-Subjectdirp=fullfile(char(Processeddir),char(subject));
-Subjectdirresult=fullfile(Subjectdirp,'TPI','Reconstruct_gridding','01-Raw');
-
-%Create the processeddir subject folders if they are not already created
-if ~exist(fullfile(Subjectdirp,'Anatomy3T'),'dir')
-    mkdir(fullfile(Subjectdirp,'Anatomy3T'));
-end
-if ~exist(fullfile(Subjectdirp,'Anatomy7T'),'dir')
-    mkdir(fullfile(Subjectdirp,'Anatomy7T'));
-end
-if ~exist(fullfile(Subjectdirp,'Anatomy3T'),'dir')
-    mkdir(fullfile(Subjectdirp,'Anatomy3T'));
-end
-if ~exist(fullfile(Subjectdirp,'TPI'),'dir')
-    mkdir(fullfile(Subjectdirp,'TPI'));
-    mkdir(fullfile(Subjectdirp,'TPI','Reconstruct_gridding','01-Raw'));
-    mkdir(fullfile(Subjectdirp,'TPI','Reconstruct_gridding','02-Post-Quantif'));
-    mkdir(fullfile(Subjectdirp,'TPI','Reconstruct_gridding','03-Filtered'));
-    mkdir(fullfile(Subjectdirp,'TPI','Reconstruct_gridding','04-7Tanatspace'));
-    mkdir(fullfile(Subjectdirp,'TPI','Reconstruct_gridding','05-3Tanatspace'));
-    mkdir(fullfile(Subjectdirp,'TPI','Reconstruct_gridding','06-MNIspace'));
-end
-if ~exist(fullfile(Subjectdirp,'Trufi'),'dir')
-    mkdir(char(fullfile(Subjectdirp,'Trufi')));
-end
-
-%Launch the reconstruction of the dat files found in raw to the processed
-%images and placed them in right location (should perhaps eventually be
-%updated to also include FISTA?
-reconstructfile=fullfile(char(Codedir),'ProcessData.py');
-launch_reconstruct_TPI(Subjectdirtwix,Subjectdirresult,subjectnumber,reconstructfile,Pythonexe );
-
-
-filesdirin=fullfile(Subjectdirp,'TPI','Reconstruct_gridding','01-Raw');
-filesdirout=fullfile(Subjectdirp,'TPI','Reconstruct_gridding','02-PostQuantif');
-filesdone=[];
-filesnii=dir(fullfile(filesdirin,'*.nii'));
-
-ComputeVFAfile=fullfile(char(Codedir),'ComputeDensity3D_clinic.py');
-ComputeQuantifile=fullfile(char(Codedir),'ComputeQuantif_clinic.py');
-
-for l=subjstartl:size(raw,1)
-    if strcmp(raw(l,subjcol),subject)
-        T1valtest=raw(l,T1col);
-        T1valtest=T1valtest{1};
-    end
-end
-
-T1val=3.947000;
-
-for file1=filesnii'
-    degloc=strfind(file1.name,'deg');
-    over=0;
-    for filesdon=filesdone'
-        if strcmp(file1.name,filesdon')
-            over=1;
-        end
-    end
-    if ~isempty(degloc)
-        file1path=fullfile(filesdirin,file1.name);
-        deg1=file1.name(degloc-2:degloc-1);   
-        if ~over   
-            for file2=filesnii'
-                VFA=~strcmp(file1.name,file2.name) && strcmp(file1.name(degloc:end),file2.name(degloc:end)); %If there is another file with the same name but a different degree value, it is treated as the second VFA file
-                if VFA
-                    deg2=file2.name(degloc-2:degloc-1);
-                    Computedniipath=fullfile(filesdirout,strcat(file1.name(1:degloc-3),file1.name(degloc+4:end)));
-                    file2path=fullfile(filesdirin,file2.name);
-                    codelaunchVFA=strcat({'"'},Pythonexe,{'" '},ComputeVFAfile,{' --i1 '},file1path,{' --i2 '},file2path,{' --deg1 '},deg1,{' --deg2 '},deg2,{' --t1 '}, num2str(T1val), {' --v --o '},Computedniipath);
-                    status = system(codelaunchVFA{1});
-                    filesdone=[filesdone;file1.name;file2.name];
-                end
             end
+            %[~,filename,ext]=fileparts(file1.name);
+            %Computedniipath=strcat(filesdir,filename,ext);
+            Computedniipath=fullfile(filesdirout,file1.name);
+            codelaunchQuant=strcat({'"'},Pythonexe,{'" '},ComputeQuantifile,{' --i '},file1path,{' --deg '},deg1,{' --t1 '}, num2str(T1val), {' --v --o '},Computedniipath);
+            status = system(codelaunchQuant{1});
         end
-        %[~,filename,ext]=fileparts(file1.name);
-        %Computedniipath=strcat(filesdir,filename,ext);
-        Computedniipath=fullfile(filesdirout,file1.name);
-        codelaunchQuant=strcat({'"'},Pythonexe,{'" '},ComputeQuantifile,{' --i '},file1path,{' --deg '},deg1,{' --t1 '}, num2str(T1val), {' --v --o '},Computedniipath);
-        status = system(codelaunchQuant{1});
     end
 end
-
-  
