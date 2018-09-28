366c776fdcf5f4609375772a4ff5578edc5b1e17
duchesnay <edouard.duchesnay@gmail.com>
2018-09-28 17:44:50 +0200

    canbind clustering based on Ctl, plots

--- 2018_canbind/clustering/clustering_clf_img_v03.py
+++ 2018_canbind/clustering/clustering_clf_img_v03.py
@@ -20,6 +20,11 @@ import matplotlib.pyplot as plt
 import scipy, scipy.ndimage
 #import nilearn.plotting
 from nilearn import datasets, plotting, image
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_samples, silhouette_score
+from sklearn import preprocessing
+scaler = preprocessing.StandardScaler()
+import sklearn.metrics as metrics
 
 # cookiecutter Directory structure
 # https://drivendata.github.io/cookiecutter-data-science/
@@ -28,55 +33,55 @@ from nilearn import datasets, plotting, image
 # OUTPUT = os.path.join(WD, "models", "vbm_resp_%s" % vs)
 WD = os.path.join('/neurospin/psy/canbind/models/clustering_v03')
 
-# Clustering Im
-from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_samples, silhouette_score
-from sklearn import preprocessing
-scaler = preprocessing.StandardScaler()
-import sklearn.metrics as metrics
+
+###############################################################################
+# Population
+###############################################################################
+img_tot = np.load(os.path.join(WD, "XTotTivSiteCtr.npz"))
+pop_tot = pd.read_csv(os.path.join(WD, "population.csv"))
 
 # QC
-pop = pd.read_csv(os.path.join(WD, "population.csv"))
-img = np.load(os.path.join(WD, "XTotTivSiteCtr.npz"))
-assert np.all(img['participant_id'] == pop['participant_id'])
-assert np.all(img["treat_ses01_mask"] == pop.respond_wk16.notnull() & (pop.session == "ses-01"))
+assert np.all(img_tot['participant_id'] == pop_tot['participant_id'])
+assert np.all(img_tot["treat_ses01_mask"] == pop_tot.respond_wk16.notnull() & (pop_tot.session == "ses-01"))
 
-cluster = pop[['participant_id', 'session', 'treatment', 'respond_wk16_num']]
+mask_ctl = pop_tot.treatment == "Control"
+mask_ctl_ses01 = (pop_tot.treatment == "Control") & (pop_tot.session == "ses-01")
+mask_treat_ses01 = pop_tot.respond_wk16.notnull() & (pop_tot.session == "ses-01")
 
-XTotTivSiteCtr = img['XTotTivSiteCtr']
-mask_ctl = pop.treatment == "Control"
-mask_ctl_ses01 = (pop.treatment == "Control") & (pop.session == "ses-01")
-mask_treat_ses01 = pop.respond_wk16.notnull() & (pop.session == "ses-01")
-
+# centers_ = np.load(os.path.join(WD, "clusters_centers.npz"))
+# cluster_tot_ = pd.read_csv(os.path.join(WD, "clusters.csv"))
 ###############################################################################
 # Clustering
 ###############################################################################
 
 centers = dict()
+XTotTivSiteCtr = img_tot['XTotTivSiteCtr']
 
 # XTreatTivSiteCtr
-XTreatSes1TivSiteCtrStdz = scaler.fit(XTotTivSiteCtr[mask_treat_ses01]).transform(XTotTivSiteCtr[mask_treat_ses01])
+XTreatSes1TivSiteCtrStdz = scaler.fit_transform(XTotTivSiteCtr[mask_treat_ses01])
 clusterer_treat = KMeans(n_clusters=2, random_state=10)#, n_jobs=5)
 clusterer_treat.fit(XTreatSes1TivSiteCtrStdz)
 centers["XTreatSes1TivSiteCtr"] = np.copy(clusterer_treat.cluster_centers_)
+#np.all(centers_["XTreatSes1TivSiteCtr"] == centers["XTreatSes1TivSiteCtr"])
 
-# Check non regression
+# Check non regression on clustering based on Patients
 orig = pd.read_csv("/neurospin/psy/canbind/models/clustering_v02/XTreatTivSite-clust1_img-scores.csv")[["participant_id", "respond_wk16", "cluster"]]
 target = np.array(orig.cluster)
-np.all(np.array(orig["participant_id"]) == np.array(pop[mask_treat_ses01]["participant_id"]))
+assert np.all(np.array(orig["participant_id"]) == np.array(pop_tot[mask_treat_ses01]["participant_id"]))
 assert np.all(clusterer_treat.predict(XTreatSes1TivSiteCtrStdz) == target)
 
 
 # XCtlSes1TivSiteCtr
-XCtlSes1TivSiteCtrStdz = scaler.fit(XTotTivSiteCtr[mask_ctl_ses01]).transform(XTotTivSiteCtr[mask_ctl_ses01])
+XCtlSes1TivSiteCtrStdz = scaler.fit_transform(XTotTivSiteCtr[mask_ctl_ses01])
 clusterer_ctl = KMeans(n_clusters=2, random_state=0, n_jobs=5)
 clusterer_ctl.fit(XCtlSes1TivSiteCtrStdz)
 # permute
 clusterer_ctl.cluster_centers_ = clusterer_ctl.cluster_centers_[[1, 0]]
 centers["XCtlSes1TivSiteCtr"] = np.copy(clusterer_ctl.cluster_centers_)
+#np.all(centers_["XCtlSes1TivSiteCtr"] == centers["XCtlSes1TivSiteCtr"])
 
-print(np.sum(target != clusterer_ctl.predict(XTreatSes1TivSiteCtrStdz)))
-metrics.confusion_matrix(pop.respond_wk16_num[mask_treat_ses01], clusterer_ctl.predict(XTreatSes1TivSiteCtrStdz))
+assert np.sum(target != clusterer_ctl.predict(XTreatSes1TivSiteCtrStdz)) == 4
+metrics.confusion_matrix(pop_tot.respond_wk16_num[mask_treat_ses01], clusterer_ctl.predict(XTreatSes1TivSiteCtrStdz))
 """
 array([[16, 16],
        [42, 50]])
@@ -103,6 +108,26 @@ cluster["proj_XCtlSes1TivSiteCtr_all"] = np.dot(XTotTivSiteCtrStdz, diff)
 cluster.to_csv(os.path.join(WD, "clusters.csv"), index=False)
 np.savez_compressed(os.path.join(WD, "clusters_centers.npz"),  **centers)
 
+######################################
+# Some QC with the previous clustering
+
+orig = pd.read_csv("/neurospin/psy/canbind/models/clustering_v02/XTreatTivSite-clust1_img-scores.csv")[["participant_id", "respond_wk16", "cluster"]]
+target = np.array(orig.cluster)
+assert np.all(np.array(orig["participant_id"]) == np.array(pop_tot[mask_treat_ses01]["participant_id"]))
+missmatch = cluster["cluster_XCtlSes1TivSiteCtr"][mask_treat_ses01] != target
+pop_ = pop_tot[mask_treat_ses01]
+pop_["orig"] = target
+pop_["new"] = cluster["cluster_XCtlSes1TivSiteCtr"]
+print(pop_[missmatch][['participant_id', 'age', 'sex', 'educ', 'respond_wk16', 'orig', 'new']])
+"""
+    participant_id   age  sex  educ  respond_wk16  orig  new
+293   sub-TGH-0040  20.0  1.0  17.0     Responder     0  1.0
+364   sub-TGH-0072  46.0  1.0  17.0     Responder     0  1.0
+427   sub-UBC-0012  21.0  1.0  19.0  NonResponder     0  1.0
+537   sub-UBC-0057  53.0  1.0  16.0     Responder     0  1.0
+"""
+assert np.sum(missmatch) == 4
+
 ###############################################################################
 # ML
 ###############################################################################
@@ -117,7 +142,7 @@ from sklearn.pipeline import make_pipeline
 #cluster_labels = clustering.cluster
 C = 0.1
 NFOLDS = 5
-#cv = StratifiedKFold(n_splits=NFOLDS)
+# Shuffle for the site effect + use 42 (magic number) + 1 (Prime number) as seed
 cv = StratifiedKFold(n_splits=NFOLDS, shuffle=True, random_state=42+1)
 #model = lm.LogisticRegression(class_weight='balanced', fit_intercept=False, C=C)
 scaler = preprocessing.StandardScaler()
@@ -126,52 +151,58 @@ def balanced_acc(estimator, X, y):
 scorers = {'auc': 'roc_auc', 'bacc':balanced_acc, 'acc':'accuracy'}
 
 ###############################################################################
-# Data
+# Re-load Clustering
+###############################################################################
 
-import nibabel
+cluster_tot = pd.read_csv(os.path.join(WD, "clusters.csv"))
+assert np.all(cluster_tot['participant_id'] == pop_tot['participant_id'])
 
-CLUST = 1
+centers = np.load(os.path.join(WD, "clusters_centers.npz"))
+'''
+cluster_tot["proj_XCtlSes1TivSiteCtr"]
+cluster_tot["cluster_XCtlSes1TivSiteCtr"]
+cluster_tot["proj_XCtlSes1TivSiteCtr_all"]
+cluster_tot["cluster_XCtlSes1TivSiteCtr_all"]
+'''
 
-# Data
-pop = pd.read_csv(os.path.join(WD, "population.csv"))[mask_treat_ses01]
-cluster = pd.read_csv(os.path.join(WD, "clusters.csv"))[mask_treat_ses01]
-Xim = XTotTivSiteCtr[mask_treat_ses01]
-Xim.shape == (124, 397559)
-yorig = np.array(pop['respond_wk16_num'], dtype=int)
-# QC
-assert np.all(pop.participant_id == cluster.participant_id)
-metrics.confusion_matrix(yorig, cluster["cluster_XCtlSes1TivSiteCtr"])
-"""
-array([[16, 16],
-       [42, 50]])
-"""
-orig = pd.read_csv("/neurospin/psy/canbind/models/clustering_v02/XTreatTivSite-clust1_img-scores.csv")[["participant_id", "respond_wk16", "cluster"]]
-target = np.array(orig.cluster)
-np.all(np.array(orig["participant_id"]) == np.array(pop[mask_treat_ses01]["participant_id"]))
-nook = cluster["cluster_XCtlSes1TivSiteCtr"] != target
-pop_ = pop.copy()
-pop_["orig"] = target
-pop_["new"] = cluster["cluster_XCtlSes1TivSiteCtr"]
-print(pop_[nook][['participant_id', 'age', 'sex', 'educ', 'respond_wk16', 'orig', 'new']])
-"""
-    participant_id   age  sex  educ  respond_wk16  orig  new
-293   sub-TGH-0040  20.0  1.0  17.0     Responder     0  1.0
-364   sub-TGH-0072  46.0  1.0  17.0     Responder     0  1.0
-427   sub-UBC-0012  21.0  1.0  19.0  NonResponder     0  1.0
-537   sub-UBC-0057  53.0  1.0  16.0     Responder     0  1.0
-"""
-assert np.sum(nook) == 4
-# QC end
+mask_gp1 = (cluster_tot["cluster_XCtlSes1TivSiteCtr"] == 1) & mask_treat_ses01
+mask_gp0 = (cluster_tot["cluster_XCtlSes1TivSiteCtr"] == 0) & mask_treat_ses01
+mask_gp1_withintreat = (cluster_tot.loc[mask_treat_ses01, "cluster_XCtlSes1TivSiteCtr"] == 1)
+mask_gp0_withintreat = (cluster_tot.loc[mask_treat_ses01, "cluster_XCtlSes1TivSiteCtr"] == 0)
 
-subset = cluster["cluster_XCtlSes1TivSiteCtr"] == CLUST
-assert subset.sum() == 66
+assert mask_gp1.sum() == 66
+assert mask_gp1.sum() + mask_gp0.sum() == mask_treat_ses01.sum()
 
-#X = scaler.fit_transform(Xim[subset, :])
-X = Xim[subset, :]
-y = yorig[subset]
+assert np.all(
+    metrics.confusion_matrix(pop_tot['respond_wk16_num'][mask_treat_ses01],
+                             cluster_tot["cluster_XCtlSes1TivSiteCtr"][mask_treat_ses01]) == \
+    np.array([[16, 16],
+              [42, 50]]))
+
+###############################################################################
+# Imaging data
+###############################################################################
+
+
+#######################
+# 1) Keep only subgroup
+
+if CLUST == 1:
+    Xim_g1 = img_tot['XTotTivSiteCtr'][mask_gp1]
+    assert Xim_g1.shape == (66, 397559)
+    y_g1 = np.array(pop_tot['respond_wk16_num'][mask_gp1], dtype=int)
+    X, y = Xim_g1, y_g1
+    assert np.all(np.array([np.sum(lab==y) for lab in np.unique(y)]) == (16, 50))
+elif CLUST == 0:
+    Xim_g0 = img_tot['XTotTivSiteCtr'][mask_gp0]
+    assert Xim_g0.shape == (58, 397559)
+    y_g0 = np.array(pop_tot['respond_wk16_num'][mask_gp0], dtype=int)
+    X, y = Xim_g0, y_g0
+
 
 ###############################################################################
 # EnetTV
+###############################################################################
 
 import parsimony.algorithms as algorithms
 import parsimony.estimators as estimators
@@ -193,9 +224,6 @@ l1 = alpha * float(1 - tv) * l1l2ratio
 l2 = alpha * float(1 - tv) * (1- l1l2ratio)
 
 print(key, algo, alpha, l1, l2, tv)
-
-
-#X = scaler.fit(X).transform(X)
 
 y_test_pred = np.zeros(len(y))
 y_test_prob_pred = np.zeros(len(y))
@@ -236,14 +264,19 @@ bacc_test_microavg = recall_test_microavg.mean()
 auc_test_microavg = metrics.roc_auc_score(y, y_test_prob_pred)
 acc_test_microavg = metrics.accuracy_score(y, y_test_pred)
 
-#print("#", IMADATASET+"-clust%i"%CLUST, X.shape)
 print("#", auc_test_microavg, bacc_test_microavg, acc_test_microavg)
 print("#", auc_test)
 print("#", recalls_test)
 print("#", acc_test)
 
+# Refit
+conesta = algorithms.proximal.CONESTA(max_iter=10000)
+estimator = estimators.LogisticRegressionL1L2TV(l1, l2, tv, Atv, algorithm=conesta,
+                                                class_weight="auto", penalty_start=0)
+estimator.fit(scaler.fit_transform(X), y)
+
 np.savez_compressed(os.path.join(WD, "XCtlSes1TivSiteCtr-clust-Ctl-%i"%CLUST +"_enettv_0.1_0.1_0.8_%icv.npz" % NFOLDS),
-                    beta=coefs_cv, y_pred=y_test_pred, y_true=y,
+                    beta_cv=coefs_cv, beta_refit=estimator.beta.ravel(), y_pred=y_test_pred, y_true=y,
                     proba_pred=y_test_prob_pred)
 
 # YEAH !
@@ -251,7 +284,6 @@ np.savez_compressed(os.path.join(WD, "XCtlSes1TivSiteCtr-clust-Ctl-%i"%CLUST +"_
 # [0.57499999999999996, 0.96666666666666679, 0.73333333333333339, 0.93333333333333335, 0.53333333333333344]
 # [array([ 0.5,  0.6]), array([ 1. ,  0.6]), array([ 0.66666667,  0.6       ]), array([ 1. ,  0.5]), array([ 0.33333333,  0.9       ])]
 # [0.5714285714285714, 0.69230769230769229, 0.61538461538461542, 0.61538461538461542, 0.76923076923076927]
-
 
 
 ###############################################################################
