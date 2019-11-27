94aa1286689068f5dbe32d7f88bbed8e52997a4a
duchesnay <edouard.duchesnay@gmail.com>
2019-11-27 17:04:55 +0100

    cat12-vbm_train_val_test

--- 2019_psy_datasets/201906_schizconnect-vip-prague-bsnip-biodb-icaar-start_assemble-all/make_dataset_cat12-vbm_train_val_test.py
+++ 2019_psy_datasets/201906_schizconnect-vip-prague-bsnip-biodb-icaar-start_assemble-all/make_dataset_cat12-vbm_train_val_test.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
-INPUT_PATH = '/neurospin/psy_sbox/analyses/201906_schizconnect-vip-prague-bsnip-biodb-icaar-start_assemble-all/data/cat12vbm/*.csv'
-OUTPUT_SEXAGE = '/neurospin/psy_sbox/analyses/201906_schizconnect-vip-prague-bsnip-biodb-icaar-start_sex-age/data/cat12vbm/{dataset}_{modality}_{tags}_{type}.{ext}'
-OUTPUT_DX = '/neurospin/psy_sbox/analyses/201906_schizconnect-vip-prague-bsnip-biodb-icaar-start_dx/data/cat12vbm/{dataset}_{modality}_{tags}_{type}.{ext}'
+INPUT_PATH = '/neurospin/psy_sbox/analyses/201906_schizconnect-vip-prague-bsnip-biodb-icaar-start_assemble-all/data/cat12vbm/*mwp1_gs_data-32.npy'
+INPUT_PATH = '/neurospin/psy_sbox/analyses/201906_schizconnect-vip-prague-bsnip-biodb-icaar-start_assemble-all/data/cat12vbm/*mwp1_gs_participants.csv'
+
+
+OUTPUT = '/neurospin/psy_sbox/analyses/201906_psy-hc_sex-age'
+SCRIPT = 'git/scripts/201906_psy-hc_sex-age/'
+# OUTPUT_DX = '/neurospin/psy_sbox/analyses/201906_psy-hc_sex-age'
 
 
 
