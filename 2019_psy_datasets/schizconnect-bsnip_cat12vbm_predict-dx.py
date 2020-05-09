50e3e848a4fdff80e02709ff3d01e9230cfec668
Edouard Duchesnay <edouard.duchesnay@cea.fr>
2020-05-09 02:28:30 +0200

    enettv biobd + schizconnect

--- 2019_psy_datasets/schizconnect-bsnip_cat12vbm_predict-dx.py
+++ 2019_psy_datasets/schizconnect-bsnip_cat12vbm_predict-dx.py
@@ -44,6 +44,7 @@ import sklearn.ensemble as ensemble
 from sklearn.model_selection import StratifiedKFold, KFold, LeaveOneOut
 from nitk.stats import Residualizer
 from nitk.utils import dict_product, parallel, reduce_cv_classif
+from nitk.utils import maps_similarity, arr_threshold_from_norm2_ratio
 
 INPUT_PATH = '/neurospin/psy_sbox/analyses/201906_schizconnect-vip-prague-bsnip-biodb-icaar-start_assemble-all/data/cat12vbm/'
 OUTPUT_PATH = '/neurospin/psy_sbox/analyses/202004_schizconnect-bsnip_cat12vbm_predict-dx'
@@ -106,7 +107,7 @@ def fit_predict(key, estimator_img, residualize, split):
     Xim_train = scaler.fit_transform(Xim_train)
     Xim_test = scaler.transform(Xim_test)
     try: # if coeficient can be retrieved given the key
-        estimator_img.coef_ = coefs_cv_cache[key]
+        estimator_img.coef_ = KEY_VALS[key]['coef_img']
     except: # if not fit
         estimator_img.fit(Xim_train, y_train)
 
@@ -184,6 +185,8 @@ if not os.path.exists(OUTPUT(dataset=DATASET_FULL, scaling=scaling, harmo=harmo,
     del imgs_arr
     import gc
     gc.collect()
+
+    # reload and do QC
     imgs_arr = np.load(OUTPUT(DATASET_FULL, scaling=scaling, harmo=harmo, type="data64", ext="npy"), mmap_mode='r')
     print(imgs_arr.shape)
 
@@ -372,6 +375,7 @@ if not os.path.exists(OUTPUT(DATASET_TRAIN, scaling=scaling, harmo=harmo, type="
 
     cv = StratifiedKFold(n_splits=NSPLITS, shuffle=True, random_state=3)
     cv_dict = {"CV%i" % fold:split for fold, split in enumerate(cv.split(Xim, y))}
+    cv_dict["ALL"] = [np.arange(Xim.shape[0]), np.arange(Xim.shape[0])]
 
     print([[lab, np.sum(y == lab)] for lab in np.unique(y)])
     #  [[0, 330], [1, 275]]
@@ -539,7 +543,7 @@ if not os.path.exists(OUTPUT(DATASET_TRAIN, scaling=scaling, harmo=harmo, type="
     # Large range
     alphas = [.01, .1, 1.]
     l1l2ratios = [0, 0.01, 0.1]
-    tvcoefs = [0.001, 0.01, 0.1, 1]
+    tvcoefs = [0, 0.0001, 0.001, 0.01, 0.1, 1]
 
     # Smaller range
     # alphas = [.1]
@@ -558,12 +562,16 @@ if not os.path.exists(OUTPUT(DATASET_TRAIN, scaling=scaling, harmo=harmo, type="
                                                 class_weight="auto", penalty_start=0)
         estimators_dict[key] = estimator
 
-
     args_collection = dict_product(estimators_dict, dict(noresidualize=False), cv_dict)
 
+    models_filename = OUTPUT(DATASET_TRAIN, scaling=scaling, harmo=harmo, type="models-5cv-enettv", ext="pkl")
+    if os.path.exists(models_filename):
+        with open(models_filename, 'rb') as fd:
+            KEY_VALS = pickle.load(fd)
+        #key = list(args_collection)[10]
+        #list(KEY_VALS)
     key_vals = parallel(fit_predict, args_collection, n_jobs=NJOBS, pass_key=True, verbose=20)
 
-    models_filename = OUTPUT(DATASET_TRAIN, scaling=scaling, harmo=harmo, type="models-5cv-enettv", ext="pkl")
     with open(models_filename, 'wb') as fd:
         pickle.dump(key_vals, fd)
 
@@ -574,4 +582,99 @@ if not os.path.exists(OUTPUT(DATASET_TRAIN, scaling=scaling, harmo=harmo, type="
         cv_scores.to_excel(writer, sheet_name='folds', index=False)
         cv_scores.groupby(["param_0"]).mean().to_excel(writer, sheet_name='mean')
 
-# [Parallel(n_jobs=8)]: Done 180 out of 180 | elapsed: 4900.6min finished
+    # [Parallel(n_jobs=8)]: Done 180 out of 180 | elapsed: 4900.6min finished
+
+    print("""
+    #------------------------------------------------------------------------------
+    # maps' similarity measures accross CV-folds
+    """)
+    models_filename = OUTPUT(DATASET_TRAIN, scaling=scaling, harmo=harmo, type="models-5cv-enettv", ext="pkl")
+    with open(models_filename, 'rb') as fd:
+        key_vals = pickle.load(fd)
+
+    # Agregate maps by key[0]
+    by_param = {k:[] for k in set([k[0] for k in key_vals])}
+    for k, v in key_vals.items():
+        by_param[k[0]].append(v['coef_img'].ravel())
+
+    maps_similarity_l = list()
+    # Compute similarity measures
+    for k, v in by_param.items():
+        maps = np.array(v)
+        maps_t = np.vstack([arr_threshold_from_norm2_ratio(maps[i, :], .99)[0] for i in range(maps.shape[0])])
+        r_bar, dice_bar, fleiss_kappa_stat = maps_similarity(maps_t)
+        prop_non_zeros_mean = np.count_nonzero(maps_t) / np.prod(maps_t.shape)
+        maps_similarity_l.append([k, prop_non_zeros_mean, r_bar, dice_bar, fleiss_kappa_stat])
+
+    map_sim = pd.DataFrame(maps_similarity_l, columns=['param_0', 'prop_non_zeros_mean', 'r_bar', 'dice_bar', 'fleiss_kappa_stat'])
+
+    # Update excel file with similariy measures
+    xls_filename = OUTPUT(DATASET_TRAIN, scaling=scaling, harmo=harmo, type="models-5cv-enettv", ext="xlsx")
+    cv_scores = pd.read_excel(xls_filename, sheet_name='folds')
+    pred_score_mean_ = cv_scores.groupby(["param_0"]).mean()
+    pred_score_mean_ = pred_score_mean_.reset_index()
+    pred_score_mean = pd.merge(pred_score_mean_, map_sim)
+    assert pred_score_mean_.shape[0] == map_sim.shape[0] == pred_score_mean.shape[0]
+
+    with pd.ExcelWriter(xls_filename) as writer:
+        cv_scores.to_excel(writer, sheet_name='folds', index=False)
+        pred_score_mean.to_excel(writer, sheet_name='mean')
+    del pred_score_mean_
+
+    print("""
+    #------------------------------------------------------------------------------
+    # plot
+    """)
+
+    xls_filename = OUTPUT(DATASET_TRAIN, scaling=scaling, harmo=harmo, type="models-5cv-enettv", ext="xlsx")
+    cv_scores = pd.read_excel(xls_filename, sheet_name='folds')
+    cv_scores = cv_scores.reset_index(drop=True)
+    cv_scores_mean = pd.read_excel(xls_filename, sheet_name='mean')
+    cv_scores_mean = cv_scores_mean.reset_index(drop=True)
+
+    keys_ = pd.DataFrame([[s.split("_")[0]] + [float(v) for v in s.split("_")[1].split(":")] for s in cv_scores["param_0"]],
+                 columns=["model", "alpha", "l1l2", "tv"])
+    cv_scores = pd.concat([keys_, cv_scores], axis=1)
+    keys_ = pd.DataFrame([[s.split("_")[0]] + [float(v) for v in s.split("_")[1].split(":")] for s in cv_scores_mean["param_0"]],
+                 columns=["model", "alpha", "l1l2", "tv"])
+    cv_scores_mean = pd.concat([keys_, cv_scores_mean], axis=1)
+    del keys_
+
+    from matplotlib.backends.backend_pdf import PdfPages
+    sns.set_style("whitegrid")
+    import matplotlib.pylab as pl
+    from matplotlib import rc
+    rc('text', usetex=True)
+    rc('font', size=14)
+    rc('legend', fontsize=14)
+    rc('text.latex', preamble=r'\usepackage{lmodern}')
+    plt.rcParams["font.family"] = ["Latin Modern Roman"]
+
+    # cv_scores["alpha"].unique(): array([0.01, 0.1 , 1.  ])
+    # cv_scores["l1l2"].unique(): array([0.  , 0.01, 0.1 ])
+    # cv_scores["tv"].unique() array([0.001, 0.01 , 0.1  , 1.   ])
+
+    pdf_filename = OUTPUT(DATASET_TRAIN, scaling=scaling, harmo=harmo, type="models-5cv-enettv", ext="pdf")
+    with PdfPages(pdf_filename) as pdf:
+        for l1l2 in cv_scores["l1l2"].unique():
+            print(l1l2)
+            df_ = cv_scores[cv_scores["l1l2"].isin([l1l2])]
+            dfm_ = cv_scores_mean[cv_scores_mean["l1l2"].isin([l1l2])]
+            df_["alpha"] = df_["alpha"].map({0.01:"1e-2'", 0.1:"1e-1'" , 1.:"1'"})
+            dfm_["alpha"] = dfm_["alpha"].map({0.01:"1e-2'", 0.1:"1e-1'" , 1.:"1'"})
+            df_.rename(columns={"alpha":"alpha", 'auc':'AUC', 'bacc':'bAcc'}, inplace=True)
+            dfm_.rename(columns={'r_bar':'$r_w$', 'prop_non_zeros_mean':'non-null', 'dice_bar':'dice', 'fleiss_kappa_stat':'Fleiss-Kappa'}, inplace=True)
+
+            fig, axs = plt.subplots(3, 2, figsize=(2 * 7.25, 3 * 5), dpi=300)
+            g = sns.lineplot(x="tv", y='AUC', hue="alpha", data=df_, ax=axs[0, 0], palette="Blues"); g.set(xscale="log")
+            g = sns.lineplot(x="tv", y='bAcc', hue="alpha", data=df_, ax=axs[0, 1], palette="Blues"); g.set(xscale="log")
+            g = sns.lineplot(x="tv", y='$r_w$', hue="alpha", data=dfm_, ax=axs[1, 0], palette="Blues"); g.set(xscale="log")
+            g = sns.lineplot(x="tv", y='non-null', hue="alpha", data=dfm_, ax=axs[1, 1], palette="Blues"); g.set(xscale="log")
+            g = sns.lineplot(x="tv", y='dice', hue="alpha", data=dfm_, ax=axs[2, 0], palette="Blues"); g.set(xscale="log")
+            g = sns.lineplot(x="tv", y='Fleiss-Kappa', hue="alpha", data=dfm_, ax=axs[2, 1], palette="Blues"); g.set(xscale="log")
+            #plt.tight_layout()
+            fig.suptitle('$\ell_1/\ell_2=%.3f$' % l1l2)
+            #plt.savefig(OUTPUT(DATASET_TRAIN, scaling=None, harmo=None, type="sensibility-l2-l1-enet_auc", ext="pdf"))
+            pdf.savefig()  # saves the current figure into a pdf page
+            fig.clf()
+            plt.close()
