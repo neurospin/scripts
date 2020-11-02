83ff39eddfecd3be93c1ac1d2cb9fae794885ba3
Benoit Dufumier <ben.dufumier@gmail.com>
2020-11-02 18:49:23 +0100

    * resampling with sklearn function added

--- 2019_psy_datasets/201906_schizconnect-vip-prague-bsnip-biodb-icaar-start_assemble-all/quasi_raw_resample.py
+++ 2019_psy_datasets/201906_schizconnect-vip-prague-bsnip-biodb-icaar-start_assemble-all/quasi_raw_resample.py
@@ -4,8 +4,9 @@ from nilearn.image import resample_img
 import pandas as pd
 import os
 from tqdm import tqdm
+from skimage.transform import rescale
 
-def resample_all(participants_path, factor=2.0, **kwargs):
+def nilearn_resample_all(participants_path, factor=2.0, **kwargs):
     nii_filenames = pd.read_csv(participants_path, **kwargs).ni_path
     if len(nii_filenames) == 0:
         return []
@@ -28,10 +29,32 @@ def resample_all(participants_path, factor=2.0, **kwargs):
     return data
 
 
+def sk_resample_all(ni_arr, factor=2.0, **kwargs):
+    if len(ni_arr) == 0:
+        return []
+    ni_ref = ni_arr[0]
+    ni_ref_resampled = rescale(ni_ref, factor, **kwargs)
+    arr_resampled = np.zeros((len(ni_arr),)+ni_ref_resampled.shape, dtype=np.float32)
+    print('Ref img of shape: {}\t After resizing: {}\tData size after resizing: {}'.format(ni_ref.shape,
+                                                                                           ni_ref_resampled.shape,
+                                                                                           arr_resampled.shape))
+    pbar = tqdm(total=len(ni_arr), desc='# Img resampled')
+    for i, arr in enumerate(ni_arr):
+        arr_resampled[i] = rescale(arr, factor, **kwargs)
+        pbar.update()
+    return arr_resampled
+
+
 if __name__ == "__main__":
 
-    participants_path = '/neurospin/psy_sbox/analyses/201906_schizconnect-vip-prague-bsnip-biodb-icaar-start_assemble-all/' \
-                 'data/quasi_raw/all_t1mri_quasi_raw_participants.tsv'
+    # participants_path = '/neurospin/psy_sbox/analyses/201906_schizconnect-vip-prague-bsnip-biodb-icaar-start_assemble-all/' \
+    #              'data/quasi_raw/all_t1mri_quasi_raw_participants.tsv'
+    #
+    # resampled_data = nilearn_resample_all(participants_path, factor=1.5, sep='\t')
+    # np.save(os.path.join(os.path.dirname(participants_path), 'all_t1mri_quasi_raw_data32_1.5mm.npy'), resampled_data)
 
-    resampled_data = resample_all(participants_path, factor=1.5, sep='\t')
-    np.save(os.path.join(os.path.dirname(participants_path), 'all_t1mri_quasi_raw_data32_1.5mm.npy'), resampled_data)
+    ni_filename = '/neurospin/psy_sbox/analyses/201906_schizconnect-vip-prague-bsnip-biodb-icaar-start_assemble-all/' \
+                 'data/quasi_raw/all_t1mri_quasi_raw_data32.npy'
+    ni_arr = np.load(ni_filename, mmap_mode='r')
+    resampled_data = sk_resample_all(ni_arr, factor=1/1.5)
+    np.save(os.path.join(os.path.dirname(ni_filename), 'all_t1mri_quasi_raw_data32_1.5mm_skimage.npy'), resampled_data)
