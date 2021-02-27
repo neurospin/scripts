77a8f72886201d6cee2832717e284d9f235ce08d
EDOUARD DUCHESNAY <edouard.duchesnay@gmail.com>
2021-02-27 16:19:57 +0100

    Memento+rundmc integrate phenotypes for rundmc

--- 2021_wmh_memento+rundmc/phenotypes_from_sourcedata.py
+++ 2021_wmh_memento+rundmc/phenotypes_from_sourcedata.py
@@ -27,6 +27,7 @@ MMSE ~ PC1 + Age + sex + education + Brain atrophy + Lacune NB + MB
 """
 
 import os.path
+import numpy as np
 import pandas as pd
 
 FS_NS = "/neurospin/brainomics"
@@ -53,7 +54,6 @@ MEMENTO_DATA = os.path.join(MEMENTO_PATH, "data")
 MEMENTO_MODEL = os.path.join(MEMENTO_PATH, "models/pca_enettv_0.000010_1.000_0.001")
 os.listdir(MEMENTO_MODEL)
 
-
 #%% RUNDMC
 RUNDMC_PATH = "{FS}/2019_rundmc_wmh/analyses/201909_rundmc_wmh_pca".format(FS=FS)
 RUNDMC_DATA = os.path.join(RUNDMC_PATH, "data")
@@ -66,6 +66,7 @@ phenotypes_filename = os.path.join(MEMENTO_PATH , "population.csv")
 phenotypes = pd.read_csv(phenotypes_filename)
 assert phenotypes.shape == (1755, 26)
 phenotypes.columns
+# phenotypes.to_excel(os.path.join(MEMENTO_PATH , "population.xlsx"), index=False)
 
 """
 phenotypes.columns
@@ -76,24 +77,40 @@ Index(['participant_id', 'nii_path', 'siteid', 'sex', 'age_cons', 'dmdip',
        'eavmem', 'eavatt', 'eavlang', 'npi_score_clin_d', 'statut', 'py',
        'etiol', 'etiol_pres'],
       dtype='object')
+      
+Je ne connais pas la nomenclature de Memento.
+Est-ce que le tmta est en secondes ? de l’ordre de 30 à 180 ?
+
+Pour les fluences oui c’est bien ça.
+
+Pour la mémoire je ne sais pas. Tu n’as aucun moyen de vérifier ? Si tu fais ça dans les règles, tu peux contacter Vincent Bouteloup qui bosse avec Carole et qui pourra t’aider. Carole est au courant de l’étude que nous faisons.
+
 """
 
-memento_mapping = dict("participant_id=participant_id",
-                       mmssctot="MMSE",
+MEMENTO_MAPPING = dict(participant_id="participant_id",
+                       sex='sex', # in [1, 2] M/F ?
+                       age_cons='age',
+                       mmssctot="MMSE", # OK
                        tmta_taux="processing_speed",
-                       flu_p="executive_functions",
-                       memory="???"
+                       flu_p="executive_functions", # OK Eric
+                       # ?? = "memory",
+                       # ?? = "education",
+                       # ?? = "brain_volume",
+                       # ?? = "tiv",
+                       # ??:'lacune_nb',
+                       # ??: 'mb_nb'
                        )
 
-    • C1 vs memory (FCSRT MEMENTO, compound RUNDMC)
+#    • C1 vs memory (FCSRT MEMENTO, compound RUNDMC)
 
 #%% RUNDMC
 
-participants_filename = os.path.join("{FS}/2019_rundmc_wmh/analyses/201909_rundmc_wmh_pca".format(FS=FS) , "participants.csv")
+participants_filename = os.path.join(RUNDMC_DATA, "WMH_2006_participants.csv")
 phenotypes_filename = os.path.join("{FS}/2019_rundmc_wmh/sourcedata/".format(FS=FS) , "Clinical_data_RUNDMC.xlsx")
 
 participants = pd.read_csv(participants_filename)
 participants.participant_id = participants.participant_id.astype(str)
+assert participants.shape[0] == 267
 
 phenotypes = pd.read_excel(phenotypes_filename)
 assert phenotypes.shape == (503, 13)
@@ -108,9 +125,27 @@ Index(['RUNDMC_ID', 'Age', 'Sex(1=male)', 'Education', 'Education_7levels',
       dtype='object')
 """
 
-rundmc_mapping = dict(RUNDMC_ID="participant_id",
-                      MMSE="MMSE",
-                      Processing_speed="processing_speed",
-                      Executive_function="executive_functions",
-                      memory="Verbal_memory"
-)
+RUNDMC_MAPPING = {'RUNDMC_ID':"participant_id",
+                  'Age':'age',
+                  'Sex(1=male)':'sex', # in [0. 1]
+                  'MMSE':"MMSE",
+                  'Processing_speed':"processing_speed",
+                  'Executive_function':"executive_functions",
+                  'Verbal_memory':"memory",
+                  'Education':"education",
+                  'Brain_volume':"brain_volume",
+                  'ICV':"tiv",
+                  'Lacune_nr':'lacune_nb',
+                  'MB_nr':'mb_nb'
+                  }
+phenotypes = phenotypes.rename(columns=RUNDMC_MAPPING)
+# Sex mapping is {1: "M", 0: "F"} => {1: "M", 0: "F"}
+phenotypes["sex"] = phenotypes["sex"].map({1: "M", 0: "F"}).map({'M': 0, 'F': 1})
+phenotypes.participant_id = phenotypes.participant_id.astype(str)
+
+participants_with_phenotypes = pd.merge(participants, phenotypes, on='participant_id', how='inner')
+assert participants_with_phenotypes.shape[0] == 267
+assert np.all(participants.participant_id == participants_with_phenotypes.participant_id)
+
+participants_with_phenotypes_filename = os.path.join(RUNDMC_DATA, "WMH_2006_participants_with_phenotypes.csv")
+participants_with_phenotypes.to_csv(participants_with_phenotypes_filename, index=False)
--- 2021_wmh_memento+rundmc/stats_memento_rundmc.py
+++ 2021_wmh_memento+rundmc/stats_memento_rundmc.py
@@ -15,6 +15,8 @@ import pandas as pd
 import matplotlib.pylab as plt
 import nilearn
 from nilearn import plotting
+from nilearn.image import resample_to_img
+from sklearn.decomposition import PCA
 import argparse
 import glob
 
@@ -23,11 +25,14 @@ from matplotlib.backends.backend_pdf import PdfPages
 sys.path.append('/home/ed203246/git/scripts/2021_wmh_memento+rundmc')
 from file_utils import load_npy_nii
 
-import nilearn.datasets
-import brainomics.image_resample
-
+#import nilearn.datasets
+#import brainomics.image_resample
 
 FS = "/home/ed203246/data"
+
+#%% MEMENTO_RUNDMC
+MEMENTO_RUNDMC_PATH = "{FS}/2021_wmh_memento+rundmc".format(FS=FS)
+MEMENTO_RUNDMC_DATA = os.path.join(MEMENTO_RUNDMC_PATH, "data")
 
 #%% MEMENTO
 MEMENTO_PATH = "{FS}/2017_memento/analysis/WMH".format(FS=FS)
@@ -35,35 +40,52 @@ MEMENTO_DATA = os.path.join(MEMENTO_PATH, "data")
 MEMENTO_MODEL = os.path.join(MEMENTO_PATH, "models/pca_enettv_0.000010_1.000_0.001")
 os.listdir(MEMENTO_MODEL)
 
-
 #%% RUNDMC
 RUNDMC_PATH = "{FS}/2019_rundmc_wmh/analyses/201909_rundmc_wmh_pca".format(FS=FS)
 RUNDMC_DATA = os.path.join(RUNDMC_PATH, "data")
 RUNDMC_MODEL = os.path.join(RUNDMC_PATH, "models/pca_enettv_0.000035_1.000_0.005")
-os.listdir(RUNDMC_MODEL)
 
+os.listdir(RUNDMC_MODEL)
 
 ###############################################################################
 #%% LOAD DATA
+tissue_filename = os.path.join(MEMENTO_RUNDMC_DATA, "mask_cortex_ventricles_dill5mm_deepwm_mni_1mm_img.nii.gz")
 
+# MEMENTO
 model_filename = os.path.join(MEMENTO_MODEL, "model.npz")
 loadings_img_filename = os.path.join(MEMENTO_MODEL, "components-brain-maps.nii.gz")
 mask_img_filename = os.path.join(MEMENTO_DATA, "mask.nii.gz")
 data_filename = os.path.join(MEMENTO_DATA, "WMH_arr_msk.npy")
 data_shape = (1755, 116037)
-phenotypes_filename = os.path.join(MEMENTO_PATH , "population.csv")
+# phenotypes_filename = os.path.join(MEMENTO_PATH , "population.csv")
+# participants_with_phenotypes_filename = ??
 
-
+# RUNDMC
 model_filename = os.path.join(RUNDMC_MODEL, "model.npz")
 loadings_img_filename = os.path.join(RUNDMC_MODEL, "components-brain-maps.nii.gz")
 mask_img_filename = os.path.join(RUNDMC_DATA, "mask.nii.gz")
+# See /home/ed203246/git/scripts/2019_rundmc_wmh/pca_pcatv.py
 data_filename = os.path.join(RUNDMC_DATA, "WMH_2006.nii.gz")
+#participants_filename = os.path.join(RUNDMC_DATA, "WMH_2006_participants.csv")
+participants_with_phenotypes_filename = os.path.join(RUNDMC_DATA, "WMH_2006_participants_with_phenotypes.csv")
+
 data_shape = (267, 371278)
+# phenotypes_filename = os.path.join(MEMENTO_PATH , "population.csv")
 
 def load_data(model_filename, loadings_img_filename, mask_img_filename,
-              data_filename, data_shape, phenotypes_filename):
+              data_filename, data_shape, participants_with_phenotypes_filename):
+    # Phenoypes
+    phenotypes = pd.read_csv(participants_with_phenotypes_filename)
+
     # Model
     model = np.load(model_filename)
+    U, d, V, PC, explained_variance = model['U'], model['d'], model['V'], model['PC'], \
+                                      model['explained_variance']
+
+    assert phenotypes.shape[0] == PC.shape[0]
+    phenotypes["PC1"] = PC[:, 0]
+    phenotypes["PC2"] = PC[:, 1]
+    phenotypes["PC3"] = PC[:, 2]
 
     # Loading_img
     loadings_img = nibabel.load(loadings_img_filename)
@@ -84,80 +106,28 @@ def load_data(model_filename, loadings_img_filename, mask_img_filename,
         data_img = data
         data = data_img.get_fdata()[mask_arr].T
 
-    # center data
-    data = data - data.mean(axis=0)
+    phenotypes["wmh_tot"] = data.sum(axis=1)
 
     assert mask_arr.sum() == data.shape[1] == data_shape[1]
     assert data.shape[0] == data_shape[0]
 
-    # Phenoypes
-    phenotypes = pd.read_csv(phenotypes_filename)
+    # wmh_by_tissue
+    tissue_img = nibabel.load(tissue_filename)
+    tissue_img = resample_to_img(source_img=tissue_img, target_img=mask_img, interpolation='nearest')
+    tissue_arr = tissue_img.get_fdata().astype(int)
+    tissue_labels = dict(cortex=1, ventricles=2, deepwm=3)
 
-    return model, loadings_img, mask_img, data
+    assert np.all(tissue_arr == tissue_img.get_fdata())
+    wmh_pc_by_tissue = pd.DataFrame({"wmh_pc_%s" % tissue:PCA(n_components=1).fit_transform(data[:, tissue_arr[mask_arr] == lab]).ravel()
+        for tissue, lab in tissue_labels.items()})
+    wmh_sum_by_tissue = pd.DataFrame({"wmh_sum_%s" % tissue:data[:, tissue_arr[mask_arr] == lab].sum(axis=1)
+        for tissue, lab in tissue_labels.items()})
+    phenotypes = pd.concat([phenotypes, wmh_pc_by_tissue, wmh_sum_by_tissue], axis=1)
 
-# MEMENTO
-mask_img = nibabel.load(os.path.join(ANALYSIS_DATA_PATH, "mask.nii.gz"))
-mask_arr = mask_img.get_data() == 1
-assert mask_arr.sum() == 116037
+    # center data
+    data = data - data.mean(axis=0)
 
-# pop = pd.read_csv(os.path.join(ANALYSIS_DATA_PATH, "%s_participants.csv" % CONF["NI"]))
-
-NI_arr_msk = np.load(os.path.join(ANALYSIS_DATA_PATH, "WMH_arr_msk.npy"))
-assert NI_arr_msk.shape == (1755, 116037)
-
-print("time elapsed={:.2f}s".format(time.time() - current_time))
-
-# Check shape
-#assert NI_arr.shape == tuple(list(CONF["shape"]) + [pop.shape[0]])
-assert NI_arr_msk.shape == (pop.shape[0], mask_arr.sum())
-
-X = NI_arr_msk - NI_arr_msk.mean(axis=0)
-del NI_arr_msk
-
-
-
-# assert X.shape == (503, 51637)
-assert X.shape == (1755, 116037)
-assert mask_arr.sum() == X.shape[1]
-assert np.allclose(X.mean(axis=0), 0)
-
-
-mod = "memento"
-mod = "rundmc"
-
-###############################################################################
-#%% MEMENTO READ DATA
-
-coefs_filename = os.path.join(MEMENTO_MODELS_PATH, "components-brain-maps.nii.gz")
-coefs_img = nibabel.load(coefs_filename)
-# mni152_t1_15mm_img = resample_to_img(mni152_t1_1mm_img, coefs_img)
-#mni152_t1_15mm_img.to_filename(os.path.join(WD, "MNI152_T1_1.5mm.nii.gz"))
-
-mod_memento = np.load(os.path.join(MEMENTO_MODELS_PATH, "model.npz"))
-U, d, V, PC, explained_variance = mod_memento['U'], mod_memento['d'], mod_memento['V'], mod_memento['PC'], mod_memento['explained_variance']
-
-NI_arr_msk = np.load(os.path.join(ANALYSIS_DATA_PATH, "WMH_arr_msk.npy"))
-assert NI_arr_msk.shape == (1755, 116037)
-wmhvol = NI_arr_msk.sum(axis=1)
-
-phenotypes = pd.read_csv(PARTICIPANTS_CSV)
-
-assert phenotypes.shape[0] == PC.shape[0]
-
-pd.Series(wmhvol).describe()
-
-thres = -np.inf
-df_memento = phenotypes[wmhvol > thres]
-
-df_memento["PC1"] = PC[wmhvol > thres, 0]
-df_memento["PC2"] = PC[wmhvol > thres, 1]
-df_memento["PC3"] = PC[wmhvol > thres, 2]
-df_memento["wmh_tot"] = wmhvol[wmhvol > thres]
-df_memento["sex"] = df_memento.sex.astype('object')
-
-
-###############################################################################
-#%% RUNDMC READ DATA
+    return model, loadings_img, mask_img, data, phenotypes
 
 
 
@@ -167,15 +137,3 @@ df_memento["sex"] = df_memento.sex.astype('object')
 
 
 
-
-
-
-STUDY_PATH = '/neurospin/brainomics/2019_rundmc_wmh'
-DATA_PATH = os.path.join(STUDY_PATH, 'sourcedata', 'wmhmask')
-
-#ANALYSIS_PATH = os.path.join(STUDY_PATH, 'analysis', '201905_rundmc_wmh_pca')
-ANALYSIS_PATH = os.path.join(STUDY_PATH, 'analyses', '201909_rundmc_wmh_pca')
-ANALYSIS_DATA_PATH = os.path.join(ANALYSIS_PATH, "data")
-ANALYSIS_MODELS_PATH = os.path.join(ANALYSIS_PATH, "models")
-
-OUTPUT_DIR = os.path.join(ANALYSIS_MODELS_PATH, '{key}')
